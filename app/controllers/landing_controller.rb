ad807b636b31d5202582151fd5e9f62815c925ec
jfederico <jesus@123it.ca>
2016-10-18 17:17:05 -0400

    Removed reference to helper from LandingController

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,17 +1,14 @@
 class LandingController < ApplicationController
-  include LandingHelper
-
   def index
     @refreshable = (params[:resource] == 'meeting' && !params.has_key?(:id))
-    @meeting_token = params[:id] || @meeting_token = new_meeting_token
+    @meeting_token = params[:id] || @meeting_token = helpers.new_meeting_token
     @meeting_url = landing_url(@meeting_token)
   end
 
   # GET /token.json
   def new_meeting
     respond_to do |format|
-      meeting_url = landing_url(new_meeting_token)
-      logger.info meeting_url
+      meeting_url = landing_url(helpers.new_meeting_token)
       format.json { render :json => { :messageKey => "ok", :message => "New meeting URL created", :status => :ok, :response => { :meeting_url => meeting_url} }, :status => status }
     end
   end
