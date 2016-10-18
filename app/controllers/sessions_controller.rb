8693ba5860f1e0e5954f7d01422cd9fdd7662057
Zachary Chai <zachary.chai@blindsidenetworks.com>
2016-10-18 13:51:05 -0400

    personalized rooms page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@ class SessionsController < ApplicationController
   def create
     @user = User.from_omniauth(request.env['omniauth.auth'])
     session[:user_id] = @user.id
+    redirect_to controller: 'landing', action: 'room', name: @user.username
   rescue => e
     logger.error "Error authenticating via omniauth: #{e}"
-  ensure
     redirect_to root_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'meeting(/:id)', to: 'landing#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  get '/rooms/:name', to: 'landing#room'
 
   root to: 'landing#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
