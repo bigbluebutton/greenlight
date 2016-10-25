f71f46427fd70a1ed822cf107d7494c19abb7211
Zachary Chai <zachary.chai@blindsidenetworks.com>
2016-10-25 10:46:30 -0400

    cleanup controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,15 @@
 Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
-
-  # This should be removed once before being released
-  get '/auth/:provider/callback', to: 'sessions#create'
-  ###########################################################
-
-  get '/users/auth/:provider/callback', to: 'sessions#create'
   get '/users/logout', to: 'sessions#destroy', as: :user_logout
-  get '/meetings/join/:resource/:id', to: 'bbb#join', as: :bbb_join
-  get '/meetings/new', to: 'landing#new_meeting', as: :new_meeting
+
+  get '/auth/:provider/callback', to: 'sessions#create'
 
   # There are two resources [meetings|rooms]
   # meetings offer a landing page for NON authenticated users to create and join session in BigBlueButton
   # rooms offer a customized landing page for authenticated users to create and join session in BigBlueButton
   get '/:resource(/:id)', to: 'landing#index', as: :resource
+  get '/:resource/:id/join', to: 'bbb#join', as: :bbb_join
 
-  root to: 'landing#meeting', :resource => "meetings"
+  root to: 'landing#index', :resource => "meetings"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
