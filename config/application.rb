84fb5c617fc602ebb6f714e9572dc8bdc138b288
Zachary Chai <zachary.chai@blindsidenetworks.com>
2016-11-03 17:22:33 -0400

    configure i18n

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,13 @@ module Greenlight
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # i18n
+    # ensure each language has a regional fallback
+    config.i18n.available_locales = %w(en en-US)
+    config.i18n.default_locale = 'en-US'
+    config.i18n.fallbacks = {'en' => 'en-US'}
+
+    # BigBlueButton
     config.bigbluebutton_endpoint = ENV['BIGBLUEBUTTON_ENDPOINT']
     config.bigbluebutton_secret = ENV['BIGBLUEBUTTON_SECRET']
   end
