05739278510183ba3b95ebc71ce49a1e69ec40fb
Zachary Chai <zachary.chai@blindsidenetworks.com>
2016-11-29 14:58:01 -0500

    add basic tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should create user from omniauth" do
+    uid = '354545'
+    provider = 'twitter'
+    name = 'user name'
+    username = 'username'
+
+    assert_difference 'User.count' do
+      User.from_omniauth({'uid' => uid, 'provider' => provider, 'info' => {'name' => name, 'nickname' => username}})
+    end
+
+    user = User.find_by uid: uid, provider: provider
+    assert_not_nil user.encrypted_id
+    assert user.username, username
+    assert user.name, name
+  end
 end
