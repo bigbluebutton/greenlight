a6aed404f045c5e722ce5b4fbc855db0d24b849e
Zachary Chai <zachary.chai@blindsidenetworks.com>
2016-12-02 16:20:48 -0500

    point docker image to bigbluebutton repo

--- scripts/build_image_master.sh
+++ scripts/build_image_master.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker build -f Dockerfile.prod -t zachblind/greenlight:master .
+docker build -f Dockerfile.prod -t bigbluebutton/greenlight:master .
 
 docker login -e zachary.chai@blindsidenetworks.com -u $DOCKER_USER -p $DOCKER_PASS
-docker push zachblind/greenlight:master
+docker push bigbluebutton/greenlight:master
 
 docker logout
