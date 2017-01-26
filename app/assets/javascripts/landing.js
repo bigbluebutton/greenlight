066755c56c95ba72418f4838bb809065377b76db
Zachary Chai <zachary.chai@blindsidenetworks.com>
2017-01-26 18:03:42 -0500

    add invite and join to room

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -134,19 +134,6 @@
       }
     });
 
-    $('.center-panel-wrapper').on('keyup', '.meeting-name', function (event, msg) {
-      var newId = $(this).val();
-      Meeting.getInstance().setMeetingId(newId);
-      $(".page-wrapper.meetings").data('id', newId);
-      $('.meeting-url').val(Meeting.getInstance().getURL());
-      $('.join-meeting-title').html(I18n.join_title.replace(/%{id}/, newId));
-      if (newId === '') {
-        $('.invite-join-wrapper').addClass('hidden');
-      } else {
-        $('.invite-join-wrapper').removeClass('hidden');
-      }
-    });
-
     // enable tooltips
     var options = {
       selector: '.has-tooltip',
@@ -171,6 +158,19 @@
   };
 
   var initIndex = function() {
+
+    $('.center-panel-wrapper').on('keyup', '.meeting-name', function (event, msg) {
+      var newId = $(this).val();
+      Meeting.getInstance().setMeetingId(newId);
+      $(".page-wrapper.meetings").data('id', newId);
+      $('.meeting-url').val(Meeting.getInstance().getURL());
+      $('.join-meeting-title').html(I18n.join_title.replace(/%{id}/, newId));
+      if (newId === '') {
+        $('.invite-join-wrapper').addClass('hidden');
+      } else {
+        $('.invite-join-wrapper').removeClass('hidden');
+      }
+    });
 
     $('ul.previously-joined').empty();
     var joinedMeetings = localStorage.getItem('joinedMeetings');
