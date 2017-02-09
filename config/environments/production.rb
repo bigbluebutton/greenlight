85485d1bf2936dbdea1cd482c1a3a45dda9d4d48
Zachary Chai <zachary.chai@blindsidenetworks.com>
2017-02-09 14:47:49 -0500

    fallback to rails default when '/' root supplied

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.relative_url_root = ENV['RAILS_RELATIVE_URL_ROOT'] || '/b'
+  if ENV['RELATIVE_URL_ROOT'] != '/'
+    config.relative_url_root = ENV['RELATIVE_URL_ROOT'] || '/b'
+  end
 end
