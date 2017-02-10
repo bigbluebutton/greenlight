b9dcb65a9283f6771ff9c536a09aa32fb1f8a9ba
Zachary Chai <zachary.chai@blindsidenetworks.com>
2017-02-10 15:05:09 -0500

    force SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,11 +40,11 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = ENV['DISABLE_FORCE_SSL'].blank?
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
