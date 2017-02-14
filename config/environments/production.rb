fa1bb0683ee49d0245fb4415f7c4a62943a8d32b
Zachary Chai <zachary.chai@blindsidenetworks.com>
2017-02-14 18:11:32 -0500

    button tooltips

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,10 +40,8 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = ENV['DISABLE_FORCE_SSL'].blank?
+  config.force_ssl = ENV['ENABLE_FORCE_SSL'] == 'true'
 
-  # Use the lowest log level to ensure availability of diagnostic information
-  # when problems arise.
   config.log_level = :info
 
   # Prepend all log lines with the following tags.
