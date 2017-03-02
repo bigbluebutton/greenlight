a13e6b991e1d33009a215bc465cf0f2fd08c2db3
Zachary Chai <zachary.chai@blindsidenetworks.com>
2017-03-02 13:40:20 -0500

    redirect help to BBB videos

--- test/controllers/help_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class HelpControllerTest < ActionDispatch::IntegrationTest
-  test "should get getting_started" do
-    get help_url
-    assert_response :success
-  end
-
-end
