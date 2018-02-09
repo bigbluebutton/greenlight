7d61bfdd0bb347fff61d7557d9032e3765083453
jeremydenhartogh <jeremydenhartogh@gmail.com>
2018-02-09 14:12:26 -0500

    Launch Controller Cleanup

--- app/controllers/lti/launch_controller.rb
+++ app/controllers/lti/launch_controller.rb
@@ -215,43 +215,7 @@ module Lti
         # roles are required for lti resources
         raise RailsLti2Provider::LtiLaunch::Unauthorized.new(:insufficient_launch_info) unless params[:roles]
         session_cache(:membership_role, params[:roles])
-
-        unless (LTI1_RECOMMENDED_PARAMETERS - params.keys).length > 0
-          # generate username should a user require to be created
-          session_cache(:nickname, isProf? ? generate_nickname(params[:lis_person_name_full]) : params[:lis_person_name_full])
-          session_cache(:email,
-                        params[:lis_person_contact_email_primary] ? params[:lis_person_contact_email_primary] : "#{session_cache(:nickname)}@nomail")
-          session_cache(:first_name, params[:lis_person_name_given])
-          session_cache(:last_name, params[:lis_person_name_family])
-        else
-          # in the case that not all lis person identifiers are supplied
-          # set the session data for the parameters that are given
-          lis_params = params.select { |k,_| (LTI1_RECOMMENDED_PARAMETERS.include?(k) || k.include?("lis")) && !params[k].blank? }.keys
-          lis_params.each do |p|
-             if LTI1_PARAMETER_ALIASES[p]
-               unless p == "lis_person_name_full" && isProf?
-                 session_cache(LTI1_PARAMETER_ALIASES[p], params[p])
-               else
-                 session_cache(LTI1_PARAMETER_ALIASES[p], generate_nickname(params[p]))
-               end
-             end
-          end
-
-          # set the parameters that were not given by the lms using fallbacks defined below
-          # NO FULL NAME:  use sourcedid                                      (blank if sourcedid unavailable)
-          # NO EMAIL:      generate email using resource_link_id and user_id  (raise error if no user_id supplied)
-          # NO FIRST NAME: use sourcedid before username                      (unless sourcedid unavailable)
-          # NO LAST NAME:  defaults to 'User'
-
-          lis_params = lis_params.join(" ")
-          raise RailsLti2Provider::LtiLaunch::Unauthorized.new(:insufficient_launch_info) if !params[:user_id] && !lis_params.include?("email_primary")\
-
-          session_cache(:nickname, isProf? ? generate_nickname(params[:lis_person_sourcedid]) :
-                                             params[:lis_person_sourcedid]) if !lis_params.include?("name_full")
-          session_cache(:email, "#{Digest::SHA1.hexdigest(session_cache(:resource_link_id)+params[:user_id])}@nomail") if !lis_params.include?("email_primary")
-          session_cache(:first_name, !lis_params.include?("person_sourcedid") ? session_cache(:nickname) : params[:lis_person_sourcedid]) if !lis_params.include?("name_given")
-          session_cache(:last_name, "User") if !lis_params.include?("name_family")
-        end
+        set_session_person
       else
         session_cache(:resourcelink_title, params[:custom_resourcelink_title])
         session_cache(:resourcelink_description, params[:custom_resourcelink_description])
@@ -271,6 +235,45 @@ module Lti
       raise RailsLti2Provider::LtiLaunch::Unauthorized.new(:keypair_not_found) unless tool
       lti_authentication(tool)
       session_cache(:tool_id, @lti_launch.tool_id)
+    end
+
+    def set_session_person
+      unless (LTI1_RECOMMENDED_PARAMETERS - params.keys).length > 0
+        # generate username should a user require to be created
+        session_cache(:nickname, isProf? ? generate_nickname(params[:lis_person_name_full]) : params[:lis_person_name_full])
+        session_cache(:email,
+                      params[:lis_person_contact_email_primary] ? params[:lis_person_contact_email_primary] : "#{session_cache(:nickname)}@nomail")
+        session_cache(:first_name, params[:lis_person_name_given])
+        session_cache(:last_name, params[:lis_person_name_family])
+      else
+        # in the case that not all lis person identifiers are supplied
+        # set the session data for the parameters that are given
+        lis_params = params.select { |k,_| (LTI1_RECOMMENDED_PARAMETERS.include?(k) || k.include?("lis")) && !params[k].blank? }.keys
+        lis_params.each do |p|
+           if LTI1_PARAMETER_ALIASES[p]
+             unless p == "lis_person_name_full" && isProf?
+               session_cache(LTI1_PARAMETER_ALIASES[p], params[p])
+             else
+               session_cache(LTI1_PARAMETER_ALIASES[p], generate_nickname(params[p]))
+             end
+           end
+        end
+
+        # set the parameters that were not given by the lms using fallbacks defined below
+        # NO FULL NAME:  use sourcedid                                      (blank if sourcedid unavailable)
+        # NO EMAIL:      generate email using resource_link_id and user_id  (raise error if no user_id supplied)
+        # NO FIRST NAME: use sourcedid before username                      (unless sourcedid unavailable)
+        # NO LAST NAME:  defaults to 'User'
+
+        lis_params = lis_params.join(" ")
+        raise RailsLti2Provider::LtiLaunch::Unauthorized.new(:insufficient_launch_info) if !params[:user_id] && !lis_params.include?("email_primary")\
+
+        session_cache(:nickname, isProf? ? generate_nickname(params[:lis_person_sourcedid]) :
+                                           params[:lis_person_sourcedid]) if !lis_params.include?("name_full")
+        session_cache(:email, "#{Digest::SHA1.hexdigest(session_cache(:resource_link_id)+params[:user_id])}@nomail") if !lis_params.include?("email_primary")
+        session_cache(:first_name, !lis_params.include?("person_sourcedid") ? session_cache(:nickname) : params[:lis_person_sourcedid]) if !lis_params.include?("name_given")
+        session_cache(:last_name, "User") if !lis_params.include?("name_family")
+      end
     end
 
     def set_referrer_session
