9ae7743289b2fa45057c20562fd2f77d7c8f0987
jeremydenhartogh <jeremydenhartogh@gmail.com>
2018-03-13 15:30:46 -0400

    schema version update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928183010) do
+ActiveRecord::Schema.define(version: 20180309152600) do
 
   create_table "users", force: :cascade do |t|
     t.string   "provider",                                null: false
