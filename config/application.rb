b51ac6aa7c13a98473fb28dadcc87185c5ac65f8
bruckwubete <bruckwendwessenwubet@cmail.carleton.ca>
2018-07-04 15:24:01 -0400

    reverting unnecessary changes

--- config/application.rb
+++ config/application.rb
@@ -19,20 +19,29 @@ module Greenlight
 
     config.loadbalanced_configuration = ENV["LOADBALANCER_ENDPOINT"].present? && ENV["LOADBALANCER_SECRET"].present?
 
+    # Setup BigBlueButton configuration.
+    if config.loadbalanced_configuration
+      # Fetch credentials from a loadbalancer based on provider.
+      config.loadbalancer_endpoint = ENV["LOADBALANCER_ENDPOINT"]
+      config.loadbalancer_secret = ENV["LOADBALANCER_SECRET"]
+    else
+      # Default credentials (test-install.blindsidenetworks.com/bigbluebutton).
+      config.bigbluebutton_endpoint_default = "http://test-install.blindsidenetworks.com/bigbluebutton/api/"
+      config.bigbluebutton_secret_default = "8cd8ef52e8e101574e400365b55e11a6"
 
-    # Default credentials (test-install.blindsidenetworks.com/bigbluebutton).
-    config.bigbluebutton_endpoint_default = "http://test-install.blindsidenetworks.com/bigbluebutton/api"
-    config.bigbluebutton_secret_default = "8cd8ef52e8e101574e400365b55e11a6"
+      # Use standalone BigBlueButton server.
+      config.bigbluebutton_endpoint = ENV["BIGBLUEBUTTON_ENDPOINT"]
+      config.bigbluebutton_secret = ENV["BIGBLUEBUTTON_SECRET"]
 
+      # Fallback to testing credentails.
+      if config.bigbluebutton_endpoint.blank?
+        config.bigbluebutton_endpoint = config.bigbluebutton_endpoint_default
+        config.bigbluebutton_secret = config.bigbluebutton_secret_default
+      end
 
-    # Fetch credentials from a loadbalancer based on provider.
-    config.loadbalancer_endpoint = ENV["LOADBALANCER_ENDPOINT"]
-    config.loadbalancer_secret = ENV["LOADBALANCER_SECRET"]
-
-    # Use standalone BigBlueButton server.
-    config.bigbluebutton_endpoint = ENV["BIGBLUEBUTTON_ENDPOINT"] || config.bigbluebutton_endpoint_default
-    config.bigbluebutton_endpoint += "api" unless config.bigbluebutton_endpoint.ends_with?('api')
-    config.bigbluebutton_secret = ENV["BIGBLUEBUTTON_SECRET"] || config.bigbluebutton_secret_default
+      # Fix endpoint format if required.
+      config.bigbluebutton_endpoint += "api/" unless config.bigbluebutton_endpoint.ends_with?('api/')
+    end
 
     # Determine if GreenLight should allow non-omniauth signup/login.
     config.allow_user_signup = (ENV['ALLOW_GREENLIGHT_ACCOUNTS'] == "true")
