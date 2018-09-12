ef140f5f9eeec61799af2df774da502e12fcc07a
jiama843 <jiama843@gmail.com>
2018-09-12 09:45:06 -0400

    <reverted configuration settings>

--- config/application.rb
+++ config/application.rb
@@ -69,7 +69,7 @@ module Greenlight
     end
 
     # Determine if GreenLight should allow non-omniauth signup/login.
-    config.allow_user_signup = "true"
+    config.allow_user_signup = (ENV['ALLOW_GREENLIGHT_ACCOUNTS'] == "true")
 
     # Configure custom banner message.
     config.banner_message = ENV['BANNER_MESSAGE']
