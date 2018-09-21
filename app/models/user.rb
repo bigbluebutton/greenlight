f970743726a37c21e025c3099d157aac29687a41
jiama843 <jiama843@gmail.com>
2018-09-21 16:47:40 -0400

    <relaunching scrutinizer>

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,7 @@ class User < ApplicationRecord
 
   # Bypass validation if omniauth
   validates :accepted_terms, acceptance: true,
-                             unless: -> { !Rails.configuration.terms || !greenlight_account? }
+                             unless: -> { !greenlight_account? || !Rails.configuration.terms }
 
   # We don't want to require password validations on all accounts.
   has_secure_password(validations: false)
