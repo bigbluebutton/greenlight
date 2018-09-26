ce74b372c1e22e92e211d83a6cf42c90cef72ad9
jiama843 <jiama843@gmail.com>
2018-09-26 12:55:24 -0400

    <fixed changes>

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -58,7 +58,6 @@ class RoomsController < ApplicationController
   # POST /:room_uid
   def join
     opts = default_meeting_options
-
     unless @room.owned_by?(current_user)
       # Assign join name if passed.
       if params[@room.invite_path]
