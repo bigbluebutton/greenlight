6b3acb488d04192e42ae14e5c3c3c65ff9a9e580
jiama843 <jiama843@gmail.com>
2018-10-02 12:08:22 -0400

    <Made requested changes>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -28,7 +28,7 @@ class SessionsController < ApplicationController
   # POST /users/login
   def create
     user = User.find_by(email: session_params[:email])
-    if user && !user.greenlight_account? && session_params[:password]
+    if user && !user.greenlight_account?
       redirect_to root_path, notice: I18n.t("invalid_login_method")
     elsif user.try(:authenticate, session_params[:password])
       login(user)
