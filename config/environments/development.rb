735b1a2c452dd5a93cd7f476764db5bd34f2bfb5
jiama843 <jiama843@gmail.com>
2018-10-16 15:23:52 -0400

    <incorrect smtp settings no longer crashes the application>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   }
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
