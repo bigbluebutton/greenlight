e8df445424ac68f2694c827a4bb059c5ff29d631
jiama843 <jiama843@gmail.com>
2018-10-25 13:44:52 -0400

    <Remove unnecessary code>

--- app/models/room.rb
+++ app/models/room.rb
@@ -53,7 +53,6 @@ class Room < ApplicationRecord
       attendeePW: random_password(12),
       moderatorOnlyMessage: options[:moderator_message],
       "meta_#{META_LISTED}": false,
-      "meta_endCallbackUrl": options[:callbackUrl],
     }
 
     # Update session info.
