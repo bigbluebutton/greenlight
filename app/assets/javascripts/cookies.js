fe1af5d29de9b5e42bb16d2029df1f998d9b22bc
farhatahmad <ahmad.af.farhat@gmail.com>
2019-02-05 12:01:40 -0500

    Changed document.ready to turbolinks:load

--- app/assets/javascripts/cookies.js
+++ app/assets/javascripts/cookies.js
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License along
 // with BigBlueButton; if not, see <http://www.gnu.org/licenses/>.
 
-$(document).ready(function() {
+$(document).on('turbolinks:load', function(){
   $("#cookies-agree-button").click(function() {
     //create a cookie that lasts 1 year
     var cookieDate = new Date();
