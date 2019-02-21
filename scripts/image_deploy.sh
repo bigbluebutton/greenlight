d178df39e34922565f675039954231ce6b1a6068
jfederico <jesus@123it.ca>
2019-02-21 15:43:16 -0500

    Relocated env variable definition

--- scripts/image_deploy.sh
+++ scripts/image_deploy.sh
@@ -27,33 +27,34 @@ if [[ ($# == "--help") ||  $# == "-h" ]]; then
 	exit 0
 fi
 
-REF_SLUG=$1
-REF_NAME=$2
-COMMIT_SHA=$3
-COMMIT_BEFORE_SHA=$4
+export CD_REF_SLUG=$1
+export CD_REF_NAME=$2
+export CD_COMMIT_SHA=$3
+export CD_COMMIT_BEFORE_SHA=$4
+
 
 if [ -z $CD_DEPLOY_SCRIPT ]; then
   echo "Source for deployment script is not defined"
   exit 0
 fi
 
-if [ -z $REF_SLUG ]; then
+if [ -z $CD_REF_SLUG ]; then
   echo "Repository not included [e.g. bigbluebutton/greenlight]"
   exit 0
 fi
 
-if [ -z $REF_NAME ]; then
+if [ -z $CD_REF_NAME ]; then
   echo "Neither branch nor tag were included [e.g. master|release-2.0.5]"
   exit 0
 fi
 
 # It deploys only master and releases unless CD_DEPLOY_ALL is included
-if [ -z $DEPLOY_ALL ] && [ "$REF_NAME" != "master" ] && [[ "$REF_NAME" != *"release"* ]]; then
-  echo "Docker image for $REF_SLUG won't be deployed"
+if [ -z $DEPLOY_ALL ] && [ "$CD_REF_NAME" != "master" ] && [[ "$CD_REF_NAME" != *"release"* ]]; then
+  echo "Docker image for $CD_REF_SLUG won't be deployed"
   exit 0
 fi
 
-echo "Docker image $REF_SLUG:$REF_NAME is being deployed"
+echo "Docker image $CD_REF_SLUG:$CD_REF_NAME is being deployed"
 
 # The actual script should be pulled from an external repository
 if [ ! -z $CD_GITHUB_OAUTH_TOKEN ]; then
@@ -65,10 +66,6 @@ else
 fi
 
 chmod +x deploy.sh
-export CD_REF_SLUG=$REF_SLUG
-export CD_REF_NAME=$REF_NAME
-export CD_COMMIT_SHA=$COMMIT_SHA
-export CD_COMMIT_BEFORE_SHA=$COMMIT_BEFORE_SHA
-./deploy.sh $REF_SLUG $REF_NAME $COMMIT_SHA $COMMIT_BEFORE_SHA
+./deploy.sh $CD_REF_SLUG $CD_REF_NAME $CD_COMMIT_SHA $CD_COMMIT_BEFORE_SHA
 
 exit 0
