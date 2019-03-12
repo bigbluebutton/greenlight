1f0f101fa7ceecc01d0817d3091843ed45fcc399
jfederico <jesus@123it.ca>
2019-03-12 17:49:46 +0000

    Fixed isses detected by rubocop

--- db/migrate/20190312003555_add_password_to_rooms.rb
+++ db/migrate/20190312003555_add_password_to_rooms.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddPasswordToRooms < ActiveRecord::Migration[5.0]
   def change
     add_column :rooms, :moderator_pw, :string
@@ -5,8 +7,8 @@ class AddPasswordToRooms < ActiveRecord::Migration[5.0]
     Room.reset_column_information
     Room.all.each do |room|
       room.update_attributes!(
-        :moderator_pw => RandomPassword.generate(length: 12),
-        :attendee_pw => RandomPassword.generate(length: 12)
+        moderator_pw: RandomPassword.generate(length: 12),
+        attendee_pw: RandomPassword.generate(length: 12)
       )
     end
   end
