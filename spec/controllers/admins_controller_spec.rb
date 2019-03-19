00980ea4c3756d7dcfd6f76856c0fb675a81f2d3
farhatahmad <ahmad.af.farhat@gmail.com>
2019-03-19 16:06:00 -0400

    Fixed rubocop and added tests

--- spec/controllers/admins_controller_spec.rb
+++ spec/controllers/admins_controller_spec.rb
@@ -16,11 +16,28 @@
 # You should have received a copy of the GNU Lesser General Public License along
 # with BigBlueButton; if not, see <http://www.gnu.org/licenses/>.
 
-class AdminsController < ApplicationController
-  authorize_resource class: false
+require "rails_helper"
 
-  # GET /
-  def index
-    @users = User.all.reverse_order
+describe AdminsController, type: :controller do
+  context "GET #index" do
+    before do
+      @user = create(:user)
+      @admin = create(:user)
+      @admin.add_role :admin
+    end
+
+    it "renders a 404 if a user tries to acccess it" do
+      @request.session[:user_id] = @user.id
+      get :index
+
+      expect(response).to render_template(:not_found)
+    end
+
+    it "renders the admin settings if an admin tries to acccess it" do
+      @request.session[:user_id] = @admin.id
+      get :index
+
+      expect(response).to render_template(:index)
+    end
   end
 end
