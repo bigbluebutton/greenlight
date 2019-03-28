5ba5b663ac34c01f8bd06b17800809e0cf3bdb9f
Jesus Federico <jesus@123it.ca>
2019-03-28 11:02:36 -0400

    GRN-86: Change the way the locales are handled (#417)
    * Changed the way locales are shown
    
    * Updated the rest of the locales
    
    * Changed the way available_locales are defined
    
    * Updated locales in Russian
    
    * Updated locaales for German

--- config/application.rb
+++ config/application.rb
@@ -34,8 +34,8 @@ module Greenlight
     config.exceptions_app = routes
 
     # Configure I18n localization.
-    config.i18n.available_locales = %w(ar de el en es fr pt pt-br ru zh-cn)
-    config.i18n.default_locale = "en"
+    config.i18n.available_locales = [:en]
+    config.i18n.default_locale = :en
 
     config.i18n.available_locales.each do |locale|
       config.i18n.fallbacks[locale] = [locale, :en]
