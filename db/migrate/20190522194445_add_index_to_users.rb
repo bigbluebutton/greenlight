17115020212037e1fd1ea240e8dfe1fd90a4f701
farhatahmad <ahmad.af.farhat@gmail.com>
2019-05-23 11:08:27 -0400

    Added Indexs to improve db performance

---
+++ db/migrate/20190522194445_add_index_to_users.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# BigBlueButton open source conferencing system - http://www.bigbluebutton.org/.
+#
+# Copyright (c) 2018 BigBlueButton Inc. and by respective authors (see below).
+#
+# This program is free software; you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free Software
+# Foundation; either version 3.0 of the License, or (at your option) any later
+# version.
+#
+# BigBlueButton is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with BigBlueButton; if not, see <http://www.gnu.org/licenses/>.
+
+class AddIndexToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_index :users, :email
+    add_index :users, :provider
+    add_index :users, :created_at
+  end
+end
