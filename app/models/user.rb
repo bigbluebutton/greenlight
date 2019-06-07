00ec4c3479cbbccffa0d0e646180d4ad4d3b0602
madumlao <madumlao@gmail.com>
2019-06-07 23:11:49 +0800

    Update name and login based on external auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,8 +56,8 @@ class User < ApplicationRecord
       # Provider is the customer name if in loadbalanced config mode
       provider = auth['provider'] == "bn_launcher" ? auth['info']['customer'] : auth['provider']
       find_or_initialize_by(social_uid: auth['uid'], provider: provider).tap do |u|
-        u.name = auth_name(auth) unless u.name
-        u.username = auth_username(auth) unless u.username
+        u.name = auth_name(auth)
+        u.username = auth_username(auth)
         u.email = auth_email(auth)
         u.image = auth_image(auth)
         u.email_verified = true
