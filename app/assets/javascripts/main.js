77d41eac44a6c53fa297a01fc44ec79f8a011dd3
farhatahmad <ahmad.af.farhat@gmail.com>
2019-08-02 15:58:53 -0400

    Fixed issue with invalid csrf token

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -14,6 +14,10 @@
 // You should have received a copy of the GNU Lesser General Public License along
 // with BigBlueButton; if not, see <http://www.gnu.org/licenses/>.
 
+$(document).on('turbolinks:load', function(){
+  $.rails.refreshCSRFTokens();
+})
+
 document.addEventListener("turbolinks:before-cache", function() {
   $(".alert").remove()
 })
