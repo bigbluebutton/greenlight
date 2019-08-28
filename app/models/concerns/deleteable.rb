da5d3cf11992e303077102f7404e6ae6878a7f42
farhatahmad <ahmad.af.farhat@gmail.com>
2019-08-28 13:10:42 -0400

    Switched from deleting to tombstoning

---
+++ app/models/concerns/deleteable.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+# BigBlueButton open source conferencing system - http://www.bigbluebutton.org/.
+#
+# Copyright (c) 2018 BigBlueButton Inc. and by respective authors (see below).
+#
+# This program is free software; you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free Software
+# Foundation; either version 3.0 of the License, or (at your option) any later
+# version.
+#
+# BigBlueButton is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with BigBlueButton; if not, see <http://www.gnu.org/licenses/>.
+
+module Deleteable
+  extend ActiveSupport::Concern
+
+  included do
+    # By default don't include deleted
+    default_scope { where(deleted: false) }
+    scope :include_deleted, -> { unscope(where: :deleted) }
+    scope :deleted, -> { include_deleted.where(deleted: true) }
+  end
+
+  def destroy
+    run_callbacks :destroy
+    update_attribute(:deleted, true)
+  end
+
+  def delete
+    destroy
+  end
+
+  def undelete
+    assign_attributes(deleted: false)
+  end
+
+  def undelete!
+    update_attribute(:deleted, false)
+  end
+end
