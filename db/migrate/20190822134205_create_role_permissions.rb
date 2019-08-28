1740dd793002171006e31a8d4dabb13d53909e62
farhatahmad <ahmad.af.farhat@gmail.com>
2019-08-28 10:26:37 -0400

    Bug fixes for 2.4

--- db/migrate/20190822134205_create_role_permissions.rb
+++ db/migrate/20190822134205_create_role_permissions.rb
@@ -13,7 +13,7 @@ class CreateRolePermissions < ActiveRecord::Migration[5.2]
 
     Role.all.each do |role|
       role.role_permissions.create(name: "can_create_rooms", value: role.can_create_rooms.to_s, enabled: true)
-      role.role_permissions.create(name: "send_promoted_email", value: role.send_promoted_email.to_s, enabled: true)
+      role.role_permissions.create(name: "send_promoted_email", value: role.send_promoted_email.to_s, enabled: true)as
       role.role_permissions.create(name: "send_demoted_email", value: role.send_demoted_email.to_s, enabled: true)
       role.role_permissions.create(name: "can_edit_site_settings", value: role.can_edit_site_settings.to_s,
         enabled: true)
@@ -21,11 +21,12 @@ class CreateRolePermissions < ActiveRecord::Migration[5.2]
       role.role_permissions.create(name: "can_manage_users", value: role.can_manage_users.to_s, enabled: true)
     end
 
-    remove_column :roles, :can_create_rooms
-    remove_column :roles, :send_promoted_email
-    remove_column :roles, :send_demoted_email
-    remove_column :roles, :can_edit_site_settings
-    remove_column :roles, :can_edit_roles
-    remove_column :roles, :can_manage_users
+    # Add these back in once the change to postgres is made
+    # remove_column :roles, :can_create_rooms
+    # remove_column :roles, :send_promoted_email
+    # remove_column :roles, :send_demoted_email
+    # remove_column :roles, :can_edit_site_settings
+    # remove_column :roles, :can_edit_roles
+    # remove_column :roles, :can_manage_users
   end
 end
