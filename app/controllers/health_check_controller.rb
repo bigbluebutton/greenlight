7c40095524d3ab82d9d3b4737ac1db7ca882c00c
farhatahmad <ahmad.af.farhat@gmail.com>
2019-10-13 11:33:21 -0400

    Initial work on removing health check gem

---
+++ app/controllers/health_check_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# BigBlueButton open source conferencing system - http://www.bigbluebutton.org/.
+#
+# Copyright (c) 2018 BigBlueButton Inc. and by respective authors (see below).
+#
+# This program is free software; you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free Software
+# Foundation; either version 3.0 of the License, or (at your option) any later
+# version.
+#
+# BigBlueButton is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with BigBlueButton; if not, see <http://www.gnu.org/licenses/>.
+
+class HealthCheckController < ApplicationController
+  skip_before_action :redirect_to_https, :set_user_domain, :set_user_settings, :maintenance_mode?, :migration_error?,
+  :user_locale, :check_admin_password, :check_user_role
+
+  # GET /health_check
+  def all
+    render plain: "success"
+  end
+end
