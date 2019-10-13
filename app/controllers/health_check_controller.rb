9a259574644c19ab3c47d32577e8f14cf8131de1
Ahmad Farhat <ahmad.af.farhat@gmail.com>
2019-10-13 13:52:53 -0400

    Added health checks

--- app/controllers/health_check_controller.rb
+++ app/controllers/health_check_controller.rb
@@ -22,6 +22,53 @@ class HealthCheckController < ApplicationController
 
   # GET /health_check
   def all
-    render plain: "success"
+    response = "success"
+    @cache_expire = 10.seconds
+
+    begin
+      cache_check
+      database_check
+      email_check
+    rescue => e
+      response = "Health Check Failure: #{e}"
+    end
+
+    render plain: response
+  end
+
+  private
+
+  def cache_check
+    if Rails.cache.write("__health_check_cache_write__", "true", expires_in: @cache_expire)
+      raise "Unable to read from cache" unless Rails.cache.read("__health_check_cache_write__") == "true"
+    else
+      raise "Unable to write to cache"
+    end
+  end
+
+  def database_check
+    if defined?(ActiveRecord)
+      raise "Database not responding" unless ActiveRecord::Migrator.current_version
+    end
+    raise "Pending migrations" unless ActiveRecord::Migration.check_pending!.nil?
+  end
+
+  def email_check
+    test_smtp if Rails.configuration.action_mailer.delivery_method == :smtp
+  end
+
+  def test_smtp
+    settings = ActionMailer::Base.smtp_settings
+
+    smtp = Net::SMTP.new(settings[:address], settings[:port])
+    if settings[:enable_starttls_auto] == "true"
+      smtp.enable_starttls_auto if smtp.respond_to?(:enable_starttls_auto)
+    end
+
+    smtp.start(settings[:domain]) do |s|
+      s.authenticate(settings[:user_name], settings[:password], settings[:authentication])
+    end
+  rescue => e
+    raise "Unable to connect to SMTP Server - #{e}"
   end
 end
