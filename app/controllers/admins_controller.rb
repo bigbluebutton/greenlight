d7de55cbbaa9a83e9e868a974d6d612540718b06
farhatahmad <ahmad.af.farhat@gmail.com>
2019-11-21 14:34:21 -0500

    Added the ability to clear social uids

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -158,6 +158,13 @@ class AdminsController < ApplicationController
     end
   end
 
+  # POST /admins/clear_auth
+  def clear_auth
+    User.include_deleted.where(provider: @user_domain).update_all(social_uid: nil)
+
+    redirect_to admin_site_settings_path, flash: { success: I18n.t("administrator.flash.settings") }
+  end
+
   # ROLES
 
   # GET /admins/roles
--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,7 @@ Rails.application.routes.draw do
     post '/registration_method', to: 'admins#registration_method', as: :admin_change_registration
     post '/coloring', to: 'admins#coloring', as: :admin_coloring
     post '/clear_cache', to: 'admins#clear_cache', as: :admin_clear_cache
+    post '/clear_auth', to: 'admins#clear_auth', as: :admin_clear_auth
     # Roles
     post '/role', to: 'admins#new_role', as: :admin_new_role
     patch 'roles/order', to: 'admins#change_role_order', as: :admin_roles_order
