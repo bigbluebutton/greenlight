dad3958c257983f62c8c018c365b39b2265e307a
Etienne Vivash <etienne.vivash@blindsidenetworks.com>
2020-01-31 15:35:15 -0500

    added autofocus to Room Name text field

--- app/assets/javascripts/room.js
+++ app/assets/javascripts/room.js
@@ -130,6 +130,7 @@ function showCreateRoom(target) {
   $('#createRoomModal').on('shown.bs.modal', function () {
     $('#create-room-name').trigger('focus')
   })
+  
   var modal = $(target)
   $("#create-room-name").val("")
   $("#create-room-access-code").text(getLocalizedString("modal.create_room.access_code_placeholder"))
