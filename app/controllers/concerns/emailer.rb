eb8cc9b17f79f7743e55c773ad63f7bd762f6420
etiennevvv <59622352+etiennevvv@users.noreply.github.com>
2020-02-04 12:17:47 -0500

    GRN2-267 Redirect admin link when approving a new user to the  "pending" tab (#947)
    * GRN2-267 redirect admin to 'pending' tab when approving a new user
    
    * changes after robocop failed
    
    * robocop changes

--- app/controllers/concerns/emailer.rb
+++ app/controllers/concerns/emailer.rb
@@ -101,7 +101,8 @@ module Emailer
       return unless Rails.configuration.enable_email_verification
 
       admin_emails = admin_emails()
-      UserMailer.approval_user_signup(user, admins_url, admin_emails, @settings).deliver_now unless admin_emails.empty?
+      UserMailer.approval_user_signup(user, admins_url(tab: "pending"),
+      admin_emails, @settings).deliver_now unless admin_emails.empty?
     rescue => e
       logger.error "Support: Error in email delivery: #{e}"
       flash[:alert] = I18n.t(params[:message], default: I18n.t("delivery_error"))
