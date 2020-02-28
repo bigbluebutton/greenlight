7745b7c6b77a5e33e18775d952b3567fbf3e0779
Etienne Vivash <etienne.vivash@blindsidenetworks.com>
2020-02-28 11:25:18 -0500

    GRN2-268: rubocop fix

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -177,7 +177,7 @@ class AdminsController < ApplicationController
       logger.info "#{current_user.uid} successfully merged #{uid_to_merge} into #{@user.uid}"
       flash[:success] = I18n.t("administrator.flash.merge_success")
     end
-    
+
     redirect_back fallback_location: admins_path
   end
 
