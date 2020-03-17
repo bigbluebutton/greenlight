3a6dc6dfe26c677e837438b24961c13b078f5d16
einhirn <marg@rz.tu-clausthal.de>
2020-03-17 18:40:33 +0100

    no approval needed for ldap users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -214,7 +214,7 @@ class SessionsController < ApplicationController
     logger.info "Support: Auth user #{user.email} is attempting to login."
 
     # Add pending role if approval method and is a new user
-    if approval_registration && !@user_exists
+    if approval_registration && !@user_exists && @auth['provider'] != :ldap
       user.add_role :pending
 
       # Inform admins that a user signed up if emails are turned on
