d3e2dc38aff7ba67b8d4677b0c08cffcaf2f0be1
Gaja Sophie Peters <gaja.peters@uni-hamburg.de>
2020-05-06 18:27:00 +0200

    Added variable in .env to enforce certain domains in account registrations
    Allow REQUIRE_MAIL_DOMAIN to not exist in addition to it being empty
    
    Undo changes in config/locales/de_DE.yml
    
    changed .env variable name to GREENLIGHT_ACCOUNT_HD; allowed only comma and no whitespace as separator
    
    Allow the admin to change email-addresses, just enforce domain on registration

--- config/application.rb
+++ config/application.rb
@@ -89,6 +89,9 @@ module Greenlight
     # Determine if GreenLight should enable email verification
     config.enable_email_verification = (ENV['ALLOW_MAIL_NOTIFICATIONS'] == "true")
 
+    # Determine if GreenLight should require a certain mail-domain
+    config.require_email_domain = ENV["GREENLIGHT_ACCOUNT_HD"].to_s.split(",")
+
     # Determine if GreenLight should allow non-omniauth signup/login.
     config.allow_user_signup = (ENV['ALLOW_GREENLIGHT_ACCOUNTS'] == "true")
 
