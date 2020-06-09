49cb923fb7d573b4f359f9a271e8bd09ff0cb5c7
MrKeksi <x.treme.field@gmx.de>
2020-06-10 00:42:12 +0200

    travis fixes

--- app/helpers/admins_helper.rb
+++ app/helpers/admins_helper.rb
@@ -121,6 +121,7 @@ module AdminsHelper
   end
 
   def can_edit_maintenance_banner
-  (Rails.configuration.loadbalanced_configuration && current_user.has_role?(:super_admin)) || (!Rails.configuration.loadbalanced_configuration && current_user.has_role?(:admin))
+  (Rails.configuration.loadbalanced_configuration && current_user.has_role?(:super_admin)) ||
+      (!Rails.configuration.loadbalanced_configuration && current_user.has_role?(:admin))
   end
 end
