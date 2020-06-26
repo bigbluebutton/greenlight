9a96df6a37d56604abd8d9f21345ac9f3c9aa916
MrKeksi <mrkeksi@users.noreply.github.com>
2020-06-26 20:30:56 +0200

    Maintenance banner moved to admin site (#1775)
    * initial
    
    * finish
    
    * travis fixes
    
    * travis again
    
    * not required

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -84,9 +84,9 @@ class ApplicationController < ActionController::Base
           help: I18n.t("errors.maintenance.help"),
         }
     end
-    if Rails.configuration.maintenance_window.present?
-      unless cookies[:maintenance_window] == Rails.configuration.maintenance_window
-        flash.now[:maintenance] = Rails.configuration.maintenance_window
+    if @settings.get_value("Maintenance Banner").present?
+      unless cookies[:maintenance_window] == @settings.get_value("Maintenance Banner")
+        flash.now[:maintenance] = @settings.get_value("Maintenance Banner")
       end
     end
   end
