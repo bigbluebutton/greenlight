94661a9b63cda95499b95d0088d25d197f2ae532
Lê Xuân Nguyên <1803lxn@gmail.com>
2020-11-18 08:44:39 +0700

    test

--- app/assets/javascripts/recording.js
+++ app/assets/javascripts/recording.js
@@ -12,7 +12,7 @@
 // PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License along
-// with BigBlueButton; if not, see <http://www.gnu.org/licenses/>.
+// with ; if not, see <http://www.gnu.org/licenses/>.
 
 // Handle changing of settings tabs.
 $(document).on('turbolinks:load', function(){
