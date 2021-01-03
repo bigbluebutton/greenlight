ebdb6e8d188acca9b8df3b9342e2ad125b062b3f
Florian <flo@mzechmeister.de>
2021-01-03 14:35:49 +0100

    freeze all the things

--- db/migrate/20201203000606_add_moderator_access_code_to_rooms.rb
+++ db/migrate/20201203000606_add_moderator_access_code_to_rooms.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddModeratorAccessCodeToRooms < ActiveRecord::Migration[5.2]
   def change
     add_column :rooms, :moderator_access_code, :string, null: true, default: nil
