f15ea2083584c392b43982edbde2e1d2463da78c
Louis VICAINNE <lvicainne@users.noreply.github.com>
2021-01-25 00:39:48 +0100

    fix(omniauth): Correct too many redrect error with OpenID provider
    This fix avoids "Too Many redirects" error with login with OpenID and without terms

--- app/controllers/concerns/authenticator.rb
+++ app/controllers/concerns/authenticator.rb
@@ -44,7 +44,11 @@ module Authenticator
     elsif user.activated?
       # Dont redirect to any of these urls
       dont_redirect_to = [root_url, signin_url, ldap_signin_url, ldap_callback_url, signup_url, unauthorized_url,
-                          internal_error_url, not_found_url]
+                          internal_error_url, not_found_url, omniauth_session_url]
+      if !(ENV['OAUTH2_REDIRECT'].nil?)
+        dont_redirect_to.push(File.join(ENV['OAUTH2_REDIRECT'], "auth", "openid_connect", "callback"))
+      end
+
       url = if cookies[:return_to] && !dont_redirect_to.include?(cookies[:return_to])
         cookies[:return_to]
       elsif user.role.get_permission("can_create_rooms")
