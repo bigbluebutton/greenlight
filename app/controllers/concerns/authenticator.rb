996f3e181979e355da41bcb248990a1563ed2280
Louis VICAINNE <lvicainne@users.noreply.github.com>
2021-01-25 08:36:02 +0100

    Remove trailing space

--- app/controllers/concerns/authenticator.rb
+++ app/controllers/concerns/authenticator.rb
@@ -45,7 +45,7 @@ module Authenticator
       # Dont redirect to any of these urls
       dont_redirect_to = [root_url, signin_url, ldap_signin_url, ldap_callback_url, signup_url, unauthorized_url,
                           internal_error_url, not_found_url]
-      
+
       unless ENV['OAUTH2_REDIRECT'].nil?
         dont_redirect_to.push(File.join(ENV['OAUTH2_REDIRECT'], "auth", "openid_connect", "callback"))
       end
