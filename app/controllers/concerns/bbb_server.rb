a42c8e4683af4df4f766ca3663beec80f9b6d630
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2021-04-07 08:58:08 +0900

    Update bbb_server.rb
    'if current_user' should not have problem, but rubocop complains...

--- app/controllers/concerns/bbb_server.rb
+++ app/controllers/concerns/bbb_server.rb
@@ -54,9 +54,7 @@ module BbbServer
     join_opts = {}
     join_opts[:userID] = uid if uid
     join_opts[:join_via_html5] = true
-    if current_user
-      join_opts[:avatarURL] = current_user.image if current_user.image.present?
-    end
+    join_opts[:avatarURL] = current_user.image if (defined? current_user) && current_user.image.present?
     join_opts[:createTime] = room.last_session.to_datetime.strftime("%Q")
 
     bbb_server.join_meeting_url(room.bbb_id, name, password, join_opts)
