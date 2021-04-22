37c77d0cda47552506c061d7d48f5b94acba8673
Mohamad Abras <mabras@users.noreply.github.com>
2021-04-22 15:23:47 +0300

    remove duplicate commented code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -122,10 +122,6 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "greenlight-2_0_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
