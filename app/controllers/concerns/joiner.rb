e19e6e02fe18c7b8a699f0416a0b3079c8e07c87
Mitsutaka Sato <mitsutakasato@catalyst.net.nz>
2021-07-08 15:00:31 +1200

    Show access code in welcome message (#2817)
    * WR361495 Show access code in welcome message
    
    * Add spec and tidyup code

--- app/controllers/concerns/joiner.rb
+++ app/controllers/concerns/joiner.rb
@@ -84,11 +84,12 @@ module Joiner
 
   # Default, unconfigured meeting options.
   def default_meeting_options
-    invite_msg = I18n.t("invite_message")
+    moderator_message = "#{I18n.t('invite_message')}<br> #{request.base_url + room_path(@room)}"
+    moderator_message += "<br> #{I18n.t('modal.create_room.access_code')}: #{@room.access_code}" if @room.access_code.present?
     {
       user_is_moderator: false,
       meeting_logout_url: request.base_url + logout_room_path(@room),
-      moderator_message: "#{invite_msg}<br> #{request.base_url + room_path(@room)}",
+      moderator_message: moderator_message,
       host: request.host,
       recording_default_visibility: @settings.get_value("Default Recording Visibility") == "public"
     }
