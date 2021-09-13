9a614fc0443d3ddaa663804df449c992e6c89454
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2021-09-13 15:23:07 +0900

    Update joiner.rb

--- app/controllers/concerns/joiner.rb
+++ app/controllers/concerns/joiner.rb
@@ -60,7 +60,6 @@ module Joiner
       opts[:mute_on_start] = room_setting_with_config("muteOnStart")
 
       if current_user
-        opts[:avatarURL_size] = url_size(current_user.image);
         opts[:avatarURL] = current_user.image
         redirect_to join_path(@room, current_user.name, opts, current_user.uid)
       else
