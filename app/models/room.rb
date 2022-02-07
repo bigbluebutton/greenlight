1ca237694d8d4edc5ed660a791d71dfe06a0be3b
Khemissi Amir <amir.khemissi@insat.u-carthage.tn>
2022-02-07 21:20:51 +0100

    GIT-3073: Fixed the session failing for rooms with one character in length (Fixes #3073). (#3098)
    Co-authored-by: Ahmad Farhat <ahmad.af.farhat@gmail.com>

--- app/models/room.rb
+++ app/models/room.rb
@@ -25,7 +25,7 @@ class Room < ApplicationRecord
 
   before_destroy :destroy_presentation
 
-  validates :name, presence: true
+  validates :name, length: { in: 2..256 }
 
   belongs_to :owner, class_name: 'User', foreign_key: :user_id
   has_many :shared_access, dependent: :destroy
