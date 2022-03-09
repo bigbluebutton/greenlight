d10983953c40b92293e8dae431cfc8fe64048d0e
farhatahmad <ahmad.af.farhat@gmail.com>
2022-03-09 10:57:50 -0500

    CR

--- app/javascript/components/OtherTest.tsx
+++ app/javascript/components/OtherTest.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Test from "./Test";
 
 export default () => (
   <div>
--- app/javascript/components/Test.tsx
+++ app/javascript/components/Test.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import OtherTest from "./OtherTest";
 
 export default () => (
   <div>
