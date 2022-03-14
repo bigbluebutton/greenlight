2bfd46ed280e9624df9486dcf5c4bec60c573f91
Ahmad Farhat <ahmad.af.farhat@gmail.com>
2022-03-14 16:03:45 -0400

    Fixed db names again (#3291)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
+Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap-icons/font')
 
 # Precompile additional assets.
 # application.jsx, application.css, and all non-JS/CSS in the app/assets
