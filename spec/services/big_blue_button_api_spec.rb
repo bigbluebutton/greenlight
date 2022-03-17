f432df25fd19a0970ea4fbe7176812a07c6f9171
Hadi Cheaito <hadicheaito1@gmail.com>
2022-03-17 16:39:29 -0400

    Test cleanup

--- spec/services/big_blue_button_api_spec.rb
+++ spec/services/big_blue_button_api_spec.rb
@@ -4,16 +4,25 @@ require 'rails_helper'
 require 'bigbluebutton_api'
 
 describe BigBlueButtonApi, type: :service do
-  let(:bbb_server) { described_class.new }
-  let(:bbb_api) { bbb_server.bbb_server }
+  before do
+    ENV['BIGBLUEBUTTON_ENDPOINT'] = 'http://test.com/bigbluebutton/api'
+    ENV['BIGBLUEBUTTON_SECRET'] = 'test'
+  end
+
+  let(:bbb_service) { described_class.new }
 
   describe 'Instance of BigBlueButtonApi being created' do
     it 'Created an instance of BigBlueButtonApi' do
-      expect(bbb_api).to be_instance_of(BigBlueButton::BigBlueButtonApi)
+      expect(BigBlueButton::BigBlueButtonApi).to receive(:new).with(ENV['BIGBLUEBUTTON_ENDPOINT'], ENV['BIGBLUEBUTTON_SECRET'], "1.8")
+      bbb_service.bbb_server
     end
 
     it 'BigBlueButton client initialized once per request' do
-      expect(bbb_server.bbb_server).to eq bbb_api
+      bbb_api = bbb_service.bbb_server
+      bbb_api2 = bbb_service.bbb_server
+      bbb_api3 = bbb_service.bbb_server
+
+      expect(bbb_api).to eq(bbb_api2).and eq(bbb_api3)
     end
   end
 end
