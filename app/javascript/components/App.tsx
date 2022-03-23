189a1595fd1fb84f4f403bb0d8223bf385226b03
kh-amir-tn <amir.khemissi@insat.ucar.tn>
2022-03-23 16:12:25 +0100

    Frontend: 	    - Set a new project structure for the front end JavaScript code influenced by the 	      react-router-demo https://stackblitz.com/edit/github-agqlf5/ : 	      -> main.jsx: An entry point where Routes, Layouts, Global context is provided. 	      -> app.jsx: The parent route component for the `/` endpoint. 	      -> components/: A directory where custom components resides. 	      -> components/styling: A directory where custom components with no business logic resides (i.e. custom buttons). 	      -> components/forms: A directory where custom forms and form related components resides. 	      -> routes: A directory where sub routes resides. 	    - Created a `comonents/styling/buttons/ButtonLink.jsx` component that wraps/connects a react bootstrap button component               with the react router API.             TODO: 	      - Create the signup form.

--- app/javascript/components/App.tsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import Routes from "../routes/Index";
-
-export default props => <>{Routes}</>;
--- app/javascript/components/OtherTest.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-export default () => (
-  <div>
-    <p>This is a test</p>
-    <Link to="/">Test</Link>
-  </div>
-);
