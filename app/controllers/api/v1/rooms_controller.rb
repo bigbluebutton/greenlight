e7164bf861c139229bd42a29f96e07e7dd9ae2b3
Hadi Cheaito <38328371+hadicheaito1@users.noreply.github.com>
2022-03-29 16:18:05 -0400

    View rooms: Back-end changes (#3325)
    * View rooms: Back-end changes
    
    remove empty line
    
    fixes
    
    * Rebase + rspec fixes

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -5,6 +5,19 @@ module Api
     class RoomsController < ApplicationController
       before_action :find_room, only: :show
 
+      # GET /api/v1/rooms.json
+      # Returns: { data: Array[serializable objects(rooms)] , errors: Array[String] }
+      # Does: Returns the Rooms that belong to the user currently logged in
+      def index
+        # Return the rooms that belong to current user
+        rooms = Room.where(user_id: current_user.id)
+
+        render json: {
+          data: rooms,
+          errors: []
+        }, status: :ok
+      end
+
       def show
         if @room
           render json: {
---
+++ spec/controllers/rooms_controller_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Api::V1::RoomsController, type: :controller do
+
+  let(:user) { create(:user)}
+  before do
+    request.headers['ACCEPT'] = 'application/json'
+    session[:user_id] = user.id
+  end
+
+  # TODO: Hadi - Make test work with current_user
+  describe 'index' do
+    it 'ids of rooms in response are matching room ids that belong to current_user' do
+      rooms = create_list(:room, 5, user: user)
+      get :index
+      expect(response).to have_http_status(:ok)
+      response_room_ids = JSON.parse(response.body)['data'].map { |room| room['id']}
+      expect(response_room_ids).to eq(rooms.pluck(:id))
+    end
+
+    it 'no rooms for current_user should return empty list' do
+      get :index
+      expect(response).to have_http_status(:ok)
+      response_room_ids = JSON.parse(response.body)['data'].map { |room| room['id']}
+      expect(response_room_ids).to be_empty
+    end
+  end
+
+  describe '#show' do
+    it 'returns a room if the friendly id is valid' do
+      room = create(:room, user: user)
+      get :show, params: { friendly_id: room.friendly_id }
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)['data']['id']).to eq(room.id)
+    end
+
+    it 'returns not_found if the room doesnt exist' do
+      get :show, params: { friendly_id: 'invalid_friendly_id' }
+      expect(response).to have_http_status(:not_found)
+      expect(JSON.parse(response.body)['data']).to be_empty
+    end
+  end
+end
