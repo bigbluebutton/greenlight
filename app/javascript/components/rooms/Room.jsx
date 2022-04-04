8b6f1c5175eb01ee3e591a1a48986fe5aefad0e1
Hadi Cheaito <hadicheaito1@gmail.com>
2022-04-04 15:54:43 -0400

    cleaning

--- app/javascript/components/rooms/Room.jsx
+++ app/javascript/components/rooms/Room.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
@@ -7,16 +6,6 @@ import ButtonLink from '../stylings/buttons/ButtonLink';
 import FeatureTabs from './FeatureTabs';
 import Spinner from '../stylings/Spinner';
 import useRoom from '../../hooks/queries/rooms/useRoom';
-=======
-import React from "react";
-import {Col, Row} from "react-bootstrap";
-import ButtonLink from "../stylings/buttons/ButtonLink";
-import FeatureTabs from "./FeatureTabs";
-import {Link, useParams} from "react-router-dom";
-import {Spinner} from "../stylings/Spinner"
-import {House} from "react-bootstrap-icons";
-import useRoom from "../../hooks/queries/rooms/useRoom";
->>>>>>> 385308f... Rooms front-end refactoring
 
 export default function Room() {
   const { friendlyId } = useParams();
