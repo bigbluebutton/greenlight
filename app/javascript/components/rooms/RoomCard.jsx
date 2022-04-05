c0d2a916c4203f38d8e5dd58f913d246266a81cc
Hadi Cheaito <38328371+hadicheaito1@users.noreply.github.com>
2022-04-05 16:15:01 -0400

    Rooms front end refactor (#3361)
    * Rooms front-end refactoring
    
    * cleaning
    
    * Styling cleaning/fixing
    
    * CI Changes
    
    * Cleaning

--- app/javascript/components/rooms/RoomCard.jsx
+++ app/javascript/components/rooms/RoomCard.jsx
@@ -10,24 +10,18 @@ export default function RoomCard(props) {
   const handleClick = useCallback(() => { navigate(id); }, [id]);
 
   return (
-    <Container>
-      <Card id="rooms-card" style={{ width: '14rem' }} border="dark">
-        <Card.Body id="room-card-top" onClick={handleClick}>
-          <PersonSquare size={30} />
-          <Card.Title>
-            {' '}
-            {name}
-            {' '}
-          </Card.Title>
-          {/* TODO: Hadi- Make last session dynamic per room */}
-          <Card.Text> Last session... </Card.Text>
-        </Card.Body>
-        <Card.Body>
-          <hr />
-          <Link45deg id="clipboard-icon" size={20} />
-          <ButtonLink className="float-end" to="#"> Start</ButtonLink>
-        </Card.Body>
-      </Card>
-    </Container>
-  );
+    <Card className='rooms-card' border='light'>
+      <Card.Body className='room-card-top pb-0' onClick={handleClick}>
+        <PersonSquare className='mb-4' size={55}/>
+        <Card.Title> {name} </Card.Title>
+        {/* TODO: Hadi- Make last session dynamic per room */}
+        <Card.Text className='text-muted'> Last session... </Card.Text>
+        <hr />
+      </Card.Body>
+      <Card.Body className='pt-0'>
+        <Link45deg size={20}/>
+        <ButtonLink variant="outline-secondary" className='float-end' to='#'> Start</ButtonLink>
+      </Card.Body>
+    </Card>
+  )
 }
--- app/javascript/components/rooms/Rooms.jsx
+++ app/javascript/components/rooms/Rooms.jsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import {
-  Row, Col, Container, Spinner,
-} from 'react-bootstrap';
-import RoomCard from './RoomCard';
-import useRooms from '../../hooks/queries/rooms/useRooms';
+import React from "react";
+import { Container } from "react-bootstrap";
+import RoomsTabs from "./RoomsTabs";
 
 export default function Rooms() {
-  const { isLoading, data: rooms } = useRooms();
-  if (isLoading) return <Spinner />;
 
   return (
     <>
-      <h1>Rooms:</h1>
-      <Container className="bg-secondary">
-        <Row md={4} className="g-4">
-          {rooms.map((room) => (
-            <Col key={room.friendly_id}>
-              <RoomCard id={room.friendly_id} name={room.name}> </RoomCard>
-            </Col>
-          ))}
-        </Row>
+      <Container>
+        <RoomsTabs />
       </Container>
     </>
-  );
+  )
 }
