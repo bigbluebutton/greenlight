3c4b50450a1195d1d0641569e4ae3e842ce86552
farhatahmad <ahmad.af.farhat@gmail.com>
2022-04-06 13:08:31 -0400

    Ignored no-spreading rule

--- app/javascript/components/forms/Form.jsx
+++ app/javascript/components/forms/Form.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
 import React, { useCallback } from 'react';
 import { FormProvider } from 'react-hook-form';
 import { Form as BootStrapForm } from 'react-bootstrap';
--- app/javascript/components/forms/FormControl.jsx
+++ app/javascript/components/forms/FormControl.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
 import React from 'react';
 import { Form as BootStrapForm } from 'react-bootstrap';
 import { useFormContext } from 'react-hook-form';
