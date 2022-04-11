a73f210f008060693d50414e202370ea21208e95
farhatahmad <ahmad.af.farhat@gmail.com>
2022-04-11 16:54:47 -0400

    Made room view a bit nicer

--- app/javascript/components/rooms/Room.jsx
+++ app/javascript/components/rooms/Room.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { Button, Col, Row } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
-import { House } from 'react-bootstrap-icons';
+import { HouseDoor, Link45deg } from 'react-bootstrap-icons';
 import ButtonLink from '../stylings/buttons/ButtonLink';
 import FeatureTabs from './FeatureTabs';
 import Spinner from '../stylings/Spinner';
 import useRoom from '../../hooks/queries/rooms/useRoom';
+
+function copyInvite() {
+  navigator.clipboard.writeText(`${window.location}/join`);
+}
 
 export default function Room() {
   const { friendlyId } = useParams();
@@ -15,19 +19,26 @@ export default function Room() {
 
   return (
     <>
-      <Row>
+      <Row className="mt-4">
         <Col>
           <Link to="/rooms">
-            <House />
+            <HouseDoor size={24} />
           </Link>
         </Col>
       </Row>
       <Row className="my-5">
         <Col>
-          { room.name }
+          <h2>{ room.name }</h2>
+          <p className="text-muted">Last Session: Jan 17,2022 11:30am</p>
         </Col>
         <Col>
-          <ButtonLink to="/" variant="primary" className="float-end">Start Session</ButtonLink>
+          <ButtonLink to="/" variant="primary" className="mt-1 mx-2 float-end">
+            Start Session
+          </ButtonLink>
+          <Button variant="light" className="mt-1 mx-2 float-end" onClick={copyInvite}>
+            <Link45deg />
+            Copy
+          </Button>
         </Col>
       </Row>
       <FeatureTabs />
