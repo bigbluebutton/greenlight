af7ab5a9d23235874eb80117284800a3619d0fe2
Khemissi Amir <amir.khemissi@insat.ucar.tn>
2022-04-12 17:04:54 +0100

    Backend: Added the room session start feature. (#3355)

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -42,4 +42,26 @@ RSpec.describe Api::V1::RoomsController, type: :controller do
       expect(JSON.parse(response.body)['data']).to be_empty
     end
   end
+
+  describe '#start_meeting' do
+    let(:join_url) { 'https://test.com/bigbluebutton/api?join' }
+    let(:bbb_service) { instance_double(BigBlueButtonApi) }
+
+    before do
+      allow(BigBlueButtonApi).to receive(:new).and_return(bbb_service)
+      allow(bbb_service).to receive(:start_meeting).and_return(join_url)
+    end
+
+    it 'returns the join_url for existent room' do
+      room = create(:room, user:)
+      post :start, params: { friendly_id: room.friendly_id }
+      expect(response).to have_http_status(:created)
+      expect(JSON.parse(response.body)['data']['join_url']).to eq(join_url)
+    end
+
+    it 'returns not_found if the room doesn\'t exist' do
+      post :start, params: { friendly_id: 'invalid_friendly_id' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
 end
