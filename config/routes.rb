52e33c61597b0d558b77ac26abf1a17d4b2d49c9
Khemissi Amir <amir.khemissi@insat.ucar.tn>
2022-04-14 17:10:52 +0100

    Backend: Added create room capability for authenticated users. (#3387)

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
         end
       end
       resources :users, only: [:create]
-      resources :rooms, only: %i[show index], param: :friendly_id do
+      resources :rooms, only: %i[show index create], param: :friendly_id do
         post '/start', to: 'rooms#start', as: :start_meeting, on: :member
       end
     end
