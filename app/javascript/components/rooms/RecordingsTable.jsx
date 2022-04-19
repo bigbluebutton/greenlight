fb93efa729b51fbe71b7acfbdab07d5970f791da
Hadi Cheaito <hadicheaito1@gmail.com>
2022-04-19 12:15:31 -0400

    eslint

--- app/javascript/components/rooms/RecordingsTable.jsx
+++ app/javascript/components/rooms/RecordingsTable.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Table, Card } from 'react-bootstrap';
-import Spinner from '../shared/stylings/Spinner';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faVideo } from '@fortawesome/free-solid-svg-icons';
+import Spinner from '../shared/stylings/Spinner';
 import useRecordings from '../../hooks/queries/recordings/useRecordings';
 
 export default function RecordingsTable() {
