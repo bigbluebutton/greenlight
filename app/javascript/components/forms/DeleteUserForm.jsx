05222247103d5e9d4b73d0f1d07ba70d5ad844a6
scouillard <couillard.samuel@gmail.com>
2022-04-20 15:05:40 -0400

    Add isSubmitting from React Hook Form to disable button on submitting

--- app/javascript/components/forms/DeleteUserForm.jsx
+++ app/javascript/components/forms/DeleteUserForm.jsx
@@ -6,16 +6,19 @@ import {
 import Form from './Form';
 import useDeleteUser from '../../hooks/mutations/users/useDeleteUser';
 import { useAuth } from '../../contexts/auth/AuthProvider';
+import Spinner from '../shared/stylings/Spinner';
 
 export default function DeleteUserForm() {
   const currentUser = useAuth();
   const methods = useForm();
+  const { isSubmitting } = methods.formState;
   const { onSubmit } = useDeleteUser(currentUser?.id);
 
   return (
     <Form methods={methods} onSubmit={onSubmit}>
-      <Button variant="danger" type="submit">
+      <Button variant="danger" type="submit" disabled={isSubmitting}>
         Delete
+        { isSubmitting && <Spinner /> }
       </Button>
     </Form>
   );
