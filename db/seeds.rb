4b3d7f8bb678aa89f85a2be33e528dbdbbd5d19c
kh-amir-tn <amir.khemissi@insat.ucar.tn>
2022-04-20 14:58:29 +0100

    Backend: Created the meeting_options seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,48 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+MEETNG_OPTS = [
+  # BBB options, for a full list check https://docs.bigbluebutton.org/dev/api.html#create:
+  {
+    name: 'record',
+    value: 'false' # true|false
+  },
+  {
+    name: 'muteOnStart',
+    value: 'false' # true|false
+  },
+  {
+    name: 'guestPolicy',
+    value: 'ALWAYS_ACCEPT' # ALWAYS_ACCEPT | ALWAYS_DENY | ASK_MODERATOR
+  },
+  {
+    name: 'attendeePW',
+    value: ''
+  },
+  {
+    name: 'moderatorPW',
+    value: ''
+  },
+  # GL only options:
+  {
+    name: 'gl_anyone_can_start',
+    value: 'false' # true | false
+  },
+  {
+    name: 'gl_anyone_join_as_moderator',
+    value: 'false' # true | false
+  },
+  {
+    name: 'gl_moderator_access_code',
+    value: ''
+  },
+  {
+    name: 'gl_attendee_access_code',
+    value: ''
+  }
+].freeze
+
+# rubocop:disable Rails/SkipsModelValidations
+MeetingOption.insert_all! MEETNG_OPTS
+# rubocop:enable Rails/SkipsModelValidations
