73d83cdfac287bd03b564a0e8691dbd82d08efcd
scouillard <couillard.samuel@gmail.com>
2022-04-28 16:45:50 -0400

    Frontend: Add Remove Avatar feature

---
+++ app/javascript/components/forms/DeleteAvatarForm.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import {
+  Button,
+} from 'react-bootstrap';
+import Form from './Form';
+import useDeleteAvatar from '../../hooks/mutations/users/useDeleteAvatar';
+import { useAuth } from '../../contexts/auth/AuthProvider';
+
+export default function DeleteUserForm() {
+  const currentUser = useAuth();
+  const methods = useForm();
+  const { onSubmit } = useDeleteAvatar(currentUser);
+
+  return (
+    <Form methods={methods} onSubmit={onSubmit}>
+      <button className="btn btn-link float-end" type="submit">
+        Delete Avatar
+      </button>
+    </Form>
+  );
+}
