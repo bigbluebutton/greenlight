332191f581e686780001ca3369f42fefa456b4f6
Hadi Cheaito <hadicheaito1@gmail.com>
2022-04-29 12:38:11 -0400

    Moving delete room button

--- app/javascript/components/rooms/RoomSettings.jsx
+++ app/javascript/components/rooms/RoomSettings.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Row, Button, Col, } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import Spinner from '../shared/stylings/Spinner';
+import useDeleteRoom from '../../hooks/mutations/rooms/useDeleteRoom';
 
 export default function RoomSettings() {
+  const { friendlyId } = useParams();
+  const { handleDeleteRoom, isLoading: deleteRoomIsLoading } = useDeleteRoom(friendlyId);
+
   return (
-    <div className="wide-background full-height-room">
-      <p>Room Settings</p>
-    </div>
+    <Row className="wide-background">
+      <Col>
+      </Col>
+      <Col>
+        {/* TODO: Hadi- This is temporary (waiting to see where the delete button should be for room) */}
+        <Button id="delete-room" className="mt-1 mx-2 float-end" onClick={handleDeleteRoom}>
+          Delete Room
+          {deleteRoomIsLoading && <Spinner />}
+        </Button>
+      </Col>
+    </Row>
   );
 }
