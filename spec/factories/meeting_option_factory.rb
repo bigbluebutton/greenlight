f0fbcb25a3c74faef0538e650cdd36a0559cfa8a
farhatahmad <ahmad.af.farhat@gmail.com>
2022-05-04 10:24:33 -0400

    Reverted backend to create room modal

--- spec/factories/meeting_option_factory.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :meeting_option do
-    name { Faker::Types.unique.rb_string }
-    default_value { Faker::Types.rb_string }
-  end
-end
