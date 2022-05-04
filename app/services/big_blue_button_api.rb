b4888971ea8024be35372476ef2c77011482f74d
Ahmad Farhat <ahmad.af.farhat@gmail.com>
2022-05-04 10:28:26 -0400

    Reverted backend to create room modal (#3434)

--- app/services/big_blue_button_api.rb
+++ app/services/big_blue_button_api.rb
@@ -14,14 +14,11 @@ class BigBlueButtonApi
 
   # Start a meeting for a specific room and returns the join URL.
   def start_meeting(room:, meeting_starter:, options: {})
-    # TODO: amir - Revisit this.
-    room_meeting_option_name_values_hash = room.room_meeting_options.includes(:meeting_option).where.not('name LIKE ?', 'gl%').pluck(:name,
-                                                                                                                                     :value).to_h
-    create_options = room_meeting_option_name_values_hash.merge(options)
+    create_options = default_create_opts.merge(options)
     join_options = { join_via_html5: true } # TODO: amir - Revisit this (createTime,...).
 
     user_name = meeting_starter&.name || 'Someone'
-    password = (meeting_starter && create_options['moderatorPW']) || create_options['attendeePW']
+    password = (meeting_starter && create_options[:moderatorPW]) || create_options[:attendeePW]
 
     bbb_server.create_meeting room.name, room.friendly_id, create_options
     bbb_server.join_meeting_url room.friendly_id, user_name, password, join_options
@@ -35,5 +32,21 @@ class BigBlueButtonApi
 
   def bbb_secret
     ENV.fetch 'BIGBLUEBUTTON_SECRET', '8cd8ef52e8e101574e400365b55e11a6'
+  end
+
+  def default_create_opts
+    {
+      # TODO: amir - revisit this.
+      record: true,
+      logoutURL: 'http://localhost',
+      moderatorPW: 'mp',
+      attendeePW: 'ap',
+      moderatorOnlyMessage: 'Welcome Moderator',
+      muteOnStart: false,
+      guestPolicy: 'ALWAYS_ACCEPT',
+      'meta_gl-v3-listed': 'public',
+      'meta_bbb-origin-version': 3,
+      'meta_bbb-origin': 'Greenlight'
+    }
   end
 end
