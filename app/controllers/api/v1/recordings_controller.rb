a176380e45b77cac814c528a7c6ec603a4a497ab
Hadi Cheaito <hadicheaito1@gmail.com>
2022-05-04 16:24:42 -0400

    n+1 recording formats

--- app/controllers/api/v1/recordings_controller.rb
+++ app/controllers/api/v1/recordings_controller.rb
@@ -7,7 +7,7 @@ module Api
       # Returns: { data: Array[serializable objects(recordings)] , errors: Array[String] }
       # Does: Returns all the Recordings from all the current user's rooms
       def index
-        recordings = current_user&.recordings
+        recordings = current_user&.recordings.includes(:formats)
 
         render_json(data: recordings, status: :ok, include: :formats)
       end
