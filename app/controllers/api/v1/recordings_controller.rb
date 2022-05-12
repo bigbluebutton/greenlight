45639ce465e80c6a338b3d9f07386f0fbbbbc23b
Hadi Cheaito <hadicheaito1@gmail.com>
2022-05-12 14:37:01 -0400

    Back-end: Recordings Re-Sync

--- app/controllers/api/v1/recordings_controller.rb
+++ app/controllers/api/v1/recordings_controller.rb
@@ -3,6 +3,8 @@
 module Api
   module V1
     class RecordingsController < ApplicationController
+      skip_before_action :verify_authenticity_token # TODO: - Revisit this.
+
       # GET /api/v1/recordings.json
       # Returns: { data: Array[serializable objects(recordings)] , errors: Array[String] }
       # Does: Returns all the Recordings from all the current user's rooms
@@ -10,6 +12,13 @@ module Api
         recordings = current_user.recordings&.includes(:formats)
 
         render_json(data: recordings, status: :ok, include: :formats)
+      end
+
+      def recordings
+        recording_sync = RecordingSync.new
+        recording_sync.recording_resync(user: current_user)
+
+        render_json(status: :ok)
       end
     end
   end
---
+++ app/services/recording_sync.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+class RecordingSync
+  def initialize; end
+
+  def recording_resync(user:)
+    bbb_api = BigBlueButtonApi.new
+    rooms = user.rooms
+
+    # Get the meeting_id of all the current user's rooms
+    meeting_ids = rooms.map(&:meeting_id)
+
+    # Clear current recordings
+    Recording.destroy_by(room_id: rooms.map(&:id))
+
+    # Get recordings
+    recordings = bbb_api.get_recordings(meeting_ids:)
+
+    # Create new recordings
+    recordings[:recordings].each do |recording|
+      room_id = Room.find_by(meeting_id: recording[:meetingID]).id
+
+      # Get length of presentation format(s)
+      length = get_recording_length(recording:)
+
+      # Create recording
+      new_recording = Recording.create(room_id:, name: recording[:name], record_id: recording[:recordID], visibility: 'Unlisted',
+                                       users: recording[:participants], length:)
+
+      # Create format(s)
+      create_formats(recording:, new_recording:)
+    end
+  end
+
+  private
+
+  # Returns the length of presentation recording for the recording given
+  def get_recording_length(recording:)
+    length = 0
+    if recording[:playback][:format].is_a?(Array)
+      recording[:playback][:format].each do |formats|
+        length = formats[:length] if formats[:type] == 'presentation'
+      end
+    else
+      length = recording[:playback][:format][:length]
+    end
+    length
+  end
+
+  # Creates format(s) for given new_recording
+  def create_formats(recording:, new_recording:)
+    if recording[:playback][:format].is_a?(Array)
+      recording[:playback][:format].each do |format|
+        Format.create(recording_id: new_recording.id, recording_type: format[:type], url: format[:url])
+      end
+    else
+      Format.create(recording_id: new_recording.id, recording_type: recording[:playback][:format][:type], url: recording[:playback][:format][:url])
+    end
+  end
+end
