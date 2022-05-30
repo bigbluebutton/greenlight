b4a8b443bc8ebd5f1809481610c7e22ddfc04339
farhatahmad <ahmad.af.farhat@gmail.com>
2022-05-30 14:20:59 -0400

    Remove unneeded ssl stuff

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-require 'openssl'
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer
 
