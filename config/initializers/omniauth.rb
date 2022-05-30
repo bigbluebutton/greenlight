ee64f36c761cd1456b649f8f89a740d63dedcc45
farhatahmad <ahmad.af.farhat@gmail.com>
2022-05-30 14:28:54 -0400

    More fixes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer
+  issuer = ENV.fetch('OPENID_CONNECT_ISSUER', '')
 
-  provider :openid_connect,
-           issuer: ENV['OPENID_CONNECT_ISSUER'],
-           scope: %i[openid email profile],
-           uid_field: ENV['OPENID_CONNECT_UID_FIELD'] || 'preferred_username',
-           discovery: true,
-           client_options: {
-             identifier: ENV['OPENID_CONNECT_CLIENT_ID'],
-             secret: ENV['OPENID_CONNECT_CLIENT_SECRET'],
-             redirect_uri: File.join(ENV['OPENID_CONNECT_REDIRECT'], 'auth', 'openid_connect', 'callback')
-           }
+  if issuer.present?
+    provider :openid_connect,
+             issuer:,
+             scope: %i[openid email profile],
+             uid_field: ENV.fetch('OPENID_CONNECT_UID_FIELD', 'preferred_username'),
+             discovery: true,
+             client_options: {
+               identifier: ENV.fetch('OPENID_CONNECT_CLIENT_ID'),
+               secret: ENV.fetch('OPENID_CONNECT_CLIENT_SECRET'),
+               redirect_uri: File.join(ENV.fetch('OPENID_CONNECT_REDIRECT', ''), 'auth', 'openid_connect', 'callback')
+             }
+  end
 end
