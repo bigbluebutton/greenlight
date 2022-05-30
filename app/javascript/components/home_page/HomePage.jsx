543c09c61f6fac94a5330b8bd10f01208d04f955
farhatahmad <ahmad.af.farhat@gmail.com>
2022-05-30 14:17:48 -0400

    Eslint

--- app/javascript/components/home_page/HomePage.jsx
+++ app/javascript/components/home_page/HomePage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import { Form } from 'react-bootstrap';
 import ButtonLink from '../shared/stylings/buttons/ButtonLink';
-import {Form} from "react-bootstrap";
 
 export default function HomePage() {
   return (
     <>
       <ButtonLink to="/signin" className="mx-2">Sign In</ButtonLink>
       <ButtonLink to="/signup" variant="outline-primary">Sign Up</ButtonLink>
-      <Form action="/auth/openid_connect" method="POST" data-turbo="false" >
-        <input type="hidden" name="authenticity_token" value={document.querySelector('meta[name="csrf-token"]').content}/>
+      <Form action="/auth/openid_connect" method="POST" data-turbo="false">
+        <input type="hidden" name="authenticity_token" value={document.querySelector('meta[name="csrf-token"]').content} />
         <input type="submit" value="Submit" />
       </Form>
     </>
