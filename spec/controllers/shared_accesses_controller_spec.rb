be38a9c9592dfa96549218bdafdfdafb5fd4dcbf
scouillard <couillard.samuel@gmail.com>
2022-06-01 12:51:33 -0400

    Add RSpec

--- spec/controllers/shared_accesses_controller_spec.rb
+++ spec/controllers/shared_accesses_controller_spec.rb
@@ -27,29 +27,52 @@ RSpec.describe Api::V1::SharedAccessesController, type: :controller do
   end
 
   describe '#show' do
-    it 'lists the users that the room has been shared to' do
+    it 'returns all the users that the room has been shared to' do
       room = create(:room)
       shared_users = create_list(:user, 5)
       unshared_users = create_list(:user, 5)
       room.shared_users = shared_users
 
-      get :show, params: { friendly_id: room.friendly_id }
+      get :show, params: { friendly_id: room.friendly_id, search: '' }
       shared_user_ids = JSON.parse(response.body)['data'].map { |user| user['id'] }
       expect(shared_user_ids).to eql(shared_users.pluck(:id))
       expect(shared_user_ids).not_to include(unshared_users.pluck(:id))
     end
+
+    it 'returns the shared users according to the query' do
+      room = create(:room)
+      room.shared_users = create_list(:user, 5)
+      searched_users = create_list(:user, 5, name: 'John Doe')
+      room.shared_users << searched_users
+
+      get :show, params: { friendly_id: room.friendly_id, search: 'John Doe' }
+      response_users_ids = JSON.parse(response.body)['data'].map { |user| user['id'] }
+      expect(response_users_ids).to match_array(searched_users.pluck(:id))
+    end
+
+
   end
 
   describe '#shareable_users' do
-    it 'lists the users that the room can be shared to' do
+    it 'returns the users that the room can be shared to' do
       room = create(:room)
       shared_users = create_list(:user, 5)
       shareable_users = create_list(:user, 5)
       room.shared_users = shared_users
 
-      get :shareable_users, params: { friendly_id: room.friendly_id }
-      shareable_user_ids = JSON.parse(response.body)['data'].map { |user| user['id'] }
-      expect(shareable_user_ids).to eql(shareable_users.pluck(:id))
+      get :shareable_users, params: { friendly_id: room.friendly_id, search: '' }
+      response_users_ids = JSON.parse(response.body)['data'].map { |user| user['id'] }
+      expect(response_users_ids).to eql(shareable_users.pluck(:id))
+    end
+
+    it 'returns the shareable users according to the query' do
+      room = create(:room)
+      room.shared_users = create_list(:user, 5)
+      shareable_users = create_list(:user, 5, name: 'Jane Doe')
+
+      get :shareable_users, params: { friendly_id: room.friendly_id, search: 'Jane Doe' }
+      response_users_ids = JSON.parse(response.body)['data'].map { |user| user['id'] }
+      expect(response_users_ids).to eql(shareable_users.pluck(:id))
     end
   end
 end
