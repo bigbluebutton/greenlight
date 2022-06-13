d514073eff0e51444f88948c62d7e1eba2ce3a29
Hadi Cheaito <38328371+hadicheaito1@users.noreply.github.com>
2022-06-13 16:18:12 -0400

    Preupload presentation Front End click to upload (#3528)
    * CR changes
    
    * cleaning
    
    * Pre-upload Presentation Click to Upload front end
    
    * fix

--- app/javascript/components/rooms/Presentation.jsx
+++ app/javascript/components/rooms/Presentation.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Button, Card } from 'react-bootstrap';
+import React from 'react';
+import { Card } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCloudArrowUp } from '@fortawesome/free-solid-svg-icons';
 import { useParams } from 'react-router-dom';
@@ -8,7 +8,6 @@ import useRoom from '../../hooks/queries/rooms/useRoom';
 
 export default function Presentation() {
   const { friendlyId } = useParams();
-  const [presentation, setPresentation] = useState();
   const { data: room } = useRoom(friendlyId);
   const { onSubmit } = useUploadPresentation(friendlyId);
 
@@ -20,16 +19,21 @@ export default function Presentation() {
             <div className="user-icon-circle rounded-circle d-block mx-auto mb-3">
               <FontAwesomeIcon icon={faCloudArrowUp} className="fa-4x text-primary d-block mx-auto pt-3" />
             </div>
-            <Card.Title className="text-primary"> Click to upload or drag and drop </Card.Title>
+            <Card.Title className="text-primary">
+              <label htmlFor="file" className="presentation-upload">Click to Upload
+                <input
+                  id="file"
+                  className="d-none"
+                  type="file"
+                  onChange={(e) => onSubmit(e.target.files[0])}
+                  accept=".doc,.docx,.pptx,.txt,.png,.jpg,.pdf"
+                />
+              </label>
+            </Card.Title>
             <Card.Text>
               Upload any office document or PDF file. Depending on the size of the
               presentation, it may require additional time to upload before it can be used
             </Card.Text>
-            {/* TODO: Hadi - Change what is accepted as a file */}
-            <input type="file" onChange={(e) => setPresentation(e.target.files[0])} accept="*" />
-            <Button variant="primary" className="w-100 my-3 py-2" onClick={() => onSubmit(presentation)}>
-              Upload Presentation
-            </Button>
           </Card.Body>
         </Card>
       </div>
