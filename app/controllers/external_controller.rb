798122b55a2f2d8a7843ac6941257845a4c3b1f7
kh-amir-tn <amir.khemissi@insat.ucar.tn>
2022-06-14 09:25:58 +0100

    Localization: Extended user_controller and external_controller actions to set language on creation.

--- app/controllers/external_controller.rb
+++ app/controllers/external_controller.rb
@@ -4,12 +4,12 @@ class ExternalController < ApplicationController
   def create
     credentials = request.env['omniauth.auth']
     user_info = credentials['info']
-
     user = User.find_or_create_by(email: user_info['email']) do |u|
       u.external_id = credentials['uid']
       u.name = user_info['name']
       u.provider = 'greenlight'
       u.role = Role.find_by(name: 'User') # TODO: - Ahmad: Move to service
+      u.language = extract_language_code user_info['locale']
     end
 
     session[:user_id] = user.id
@@ -17,5 +17,11 @@ class ExternalController < ApplicationController
     # TODO: - Ahmad: deal with errors
 
     redirect_to '/rooms'
+  end
+
+  private
+
+  def extract_language_code(locale)
+    locale.try(:scan, /^[a-z]{2}/)&.first || I18n.default_locale
   end
 end
