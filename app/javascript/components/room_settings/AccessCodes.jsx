f04947e1c6c990ba27199f3203530b0654a45af6
scouillard <samuel.couillard@blindsidenetworks.com>
2022-06-21 13:57:34 -0400

    Rework AccessCode generator frontend

--- app/javascript/components/room_settings/AccessCodes.jsx
+++ app/javascript/components/room_settings/AccessCodes.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Button, Row } from 'react-bootstrap';
+import { Button, Row, Stack } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt, faCopy } from '@fortawesome/free-regular-svg-icons';
 import useGenerateAccessCode from '../../hooks/mutations/rooms/useGenerateAccessCode';
 import useDeleteAccessCode from '../../hooks/mutations/rooms/useDeleteAccessCode';
 import useAccessCodes from '../../hooks/queries/rooms/useAccessCodes';
+
+const copyAccessCode = (accessCode) => navigator.clipboard.writeText(accessCode);
 
 export default function AccessCodes() {
   const { friendlyId } = useParams();
@@ -13,49 +17,77 @@ export default function AccessCodes() {
 
   return (
     <>
-      <Row>
+      <Row className="my-3">
         <h6 className="text-primary">Generate access code for viewers</h6>
-        <div>
-          <Button
-            variant="primary-light"
-            onClick={() => handleGenerateAccessCode('Viewer')}
-          >
-            Generate
-          </Button>
-        </div>
         {
           accessCodes?.viewer_access_code
+            ? (
+              <Stack direction="horizontal">
+                <div className="access-code-input w-100">
+                  <input type="text" className="form-control" value={accessCodes.viewer_access_code} />
+                  <Button
+                    variant="font-awesome"
+                    onClick={() => copyAccessCode(accessCodes.viewer_access_code)}
+                  >
+                    <FontAwesomeIcon icon={faCopy} />
+                  </Button>
+                </div>
+                <Button
+                  className="px-3"
+                  variant="font-awesome"
+                  onClick={() => handleDeleteAccessCode('Viewer')}
+                >
+                  <FontAwesomeIcon icon={faTrashAlt} />
+                </Button>
+              </Stack>
+            )
+            : (
+              <div>
+                <Button
+                  variant="primary-light"
+                  onClick={() => handleGenerateAccessCode('Viewer')}
+                >
+                  Generate
+                </Button>
+              </div>
+            )
         }
-        <div>
-          <Button
-            variant="danger"
-            onClick={() => handleDeleteAccessCode('Viewer')}
-          >
-            Remove
-          </Button>
-        </div>
       </Row>
       <Row>
         <h6 className="text-primary">Generate access code for moderators</h6>
-        <div>
-          <Button
-            variant="primary-light"
-            onClick={() => handleGenerateAccessCode('Moderator')}
-          >
-            Generate
-          </Button>
-        </div>
         {
           accessCodes?.moderator_access_code
+            ? (
+              <Stack direction="horizontal">
+                <div className="access-code-input w-100">
+                  <input type="text" className="form-control" value={accessCodes.moderator_access_code} />
+                  <Button
+                    variant="font-awesome"
+                    onClick={() => copyAccessCode(accessCodes.moderator_access_code)}
+                  >
+                    <FontAwesomeIcon icon={faCopy} />
+                  </Button>
+                </div>
+                <Button
+                  className="px-3"
+                  variant="font-awesome"
+                  onClick={() => handleDeleteAccessCode('Moderator')}
+                >
+                  <FontAwesomeIcon icon={faTrashAlt} />
+                </Button>
+              </Stack>
+            )
+            : (
+              <div>
+                <Button
+                  variant="primary-light"
+                  onClick={() => handleGenerateAccessCode('Moderator')}
+                >
+                  Generate
+                </Button>
+              </div>
+            )
         }
-        <div>
-          <Button
-            variant="danger"
-            onClick={() => handleDeleteAccessCode('Moderator')}
-          >
-            Remove
-          </Button>
-        </div>
       </Row>
     </>
   );
