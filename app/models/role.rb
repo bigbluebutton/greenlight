11df062b2ec9565165158c6649b9a2ba1b11c00c
kh-amir-tn <amir.khemissi@insat.ucar.tn>
2022-06-23 18:19:29 +0100

    Create Role: Added color attr to Role model and random color generation on create.

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,4 +4,12 @@ class Role < ApplicationRecord
   has_many :users, dependent: :restrict_with_exception
 
   validates :name, presence: true
+
+  before_validation :set_random_color, on: :create
+
+  private
+
+  def set_random_color
+    self.color = "##{SecureRandom.hex(3)}"
+  end
 end
--- db/migrate/20220531180853_create_roles.rb
+++ db/migrate/20220531180853_create_roles.rb
@@ -4,6 +4,7 @@ class CreateRoles < ActiveRecord::Migration[7.0]
   def change
     create_table :roles do |t|
       t.string :name, null: false
+      t.string :color, default: '', null: false
       t.timestamps
     end
   end
