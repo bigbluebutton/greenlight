67e55aa7cda60cfa84cc875e7ea6746d54abb31f
Samuel Couillard <43917914+scouillard@users.noreply.github.com>
2022-06-30 13:46:37 -0400

    Change color (#3600)
    quickfix

--- app/javascript/components/admin/shared/AdminNavSideBar.jsx
+++ app/javascript/components/admin/shared/AdminNavSideBar.jsx
@@ -9,38 +9,38 @@ export default function AdminNavSideBar() {
   return (
     <Nav variant="pills" className="flex-column">
       <Nav.Item>
-        <Nav.Link className="cursor-pointer" as={Link} to="/adminpanel/users" eventKey="users">
-          <UsersIcon className="hi-s text-primary me-3" />
+        <Nav.Link className="cursor-pointer text-muted" as={Link} to="/adminpanel/users" eventKey="users">
+          <UsersIcon className="hi-s me-3" />
           Manage Users
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link className="cursor-pointer" as={Link} to="/adminpanel/server-rooms" eventKey="server-rooms">
-          <ServerIcon className="hi-s text-primary me-3" />
+        <Nav.Link className="cursor-pointer text-muted" as={Link} to="/adminpanel/server-rooms" eventKey="server-rooms">
+          <ServerIcon className="hi-s me-3" />
           Server Rooms
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link className="cursor-pointer" as={Link} to="/adminpanel/server-recordings" eventKey="server-recordings">
-          <VideoCameraIcon className="hi-s text-primary me-3" />
+        <Nav.Link className="cursor-pointer text-muted" as={Link} to="/adminpanel/server-recordings" eventKey="server-recordings">
+          <VideoCameraIcon className="hi-s me-3" />
           Server Recordings
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link className="cursor-pointer" as={Link} to="/adminpanel/site-settings" eventKey="site-settings">
-          <CogIcon className="hi-s text-primary me-3" />
+        <Nav.Link className="cursor-pointer text-muted" as={Link} to="/adminpanel/site-settings" eventKey="site-settings">
+          <CogIcon className="hi-s me-3" />
           Site Settings
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link className="cursor-pointer" as={Link} to="/adminpanel/room-configuration" eventKey="room-configuration">
-          <AdjustmentsIcon className="hi-s text-primary me-3" />
+        <Nav.Link className="cursor-pointer text-muted" as={Link} to="/adminpanel/room-configuration" eventKey="room-configuration">
+          <AdjustmentsIcon className="hi-s me-3" />
           Room Configuration
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link className="cursor-pointer" as={Link} to="/adminpanel/roles" eventKey="roles">
-          <IdentificationIcon className="hi-s text-primary me-3" />
+        <Nav.Link className="cursor-pointer text-muted" as={Link} to="/adminpanel/roles" eventKey="roles">
+          <IdentificationIcon className="hi-s me-3" />
           Roles
         </Nav.Link>
       </Nav.Item>
