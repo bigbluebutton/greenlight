3af09ee91d4226591565dc79329ac29f428b1689
Hadi Cheaito <38328371+hadicheaito1@users.noreply.github.com>
2022-07-04 12:32:23 -0400

    Cursor Pointer Delete Pres + upload pres (#3603)
    * Cursor Pointer Delete Pres
    
    * Cursor for upload pres

--- app/javascript/components/rooms/Presentation.jsx
+++ app/javascript/components/rooms/Presentation.jsx
@@ -29,7 +29,7 @@ export default function Presentation() {
         >
           <Card className="border-0 shadow-sm mt-3 text-center">
             <label htmlFor="file" className="presentation-upload">
-              <Card.Body className="py-5 text-secondary">
+              <Card.Body className="py-5 text-secondary cursor-pointer">
                 <div className="icon-circle rounded-circle d-block mx-auto mb-3">
                   <CloudUploadIcon className="hi-l text-primary d-block mx-auto pt-3" />
                 </div>
@@ -72,7 +72,7 @@ export default function Presentation() {
             <Col />
             <Col>
               <Modal
-                modalButton={<TrashIcon className="hi-s" />}
+                modalButton={<TrashIcon className="cursor-pointer hi-s" />}
                 title="Are you sure?"
                 body={<DeletePresentationForm />}
               />
