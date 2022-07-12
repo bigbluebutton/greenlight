0beb5ecbcb8f9595791edf5f2abf08f3e2484322
Khemissi Amir <amir.khemissi@insat.ucar.tn>
2022-07-12 15:05:24 +0100

    Server Recordings: Added `Admin::ServerRecordingsController#index` API. (#3634)
    Co-authored-by: Ahmad Farhat <ahmad.af.farhat@gmail.com>

---
+++ spec/controllers/admin/server_recordings_controller_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Api::V1::Admin::ServerRecordingsController, type: :controller do
+  before { request.headers['ACCEPT'] = 'application/json' }
+
+  describe '#index' do
+    it 'returns the list of recordings' do
+      recordings = [create(:recording, name: 'Tinky-Winky'), create(:recording, name: 'Dipsy'), create(:recording, name: 'Laa-Laa')]
+
+      get :index
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)['data'].pluck('id')).to match_array(recordings.pluck(:id))
+    end
+
+    it 'returns the recordings according to the query' do
+      search_recordings = [create(:recording, name: 'Recording 1'), create(:recording, name: 'Recording 2'), create(:recording, name: 'Recording 3')]
+
+      get :index, params: { search: 'recording' }
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)['data'].pluck('id')).to match_array(search_recordings.pluck(:id))
+    end
+
+    it 'returns all recordings if the search query is empty' do
+      recordings = create_list(:recording, 5)
+
+      get :index, params: { search: '' }
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)['data'].pluck('id')).to match_array(recordings.pluck(:id))
+    end
+
+    context 'ordering' do
+      before do
+        create(:recording, name: 'O')
+        create(:recording, name: 'O')
+        create(:recording, name: 'P')
+      end
+
+      it 'orders the recordings list by column and direction DESC' do
+        get :index, params: { sort: { column: 'name', direction: 'DESC' } }
+        expect(response).to have_http_status(:ok)
+        # Order is important match_array isn't adequate for this test.
+        expect(JSON.parse(response.body)['data'].pluck('name')).to eq(%w[P O O])
+      end
+
+      it 'orders the recordings list by column and direction ASC' do
+        get :index, params: { sort: { column: 'name', direction: 'ASC' } }
+        expect(response).to have_http_status(:ok)
+        # Order is important match_array isn't adequate for this test.
+        expect(JSON.parse(response.body)['data'].pluck('name')).to eq(%w[O O P])
+      end
+    end
+  end
+end
