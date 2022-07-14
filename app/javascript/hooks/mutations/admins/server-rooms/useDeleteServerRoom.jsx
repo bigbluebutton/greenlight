9ee625245f8cc79f69cfcb9bf6772afa839a8a82
kh-amir-tn <amir.khemissi@insat.ucar.tn>
2022-07-14 16:48:14 +0100

    Server Rooms: Integrated with `Admin::ServerRoomsController#destroy`.

---
+++ app/javascript/hooks/mutations/admins/server-rooms/useDeleteServerRoom.jsx
@@ -0,0 +1,21 @@
+import { useMutation, useQueryClient } from 'react-query';
+import { toast } from 'react-hot-toast';
+import axios from '../../../../helpers/Axios';
+
+export default function useDeleteServerRoom({ friendlyId, onSettled }) {
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    () => axios.delete(`admin/server_rooms/${friendlyId}.json`),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('getServerRooms');
+        toast.success('Server room deleted.');
+      },
+      onError: () => {
+        toast.error('There was a problem completing that action. \n Please try again.');
+      },
+      onSettled,
+    },
+  );
+}
