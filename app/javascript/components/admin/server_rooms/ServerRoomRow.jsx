2bd030c6b37494ad34e63eff443930330ef36cf5
Khemissi Amir <amir.khemissi@insat.ucar.tn>
2022-07-14 18:20:00 +0100

    Server Rooms: Integrated with `Admin::ServerRoomsController#destroy`. (#3633)

--- app/javascript/components/admin/server_rooms/ServerRoomRow.jsx
+++ app/javascript/components/admin/server_rooms/ServerRoomRow.jsx
@@ -3,9 +3,14 @@ import { CursorClickIcon, DotsVerticalIcon, TrashIcon } from '@heroicons/react/o
 import { Dropdown, Stack } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import useDeleteServerRoom from '../../../hooks/mutations/admins/server-rooms/useDeleteServerRoom';
+import Modal from '../../shared/Modal';
+import DeleteRoomForm from '../../forms/DeleteRoomForm';
 
 export default function ServerRoomRow({ room }) {
-  console.log();
+  const { friendly_id: friendlyId } = room;
+  const mutationWrapper = (args) => useDeleteServerRoom({ friendlyId, ...args });
+
   return (
     <tr className="align-middle text-muted border border-2">
       <td className="border-end-0">
@@ -14,16 +19,20 @@ export default function ServerRoomRow({ room }) {
           <span> Ended: </span>
         </Stack>
       </td>
-      <td className="border-0"> { room.owner }</td>
-      <td className="border-0"> { room.friendly_id } </td>
-      <td className="border-0"> { room.participants ? room.participants : '-' } </td>
-      <td className="border-0"> { room.status } </td>
+      <td className="border-0"> {room.owner}</td>
+      <td className="border-0"> {room.friendly_id} </td>
+      <td className="border-0"> {room.participants ? room.participants : '-'} </td>
+      <td className="border-0"> {room.status} </td>
       <td className="border-start-0">
         <Dropdown className="float-end cursor-pointer">
           <Dropdown.Toggle className="hi-s" as={DotsVerticalIcon} />
           <Dropdown.Menu>
             <Dropdown.Item as={Link} to={`/rooms/${room.friendly_id}`}><CursorClickIcon className="hi-s" /> View</Dropdown.Item>
-            <Dropdown.Item><TrashIcon className="hi-s" /> Delete</Dropdown.Item>
+            <Modal
+              modalButton={<Dropdown.Item><TrashIcon className="hi-s" /> Delete</Dropdown.Item>}
+              title="Delete Server Room"
+              body={<DeleteRoomForm mutation={mutationWrapper} />}
+            />
           </Dropdown.Menu>
         </Dropdown>
       </td>
