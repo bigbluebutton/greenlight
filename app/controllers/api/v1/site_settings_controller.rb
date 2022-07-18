6464e0e5d0ec1eff967f13ba20b04f07041437c2
Hadi Cheaito <hadicheaito1@gmail.com>
2022-07-18 15:24:23 -0400

    Share Rooms Site Setting

--- app/controllers/api/v1/site_settings_controller.rb
+++ app/controllers/api/v1/site_settings_controller.rb
@@ -3,9 +3,26 @@
 module Api
   module V1
     class SiteSettingsController < ApiController
+      def index
+        site_setting_data = {}
+        site_settings = SiteSetting.joins(:setting).select(:id, :name, :value)
+
+        site_settings.each do |setting|
+          site_setting_data[setting.name] = { value: setting.value, id: setting.id }
+        end
+
+        render_json data: site_setting_data, status: :ok
+      end
+
       # GET /api/v1/site_settings/:name
       def show
         render_data data: SettingGetter.new(setting_name: params[:name], provider: 'greenlight').call, status: :ok # TODO: - ahmad: fix provider
+      end
+
+      def update
+        SiteSetting.find_by(setting_id: params[:settingId]).update(value: params[:settingValue].to_s)
+
+        render_data status: :ok
       end
     end
   end
