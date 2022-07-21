9e51cb28ce9f19705143dc3a29bf1fa811d975e5
kh-amir-tn <amir.khemissi@insat.ucar.tn>
2022-07-21 12:05:31 +0100

    Site Settings: Added RoleMapping UI.

--- app/javascript/components/admin/site_settings/Registration.jsx
+++ app/javascript/components/admin/site_settings/Registration.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+import RegistrationForm from '../../forms/admin/RegistrationForm';
 
-export default function Appearance() {
+export default function Registration() {
   return (
-    <p>Registration</p>
+    <Container className="w-75 mt-2 ms-0">
+      <Row className="mb-3">
+        <Row> <h6> Role Mapping By Email </h6> </Row>
+        <Row> <p className="text-muted"> Map the user to a role using their email.Must be in the format: role1=email1, role2=email2  </p> </Row>
+        <Row>
+          <RegistrationForm
+            mutation={() => ({ mutate: (data) => console.log(data) })}
+            value="User=users.com"
+          />
+        </Row>
+      </Row>
+    </Container>
   );
 }
