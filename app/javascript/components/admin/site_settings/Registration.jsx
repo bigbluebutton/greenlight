8b2a1073ce38b58591b4c21ae067fa97737af949
kh-amir-tn <amir.khemissi@insat.ucar.tn>
2022-07-21 19:59:55 +0100

    Site Settings: Integrated with the APIs.

--- app/javascript/components/admin/site_settings/Registration.jsx
+++ app/javascript/components/admin/site_settings/Registration.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Container, Row } from 'react-bootstrap';
+import useUpdateSiteSetting from '../../../hooks/mutations/admins/site_settings/useUpdateSiteSetting';
 import RegistrationForm from '../../forms/admin/RegistrationForm';
 
-export default function Registration() {
+export default function Registration({ value }) {
   return (
     <Container className="w-75 mt-2 ms-0">
       <Row className="mb-3">
@@ -10,11 +12,19 @@ export default function Registration() {
         <Row> <p className="text-muted"> Map the user to a role using their email.Must be in the format: role1=email1, role2=email2  </p> </Row>
         <Row>
           <RegistrationForm
-            mutation={() => ({ mutate: (data) => console.log(data) })}
-            value="User=users.com"
+            mutation={() => useUpdateSiteSetting('RoleMapping')}
+            value={value}
           />
         </Row>
       </Row>
     </Container>
   );
 }
+
+Registration.defaultProps = {
+  value: '',
+};
+
+Registration.propTypes = {
+  value: PropTypes.string,
+};
