e21c5bb58e0d90895d3672713599984e5ab98bcd
scouillard <samuel.couillard@blindsidenetworks.com>
2022-07-25 16:16:52 -0400

    Basic UI + endpoint hookup
    qf

--- app/javascript/App.jsx
+++ app/javascript/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Header from './components/shared/Header';
 import { useAuth } from './contexts/auth/AuthProvider';
 import Footer from './components/shared/Footer';
+import useSiteSettings from './hooks/queries/admin/site_settings/useSiteSettings';
 
 export default function App() {
   const currentUser = useAuth();
   const containerHeight = currentUser?.signed_in ? 'full-height' : 'h-100';
+  const { data: siteSettings } = useSiteSettings();
+
+  useEffect(() => {
+    document.documentElement.style.setProperty('--brand-color', siteSettings?.PrimaryColor);
+  }, [siteSettings]);
+
   return (
     <>
       {currentUser?.signed_in && <Header /> }
