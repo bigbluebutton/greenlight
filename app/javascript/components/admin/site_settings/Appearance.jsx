2bc8c60d37c909362881d0c4a09797161fd7394b
scouillard <samuel.couillard@blindsidenetworks.com>
2022-07-25 16:16:52 -0400

    Basic UI + endpoint hookup

--- app/javascript/components/admin/site_settings/Appearance.jsx
+++ app/javascript/components/admin/site_settings/Appearance.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { Form } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import { useForm } from 'react-hook-form';
+import useUpdateSiteSetting
+  from '../../../hooks/mutations/admins/site_settings/useUpdateSiteSetting';
+import useSiteSettings from '../../../hooks/queries/admin/site_settings/useSiteSettings';
 
 export default function Appearance() {
+  const { register, handleSubmit } = useForm();
+  const updateSiteSetting = useUpdateSiteSetting('PrimaryColor');
+  const { data: siteSettings } = useSiteSettings();
+
   return (
-    <p>Appearance</p>
+    <>
+      <h6>Primary Color</h6>
+      <Form onSubmit={handleSubmit(updateSiteSetting.mutate)}>
+        <Form.Group className="mb-3">
+          <Form.Control
+            type="color"
+            id="brandColor"
+            defaultValue={siteSettings?.PrimaryColor}
+            title="Choose your color"
+            {...register('value')}
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
   );
 }
