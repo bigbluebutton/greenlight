ccfe7ee11f68231b6b70f31e2d18da0ed1317190
Hadi Cheaito <hadicheaito1@gmail.com>
2022-07-28 14:06:55 -0400

    Making logo redirect to root on sign up/in

--- app/javascript/components/forms/FormLogo.jsx
+++ app/javascript/components/forms/FormLogo.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image';
+import { Link } from 'react-router-dom';
 
 export default function FormLogo() {
   return (
     <div className="text-center">
-      <Image src="https://blindsidenetworks.com/wp-content/uploads/2021/04/cropped-bn_logo-02.png" width="150px" className="pb-4" />
+      <Link to="/">
+        <Image src="https://blindsidenetworks.com/wp-content/uploads/2021/04/cropped-bn_logo-02.png" width="150px" className="pb-4" />
+      </Link>
     </div>
   );
 }
