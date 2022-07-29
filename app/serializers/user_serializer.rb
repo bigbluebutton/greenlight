31008de3427cdd335a9acd90433aea4938276793
Hadi Cheaito <hadicheaito1@gmail.com>
2022-07-29 13:37:52 -0400

    Adding created_at to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,9 +3,13 @@
 class UserSerializer < ApplicationSerializer
   include Avatarable
 
-  attributes :id, :name, :email, :provider, :language, :avatar
+  attributes :id, :name, :email, :provider, :language, :avatar, :created_at
 
   def avatar
     user_avatar(object)
+  end
+
+  def created_at
+    object.created_at.strftime('%A %B %e, %Y')
   end
 end
