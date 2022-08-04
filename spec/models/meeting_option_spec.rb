6919599157bf543c616a44c5e18d14577f946990
Khemissi Amir <amir.khemissi@insat.ucar.tn>
2022-08-04 15:44:03 +0100

    Room Settings: Added `MeetingOption::get_config_value`. (#3713)

--- spec/models/meeting_option_spec.rb
+++ spec/models/meeting_option_spec.rb
@@ -9,44 +9,70 @@ RSpec.describe MeetingOption, type: :model do
     it { is_expected.to validate_uniqueness_of(:name) }
   end
 
-  describe '#bbb_options' do
-    it 'returns all non-greenlight options' do
-      room = create(:room)
-      setting1 = create(:meeting_option, name: 'glSetting1')
-      setting2 = create(:meeting_option, name: 'glSetting2')
-      setting3 = create(:meeting_option, name: 'setting1')
-      setting4 = create(:meeting_option, name: 'setting2')
+  describe 'classs methods' do
+    describe '#bbb_options' do
+      it 'returns all non-greenlight options' do
+        room = create(:room)
+        setting1 = create(:meeting_option, name: 'glSetting1')
+        setting2 = create(:meeting_option, name: 'glSetting2')
+        setting3 = create(:meeting_option, name: 'setting1')
+        setting4 = create(:meeting_option, name: 'setting2')
 
-      create(:room_meeting_option, room:, meeting_option: setting1)
-      create(:room_meeting_option, room:, meeting_option: setting2)
-      room_option1 = create(:room_meeting_option, room:, meeting_option: setting3, value: 'value1')
-      room_option2 = create(:room_meeting_option, room:, meeting_option: setting4, value: 'value2')
+        create(:room_meeting_option, room:, meeting_option: setting1)
+        create(:room_meeting_option, room:, meeting_option: setting2)
+        room_option1 = create(:room_meeting_option, room:, meeting_option: setting3, value: 'value1')
+        room_option2 = create(:room_meeting_option, room:, meeting_option: setting4, value: 'value2')
 
-      expect(
-        described_class.bbb_options(room_id: room.id)
-      ).to eq([
-                [setting3.name, room_option1.value],
-                [setting4.name, room_option2.value]
-              ])
-    end
-  end
-
-  describe '#get_value' do
-    it 'returns the room meeting option for a room by name' do
-      room = create(:room)
-
-      meeting_option1 = create(:meeting_option, name: 'setting')
-      create(:room_meeting_option, room:, meeting_option: meeting_option1, value: 'value1')
-
-      room_meeting_option = described_class.get_value(name: 'setting', room_id: room.id)
-
-      expect(room_meeting_option.value).to eq('value1')
+        expect(
+          described_class.bbb_options(room_id: room.id)
+        ).to eq([
+                  [setting3.name, room_option1.value],
+                  [setting4.name, room_option2.value]
+                ])
+      end
     end
 
-    it 'returns nil for unfound room meeting option' do
-      expect(
-        described_class.get_value(name: 'notASettingTrustMe', room_id: 'YouAlreadyTrustedMe')
-      ).to be_nil
+    describe '#get_setting_value' do
+      context 'existing setting' do
+        it 'returns the room meeting option value' do
+          room = create(:room)
+
+          meeting_option1 = create(:meeting_option, name: 'setting')
+          create(:room_meeting_option, room:, meeting_option: meeting_option1, value: 'value1')
+
+          room_meeting_option = described_class.get_setting_value(name: 'setting', room_id: room.id)
+
+          expect(room_meeting_option.value).to eq('value1')
+        end
+      end
+
+      context 'inexisting setting' do
+        it 'returns nil' do
+          expect(
+            described_class.get_setting_value(name: 'notASettingTrustMe', room_id: 'YouAlreadyTrustedMe')
+          ).to be_nil
+        end
+      end
+    end
+
+    describe '#get_config_value' do
+      context 'existing configuration' do
+        it 'returns the rooms configuration value' do
+          meeting_option = create(:meeting_option, name: 'setting')
+          create(:rooms_configuration, meeting_option:, provider: 'greenlight', value: 'optional')
+
+          rooms_config = described_class.get_config_value(name: 'setting', provider: 'greenlight')
+          expect(rooms_config.value).to eq('optional')
+        end
+      end
+
+      context 'inexisting configuration' do
+        it 'returns nil' do
+          expect(
+            described_class.get_config_value(name: 'notAConfigTrustMe', provider: 'YouShould\'ve')
+          ).to be_nil
+        end
+      end
     end
   end
 end
