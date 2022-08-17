003885b3cf9694eb0d78687b2dc8a9c80f72b040
kh-amir-tn <amir.khemissi@insat.ucar.tn>
2022-08-17 18:12:34 +0100

    Room settings: Upgraded `RoomSettingsGetter` service. 	+ Added access codes handling to the service. 	+ Added `only_enabled` mode to fetch only enabled options.

--- app/services/room_settings_getter.rb
+++ app/services/room_settings_getter.rb
@@ -7,9 +7,10 @@ class RoomSettingsGetter
   # Hash(`<option_name> => {'true' => <Postive>, 'false' => <Negative>})`
   SPECIAL_OPTIONS = { 'guestPolicy' => { 'true' => 'ASK_MODERATOR', 'false' => 'ALWAYS_ACCEPT' } }.freeze
 
-  def initialize(room_id:, provider:, only_bbb_options: false)
+  def initialize(room_id:, provider:, only_enabled: false, only_bbb_options: false)
     @room_id = room_id
     @only_bbb_options = only_bbb_options
+    @only_enabled = only_enabled
     # Fetching only rooms configs that are not optional to overwrite the settings values.
     @rooms_configs = MeetingOption.joins(:rooms_configurations)
                                   .where(rooms_configurations: { provider: })
@@ -23,7 +24,25 @@ class RoomSettingsGetter
     room_settings = room_settings.where.not('name ILIKE :prefix', prefix: 'gl%') if @only_bbb_options
     room_settings = room_settings.pluck(:name, :value).to_h
 
+    access_codes = room_settings.slice('glViewerAccessCode', 'glModeratorAccessCode') # Holding room original access code values.
+
     room_settings.merge!(@rooms_configs) # Merging rooms settings with its none optional configurations prioritizing forced configs over settings.
+
+    filter_disabled(room_settings:) if @only_enabled # Only enabled(optional|force enabled) setting values will be returned.
+    infer_specials(room_settings:) # Special options should map their forced values to what was configured in `SPECIAL_OPTIONS` registry.
+    infer_codes(room_settings:, access_codes:) # Access codes should map their forced values as intended.
+
+    room_settings
+  end
+
+  private
+
+  def filter_disabled(room_settings:)
+    disabled_settings = @rooms_configs.filter { |_k, v| v == 'false' }
+    room_settings.except!(*disabled_settings.keys)
+  end
+
+  def infer_specials(room_settings:)
     room_settings_special_options = SPECIAL_OPTIONS.slice(*room_settings.keys) # Extracting the room settings special options to minimize iterations.
 
     # Configs are enum of ['true', 'false', 'optional'], after merging none optional settings with their configs only the room
@@ -32,7 +51,16 @@ class RoomSettingsGetter
       config = room_settings[name]
       room_settings[name] = SPECIAL_OPTIONS[name][config] if %w[true false].include? config # Config values are expected to be 'true'|'false'
     end
+  end
 
-    room_settings
+  def infer_codes(room_settings:, access_codes:)
+    access_codes.each do |key, code|
+      case room_settings[key]
+      when 'false'
+        room_settings[key] = '' # Forced disbaled access code will have an empty value.
+      when 'true'
+        room_settings[key] = code # Forced enabled access code will conserve its   original value.
+      end
+    end
   end
 end
