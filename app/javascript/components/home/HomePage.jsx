114b7b0d6f4661f8ea6527a16632d9ad7f5d4fe9
scouillard <samuel.couillard@blindsidenetworks.com>
2022-08-31 15:49:11 -0400

    Add homepage

--- app/javascript/components/home/HomePage.jsx
+++ app/javascript/components/home/HomePage.jsx
@@ -1,26 +1,46 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import { Card, Form, Stack } from 'react-bootstrap';
+import { PlayIcon } from '@heroicons/react/outline';
 import ButtonLink from '../shared_components/utilities/ButtonLink';
 import Spinner from '../shared_components/utilities/Spinner';
 import useEnv from '../../hooks/queries/env/useEnv';
+import Logo from '../shared_components/Logo';
 
 export default function HomePage() {
   const { isLoading, data: env } = useEnv();
   if (isLoading) return <Spinner />;
 
-  return (
-    env.OPENID_CONNECT ? (
+  if (env.OPENID_CONNECT) {
+    return (
+    // env.OPENID_CONNECT ? (
       <Form action="/auth/openid_connect" method="POST" data-turbo="false">
         <input type="hidden" name="authenticity_token" value={document.querySelector('meta[name="csrf-token"]').content} />
         <input variant="brand" className="btn mx-2" type="submit" value="Sign In" />
         <input variant="brand-backward" className="btn" type="submit" value="Sign Up" />
       </Form>
+    );
+  }
 
-    ) : (
-      <>
-        <ButtonLink to="/signin" variant="brand" className="mx-2">Sign In</ButtonLink>
-        <ButtonLink to="/signup" variant="brand-backward">Sign Up</ButtonLink>
-      </>
-    )
+  return (
+    <div className="vertical-center">
+      <Logo className="d-block mx-auto mb-4 brand-image-lg" />
+      <Card className="col-md-8 mx-auto p-4 border-0 shadow-sm text-center">
+        <h1 className="mt-4"> Welcome to BigBlueButton. </h1>
+        <span className="text-muted my-3 px-xxl-5">
+          Greenlight is a simple front-end for your BigBlueButton open-source web
+          conferencing server.
+          You can create your own rooms to host sessions, or
+          join others using a short and convenient link.
+        </span>
+        <a href="https://youtube.com" className="mt-1 text-link">
+          Watch our tutorial on using Greenlight
+          <PlayIcon className="hi-s mb-1 ms-1" />
+        </a>
+        <Stack direction="horizontal" className="mx-auto mt-4 mb-2">
+          <ButtonLink to="/signup" variant="brand-backward" className="btn-lg">Sign Up</ButtonLink>
+          <ButtonLink to="/signin" variant="brand" className="btn-lg mx-2">Sign In</ButtonLink>
+        </Stack>
+      </Card>
+    </div>
   );
 }
--- app/javascript/components/shared_components/Logo.jsx
+++ app/javascript/components/shared_components/Logo.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image';
+import PropTypes from 'prop-types';
 import useSiteSetting from '../../hooks/queries/site_settings/useSiteSetting';
 
-export default function Logo() {
+export default function Logo({ className }) {
   const { data: brandingImage } = useSiteSetting('BrandingImage');
 
   return (
     <Image
       src={brandingImage}
-      className="brand-image"
+      className={className}
       alt="CompanyLogo"
     />
   );
 }
+
+Logo.propTypes = {
+  className: PropTypes.string,
+};
+
+Logo.defaultProps = {
+  className: 'brand-image',
+};
