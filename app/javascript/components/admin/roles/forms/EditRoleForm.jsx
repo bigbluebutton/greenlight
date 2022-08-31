4200fbce38714e531873fb378c4a4bd4b2782058
Hadi Cheaito <38328371+hadicheaito1@users.noreply.github.com>
2022-08-31 15:26:23 -0400

    RolePermissionRow + fixes (#3813)

--- app/javascript/components/admin/roles/forms/EditRoleForm.jsx
+++ app/javascript/components/admin/roles/forms/EditRoleForm.jsx
@@ -12,11 +12,12 @@ import DeleteRoleForm from './DeleteRoleForm';
 import useUpdateRolePermission from '../../../../hooks/mutations/admin/role_permissions/useUpdateRolePermissions';
 import useRoomConfigs from '../../../../hooks/queries/admin/room_configuration/useRoomConfigs';
 import useRolePermissions from '../../../../hooks/queries/admin/role_permissions/useRolePermissions';
+import RolePermissionRow from '../RolePermissionRow';
 
 export default function EditRoleForm({ role }) {
   const methods = useForm(editRoleFormConfig);
   const updateRoleAPI = useUpdateRole(role.id);
-  const updateRolePermission = useUpdateRolePermission();
+  const updateRolePermission = () => useUpdateRolePermission();
   const { defaultValues } = editRoleFormConfig;
   defaultValues.name = role.name;
   const fields = editRoleFormFields;
@@ -27,47 +28,39 @@ export default function EditRoleForm({ role }) {
   if (roomConfigs.isLoading || rolePermissions.isLoading) return <Spinner />;
   return (
     <div>
-      <Form methods={methods} onSubmit={updateRoleAPI.mutate}>
-        <FormControl field={fields.name} type="text" />
-        <Stack className="mt-1 float-end" gap={2} direction="horizontal">
-          <Modal
-            modalButton={<Button className="danger-light-button"> Delete Role </Button>}
-            title="Delete Role"
-            body={<DeleteRoleForm role={role} />}
-          />
-          <Button
-            variant="outline-primary"
-            onClick={() => methods.reset(defaultValues)}
-          >
-            Cancel
-          </Button>
-          <Button variant="brand" type="submit" disabled={updateRoleAPI.isLoading}>
-            Update
-            {updateRoleAPI.isLoading && <Spinner />}
-          </Button>
-        </Stack>
-      </Form>
-      {/* TODO: Put into row component & classname scss */}
-      {(roomConfigs.isLoading || rolePermissions.isLoading || roomConfigs.data.record === 'optional') && (
-        <div className="room-settings-row text-muted py-3 d-flex">
-          { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
-          <label className="form-check-label me-auto" htmlFor="test">
-            Allow users with this role to record their meetings
-          </label>
-          <div className="form-switch">
-            <input
-              id="test"
-              className="form-check-input fs-5"
-              type="checkbox"
-              defaultChecked={rolePermissions.data.CanRecord === 'true'}
-              onClick={(event) => {
-                // TODO: Currently using role_id and name, review this
-                updateRolePermission.mutate({ role_id: role.id, name: 'CanRecord', value: event.target.checked });
-              }}
+      <Stack>
+        <Form methods={methods} onSubmit={updateRoleAPI.mutate}>
+          <FormControl field={fields.name} type="text" />
+          <Stack className="mt-1 float-end" gap={2} direction="horizontal">
+            <Modal
+              modalButton={<Button className="danger-light-button"> Delete Role </Button>}
+              title="Delete Role"
+              body={<DeleteRoleForm role={role} />}
             />
-          </div>
-        </div>
-      )}
+            <Button
+              variant="outline-primary"
+              onClick={() => methods.reset(defaultValues)}
+            >
+              Cancel
+            </Button>
+            <Button variant="brand" type="submit" disabled={updateRoleAPI.isLoading}>
+              Update
+              {updateRoleAPI.isLoading && <Spinner />}
+            </Button>
+          </Stack>
+        </Form>
+        {(roomConfigs.isLoading || rolePermissions.isLoading || roomConfigs.data.record === 'optional') && (
+          <Stack>
+            <RolePermissionRow
+              permissionName="CanRecord"
+              description="Allow users with this role to record their meetings"
+              roleId={role.id}
+              defaultValue={rolePermissions.data.CanRecord === 'true'}
+              updateMutation={updateRolePermission}
+            />
+          </Stack>
+        )}
+      </Stack>
     </div>
   );
 }
