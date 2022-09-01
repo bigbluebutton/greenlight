b7c5da90c4d2d5857f9e5ae6f4166feaca58bdb9
farhatahmad <ahmad.af.farhat@gmail.com>
2022-09-01 11:15:06 -0400

    Made AnyoneCanStart setting work properly

--- app/controllers/api/v1/meetings_controller.rb
+++ app/controllers/api/v1/meetings_controller.rb
@@ -13,8 +13,6 @@ module Api
       # Returns: { data: Array[serializable objects] , errors: Array[String] }
       # Does: Starts the Room meeting and joins in the meeting starter.
       def start
-        presentation_url = (url_for(@room.presentation).gsub('&', '%26') if @room.presentation.attached?)
-
         begin
           MeetingStarter.new(
             room: @room,
@@ -34,13 +32,34 @@ module Api
 
       # POST /api/v1/meetings/:friendly_id/status.json
       def status
-        bbb_role = infer_bbb_role(room_id: @room.id)
+        settings = RoomSettingsGetter.new(room_id: @room.id, provider: current_provider, current_user:, show_codes: true,
+                                          settings: %w[glViewerAccessCode glModeratorAccessCode glAnyoneCanStart]).call
+
+        bbb_role = infer_bbb_role(mod_code: settings['glModeratorAccessCode'], viewer_code: settings['glViewerAccessCode'])
 
         return render_error status: :forbidden if bbb_role.nil?
 
         data = {
           status: BigBlueButtonApi.new.meeting_running?(room: @room)
         }
+
+        if !data[:status] && settings['glAnyoneCanStart'] == 'true' # Meeting isnt running and anyoneCanStart setting is enabled
+          begin
+            MeetingStarter.new(
+              room: @room,
+              logout_url: request.referer,
+              presentation_url:,
+              meeting_ended: meeting_ended_url,
+              recording_ready: recording_ready_url,
+              current_user:,
+              provider: current_provider
+            ).call
+          rescue BigBlueButton::BigBlueButtonException => e
+            return render_error status: :bad_request unless e.key == 'idNotUnique'
+          end
+
+          data[:status] = true
+        end
 
         data[:joinUrl] = BigBlueButtonApi.new.join_meeting(room: @room, name: params[:name], role: bbb_role) if data[:status]
         render_data data:, status: :ok
@@ -52,23 +71,26 @@ module Api
         @room = Room.find_by!(friendly_id: params[:friendly_id])
       end
 
+      def presentation_url
+        url_for(@room.presentation).gsub('&', '%26') if @room.presentation.attached?
+      end
+
       def authorized_as_viewer?(access_code:)
-        (params[:access_code].blank? && access_code.blank?) ||
-          (params[:access_code].present? && access_code == params[:access_code])
+        return true if access_code.blank?
+
+        access_code == params[:access_code]
       end
 
       def authorized_as_moderator?(access_code:)
         (params[:access_code].present? && access_code == params[:access_code]) || @room.anyone_joins_as_moderator?
       end
 
-      def infer_bbb_role(room_id:)
+      def infer_bbb_role(mod_code:, viewer_code:)
         # TODO: Handle access code circumventing when 'anyone_joins_as_moderator?' is true.
-        room_access_codes = RoomSettingsGetter.new(room_id:, provider: current_provider, current_user:, show_codes: true,
-                                                   settings: %w[glViewerAccessCode glModeratorAccessCode]).call
 
-        if authorized_as_moderator?(access_code: room_access_codes['glModeratorAccessCode'])
+        if authorized_as_moderator?(access_code: mod_code)
           'Moderator'
-        elsif authorized_as_viewer?(access_code: room_access_codes['glViewerAccessCode'])
+        elsif authorized_as_viewer?(access_code: viewer_code)
           'Viewer'
         end
       end
--- spec/controllers/meetings_controller_spec.rb
+++ spec/controllers/meetings_controller_spec.rb
@@ -4,28 +4,27 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::MeetingsController, type: :controller do
   let(:user) { create(:user) }
+  let(:room) { create(:room, user:) }
   let(:user_with_manage_rooms_permission) { create(:user, :with_manage_rooms_permission) }
 
   before do
     request.headers['ACCEPT'] = 'application/json'
     session[:user_id] = user.id
+
+    allow_any_instance_of(BigBlueButtonApi)
+      .to receive(:start_meeting)
+      .and_return(meeting_starter_response)
+
+    allow_any_instance_of(BigBlueButtonApi)
+      .to receive(:join_meeting)
+      .and_return('JOIN_URL')
   end
 
   describe '#start' do
-    let(:room) { create(:room, user:) }
-
-    before do
-      allow_any_instance_of(BigBlueButtonApi)
-        .to receive(:start_meeting)
-        .and_return(meeting_starter_response)
-    end
-
     it 'makes a call to the MeetingStarter service with the right values and returns the join url' do
       logout = 'http://example.com'
       request.env['HTTP_REFERER'] = logout
       presentation_url = nil
-
-      allow_any_instance_of(BigBlueButtonApi).to receive(:join_meeting).and_return('JOIN_URL')
 
       expect(
         MeetingStarter
@@ -85,14 +84,10 @@ RSpec.describe Api::V1::MeetingsController, type: :controller do
     end
 
     it 'returns the join url to the front-end for redirecting' do
-      allow_any_instance_of(BigBlueButtonApi)
-        .to receive(:join_meeting)
-        .and_return('https://example.com')
-
       post :start, params: { friendly_id: room.friendly_id }
 
       expect(response).to have_http_status(:created)
-      expect(JSON.parse(response.body)['data']).to eq('https://example.com')
+      expect(JSON.parse(response.body)['data']).to eq('JOIN_URL')
     end
 
     it 'returns an error if the user is not logged in' do
@@ -112,14 +107,10 @@ RSpec.describe Api::V1::MeetingsController, type: :controller do
           .to receive(:call)
           .and_raise(exception)
 
-        allow_any_instance_of(BigBlueButtonApi)
-          .to receive(:join_meeting)
-          .and_return('https://example.com')
-
         post :start, params: { friendly_id: room.friendly_id }
 
         expect(response).to have_http_status(:created)
-        expect(JSON.parse(response.body)['data']).to eq('https://example.com')
+        expect(JSON.parse(response.body)['data']).to eq('JOIN_URL')
       end
     end
 
@@ -139,11 +130,7 @@ RSpec.describe Api::V1::MeetingsController, type: :controller do
 
   describe '#status' do
     it 'gets the joinUrl if the meeting is running' do
-      room = create(:room, user:)
-
       allow_any_instance_of(BigBlueButtonApi).to receive(:meeting_running?).and_return(true)
-      allow_any_instance_of(BigBlueButtonApi).to receive(:join_meeting).and_return('JOIN_URL')
-
       expect_any_instance_of(BigBlueButtonApi).to receive(:join_meeting).with(room:, name: user.name, role: 'Viewer')
 
       post :status, params: { friendly_id: room.friendly_id, name: user.name }
@@ -152,8 +139,6 @@ RSpec.describe Api::V1::MeetingsController, type: :controller do
     end
 
     it 'returns status false if the meeting is NOT running' do
-      room = create(:room, user:)
-
       allow_any_instance_of(BigBlueButtonApi).to receive(:meeting_running?).and_return(false)
       expect_any_instance_of(BigBlueButtonApi).not_to receive(:join_meeting)
 
@@ -163,8 +148,6 @@ RSpec.describe Api::V1::MeetingsController, type: :controller do
     end
 
     it 'joins as viewer if no access code is required nor provided' do
-      room = create(:room, user:)
-
       allow_any_instance_of(BigBlueButtonApi).to receive(:meeting_running?).and_return(true)
       expect_any_instance_of(BigBlueButtonApi).to receive(:join_meeting).with(room:, name: user.name, role: 'Viewer')
       post :status, params: { friendly_id: room.friendly_id, name: user.name }
@@ -180,38 +163,82 @@ RSpec.describe Api::V1::MeetingsController, type: :controller do
       end
 
       it 'joins as viewer if access code correspond to the viewer access code' do
-        room = create(:room, user:)
         allow_any_instance_of(BigBlueButtonApi).to receive(:meeting_running?).and_return(true)
 
         expect_any_instance_of(BigBlueButtonApi).to receive(:join_meeting).with(room:, name: user.name, role: 'Viewer')
         expect(RoomSettingsGetter).to receive(:new).with(room_id: room.id, provider: 'greenlight', show_codes: true, current_user: user,
-                                                         settings: %w[glViewerAccessCode glModeratorAccessCode])
+                                                         settings: %w[glViewerAccessCode glModeratorAccessCode glAnyoneCanStart])
         expect(fake_room_settings_getter).to receive(:call)
         post :status, params: { friendly_id: room.friendly_id, name: user.name, access_code: 'AAA' }
         expect(response).to have_http_status(:ok)
       end
 
       it 'joins as moderator if access code correspond to moderator access code' do
-        room = create(:room, user:)
         allow_any_instance_of(BigBlueButtonApi).to receive(:meeting_running?).and_return(true)
 
         expect_any_instance_of(BigBlueButtonApi).to receive(:join_meeting).with(room:, name: user.name, role: 'Moderator')
         expect(RoomSettingsGetter).to receive(:new).with(room_id: room.id, provider: 'greenlight', show_codes: true, current_user: user,
-                                                         settings: %w[glViewerAccessCode glModeratorAccessCode])
+                                                         settings: %w[glViewerAccessCode glModeratorAccessCode glAnyoneCanStart])
         expect(fake_room_settings_getter).to receive(:call)
         post :status, params: { friendly_id: room.friendly_id, name: user.name, access_code: 'BBB' }
         expect(response).to have_http_status(:ok)
       end
 
       it 'returns unauthorized if the access code is wrong' do
-        room = create(:room, user:)
         allow_any_instance_of(BigBlueButtonApi).to receive(:meeting_running?).and_return(true)
 
         expect(RoomSettingsGetter).to receive(:new).with(room_id: room.id, provider: 'greenlight', show_codes: true, current_user: user,
-                                                         settings: %w[glViewerAccessCode glModeratorAccessCode])
+                                                         settings: %w[glViewerAccessCode glModeratorAccessCode glAnyoneCanStart])
         expect(fake_room_settings_getter).to receive(:call)
         post :status, params: { friendly_id: room.friendly_id, name: user.name, access_code: 'ZZZ' }
         expect(response).to have_http_status(:forbidden)
+      end
+    end
+
+    context 'glAnyoneCanStart' do
+      let(:fake_room_settings_getter) { instance_double(RoomSettingsGetter) }
+
+      before do
+        allow(RoomSettingsGetter).to receive(:new).and_return(fake_room_settings_getter)
+        allow(fake_room_settings_getter).to receive(:call).and_return({ 'glAnyoneCanStart' => 'true' })
+      end
+
+      it 'starts the meeting if the meeting isnt already running' do
+        request.env['HTTP_REFERER'] = 'http://example.com'
+
+        allow_any_instance_of(BigBlueButtonApi).to receive(:meeting_running?).and_return(false)
+
+        expect(
+          MeetingStarter
+        ).to receive(:new).with(
+          room:,
+          logout_url: 'http://example.com',
+          presentation_url: nil,
+          meeting_ended: meeting_ended_url,
+          recording_ready: recording_ready_url,
+          current_user: user,
+          provider: 'greenlight'
+        ).and_call_original
+
+        expect_any_instance_of(MeetingStarter).to receive(:call)
+
+        post :status, params: { friendly_id: room.friendly_id, name: user.name }
+
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body)['data']).to eq({ 'joinUrl' => 'JOIN_URL', 'status' => true })
+      end
+
+      it 'doesnt start the meeting if its already running' do
+        request.env['HTTP_REFERER'] = 'http://example.com'
+
+        allow_any_instance_of(BigBlueButtonApi).to receive(:meeting_running?).and_return(true)
+
+        expect_any_instance_of(MeetingStarter).not_to receive(:call)
+
+        post :status, params: { friendly_id: room.friendly_id, name: user.name }
+
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body)['data']).to eq({ 'joinUrl' => 'JOIN_URL', 'status' => true })
       end
     end
 
