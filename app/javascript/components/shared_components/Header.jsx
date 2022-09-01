7e336a2c9aec4180349ae46650f7fa52731bff23
scouillard <samuel.couillard@blindsidenetworks.com>
2022-09-01 12:34:17 -0400

    Replace FormLogo component with Logo

--- app/javascript/components/shared_components/Header.jsx
+++ app/javascript/components/shared_components/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
     <Navbar className="header">
       <Container className="ps-0">
         <Navbar.Brand as={Link} to="/rooms">
-          <Logo />
+          <Logo width="200px" />
         </Navbar.Brand>
 
         <div className="d-inline-flex">
--- app/javascript/components/users/authentication/SignIn.jsx
+++ app/javascript/components/users/authentication/SignIn.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import SigninForm from './forms/SigninForm';
-import FormLogo from '../../shared_components/forms/FormLogo';
+import Logo from '../../shared_components/Logo';
 
 export default function SignIn() {
   return (
     <div className="vertical-center">
-      <FormLogo />
+      <div className="text-center pb-4">
+        <Logo width="300px" />
+      </div>
       <Card className="col-md-4 mx-auto p-4 border-0 shadow-sm">
         <Card.Title className="text-center pb-2"> Login </Card.Title>
         <SigninForm />
