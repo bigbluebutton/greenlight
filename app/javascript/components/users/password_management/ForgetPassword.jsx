43d53fb16956e784fbc2096cbb07c1c6c5e3038b
Samuel Couillard <43917914+scouillard@users.noreply.github.com>
2022-09-06 13:31:48 -0400

    Replace FormLogo component with Logo (#3823)
    * Replace FormLogo component with Logo
    
    * Delete FormLogo component
    
    * Remove FormLogo from RoomJoin, ForgetPassword, ResetPassword
    
    * Rebased on #3819

--- app/javascript/components/users/password_management/ForgetPassword.jsx
+++ app/javascript/components/users/password_management/ForgetPassword.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
-import FormLogo from '../../shared_components/forms/FormLogo';
 import ForgetPwdForm from './forms/ForgetPwdForm';
+import Logo from '../../shared_components/Logo';
 
 export default function ForgetPassword() {
   return (
     <div className="vertical-center">
-      <FormLogo />
+      <div className="text-center pb-4">
+        <Logo size="medium" />
+      </div>
       <Card className="col-md-4 mx-auto p-4 border-0 shadow-sm">
         <Card.Title className="text-center pb-2"> Reset Password </Card.Title>
         <ForgetPwdForm />
