e3299b3baab0338d0fd53c7be521d65bf32fc153
Samuel Couillard <43917914+scouillard@users.noreply.github.com>
2022-09-06 12:42:07 -0400

    Add Homepage (#3819)
    * Add homepage
    
    * Bigger buttons, remove link
    
    * Add some padding
    
    * Remove className from Logo props
    
    * Change width props to size with small, medium or large value

--- app/javascript/components/home/HomePage.jsx
+++ app/javascript/components/home/HomePage.jsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import { Card, Form, Stack } from 'react-bootstrap';
 import ButtonLink from '../shared_components/utilities/ButtonLink';
 import Spinner from '../shared_components/utilities/Spinner';
 import useEnv from '../../hooks/queries/env/useEnv';
+import Logo from '../shared_components/Logo';
 
 export default function HomePage() {
   const { isLoading, data: env } = useEnv();
   if (isLoading) return <Spinner />;
 
   return (
-    env.OPENID_CONNECT ? (
-      <Form action="/auth/openid_connect" method="POST" data-turbo="false">
-        <input type="hidden" name="authenticity_token" value={document.querySelector('meta[name="csrf-token"]').content} />
-        <input variant="brand" className="btn mx-2" type="submit" value="Sign In" />
-        <input variant="brand-backward" className="btn" type="submit" value="Sign Up" />
-      </Form>
-
-    ) : (
-      <>
-        <ButtonLink to="/signin" variant="brand" className="mx-2">Sign In</ButtonLink>
-        <ButtonLink to="/signup" variant="brand-backward">Sign Up</ButtonLink>
-      </>
-    )
+    <div className="vertical-center">
+      <div className="text-center mb-4">
+        <Logo size="large" />
+      </div>
+      <Card className="col-md-8 mx-auto p-5 border-0 shadow-sm text-center">
+        <h1 className="mt-4"> Welcome to BigBlueButton. </h1>
+        <span className="text-muted mt-4 mb-5 px-xxl-5">
+          Greenlight is a simple front-end for your BigBlueButton open-source web
+          conferencing server.
+          You can create your own rooms to host sessions, or
+          join others using a short and convenient link.
+        </span>
+        <Stack direction="horizontal" className="mx-auto mb-2">
+          {
+            env.OPENID_CONNECT ? (
+              <Form action="/auth/openid_connect" method="POST" data-turbo="false">
+                <input type="hidden" name="authenticity_token" value={document.querySelector('meta[name="csrf-token"]').content} />
+                <input variant="brand-backward" className="btn btn-xlg" type="submit" value="Sign Up" />
+                <input variant="brand" className="btn btn-xlg mx-4" type="submit" value="Sign In" />
+              </Form>
+            ) : (
+              <>
+                <ButtonLink to="/signup" variant="brand-backward" className="btn btn-xlg">Sign Up</ButtonLink>
+                <ButtonLink to="/signin" variant="brand" className="btn btn-xlg mx-4">Sign In</ButtonLink>
+              </>
+            )
+          }
+        </Stack>
+      </Card>
+    </div>
   );
 }
--- app/javascript/components/shared_components/Logo.jsx
+++ app/javascript/components/shared_components/Logo.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image';
+import PropTypes from 'prop-types';
 import useSiteSetting from '../../hooks/queries/site_settings/useSiteSetting';
 
-export default function Logo() {
+export default function Logo({ size }) {
   const { data: brandingImage } = useSiteSetting('BrandingImage');
+
+  const sizeClass = `${size}-logo`;
 
   return (
     <Image
       src={brandingImage}
-      className="brand-image"
+      className={sizeClass}
       alt="CompanyLogo"
     />
   );
 }
+
+Logo.propTypes = {
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+};
+
+Logo.defaultProps = {
+  size: 'small',
+};
