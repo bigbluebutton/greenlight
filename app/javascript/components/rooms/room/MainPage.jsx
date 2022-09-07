0144ae36cf2f2e95255086804c1695fc87a96474
Hadi Cheaito <hadicheaito1@gmail.com>
2022-09-07 16:31:15 -0400

    comment fix

--- app/javascript/components/rooms/room/MainPage.jsx
+++ app/javascript/components/rooms/room/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// This room is shown is the current_user does NOT have CreateRoom permission
+// This page is shown is the current_user does NOT have CreateRoom permission
 export default function MainPage() {
   return (
     <>This page is for any user who doesnt have CreateRoom permission</>
