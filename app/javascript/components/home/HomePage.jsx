76e0120527e328f5cd5025ba8d8cfe5ac5447dca
scouillard <samuel.couillard@blindsidenetworks.com>
2022-09-08 15:02:17 -0400

    Fix homepage layout, add join url input

--- app/javascript/components/home/HomePage.jsx
+++ app/javascript/components/home/HomePage.jsx
@@ -1,12 +1,63 @@
 import React from 'react';
 import { Card, Form, Stack } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 import ButtonLink from '../shared_components/utilities/ButtonLink';
 import Spinner from '../shared_components/utilities/Spinner';
 import useEnv from '../../hooks/queries/env/useEnv';
 import Logo from '../shared_components/Logo';
+import { useAuth } from '../../contexts/auth/AuthProvider';
 
 export default function HomePage() {
   const { isLoading, data: env } = useEnv();
+  const currentUser = useAuth();
+
+  function homepageButtons() {
+    if (currentUser?.signed_in) {
+      return (
+        <div className="text-start">
+          <h6> Please enter the URL of your meeting. </h6>
+          <Stack direction="horizontal">
+            <input className="form-control" id="joinUrl" />
+            <Button variant="brand" className="ms-2">Join</Button>
+          </Stack>
+        </div>
+      );
+    }
+    return (
+      <>
+        <span className="text-muted">
+          Greenlight is a simple front-end for your BigBlueButton open-source web
+          conferencing server.
+          You can create your own rooms to host sessions, or
+          join others using a short and convenient link.
+        </span>
+        <Stack direction="horizontal" className="d-block mx-auto mt-5 mb-2">
+          { env.OPENID_CONNECT ? (
+            <Form action="/auth/openid_connect" method="POST" data-turbo="false">
+              <input
+                type="hidden"
+                name="authenticity_token"
+                value={document.querySelector('meta[name="csrf-token"]').content}
+              />
+              <input
+                variant="brand-backward"
+                className="btn btn-xlg"
+                type="submit"
+                value="Sign Up"
+              />
+              <input variant="brand" className="btn btn-xlg mx-4" type="submit" value="Sign In" />
+            </Form>
+          ) : (
+            <>
+              <ButtonLink to="/signup" variant="brand-backward" className="btn btn-xlg">Sign Up</ButtonLink>
+              <ButtonLink to="/signin" variant="brand" className="btn btn-xlg mx-4">Sign In</ButtonLink>
+            </>
+          )}
+        </Stack>
+      </>
+    );
+  }
+
   if (isLoading) return <Spinner />;
 
   return (
@@ -16,28 +67,9 @@ export default function HomePage() {
       </div>
       <Card className="col-md-8 mx-auto p-5 border-0 shadow-sm text-center">
         <h1 className="mt-4"> Welcome to BigBlueButton. </h1>
-        <span className="text-muted mt-4 mb-5 px-xxl-5">
-          Greenlight is a simple front-end for your BigBlueButton open-source web
-          conferencing server.
-          You can create your own rooms to host sessions, or
-          join others using a short and convenient link.
-        </span>
-        <Stack direction="horizontal" className="mx-auto mb-2">
-          {
-            env.OPENID_CONNECT ? (
-              <Form action="/auth/openid_connect" method="POST" data-turbo="false">
-                <input type="hidden" name="authenticity_token" value={document.querySelector('meta[name="csrf-token"]').content} />
-                <input variant="brand-backward" className="btn btn-xlg" type="submit" value="Sign Up" />
-                <input variant="brand" className="btn btn-xlg mx-4" type="submit" value="Sign In" />
-              </Form>
-            ) : (
-              <>
-                <ButtonLink to="/signup" variant="brand-backward" className="btn btn-xlg">Sign Up</ButtonLink>
-                <ButtonLink to="/signin" variant="brand" className="btn btn-xlg mx-4">Sign In</ButtonLink>
-              </>
-            )
-          }
-        </Stack>
+        <div className="mt-4 px-xxl-5">
+          { homepageButtons() }
+        </div>
       </Card>
     </div>
   );
