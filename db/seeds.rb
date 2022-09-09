3be071ed282f42cc91b59f436e5dcb01dafcf59c
farhatahmad <ahmad.af.farhat@gmail.com>
2022-09-09 10:31:50 -0400

    Added admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(
+  name: 'Administrator',
+  email: "admin@admin.com",
+  password: 'Administrator1!',
+  password_confirmation: 'Administrator1!',
+  provider: 'greenlight',
+  language: 'en',
+  role: Role.find_by(name: 'Administrator')
+)
+
+puts "Successfully created an administrator account"
+puts "email: admin@admin.com"
+puts "password: Administrator1!"
+puts "Sign up using your personal email and then promote that account using this administrator"
+puts "Once you've promoted that account, this admin account must be deleted"
