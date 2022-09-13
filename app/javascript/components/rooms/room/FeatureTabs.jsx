f3d8676b2009f063dc533354365a246f3b15e132
Ahmad Farhat <ahmad.af.farhat@gmail.com>
2022-09-13 13:59:13 -0400

    Remove shared access (#3854)
    * Removed shared access
    
    * eslint

--- app/javascript/components/rooms/room/FeatureTabs.jsx
+++ app/javascript/components/rooms/room/FeatureTabs.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import { Row, Tabs, Tab } from 'react-bootstrap';
 import RoomRecordings from '../../recordings/room_recordings/RoomRecordings';
 import Presentation from './presentation/Presentation';
-import SharedAccess from './shared_access/SharedAccess';
 import RoomSettings from './room_settings/RoomSettings';
 import useSiteSetting from '../../../hooks/queries/site_settings/useSiteSetting';
 
 export default function FeatureTabs() {
-  const { data: shareRoomEnabled } = useSiteSetting('ShareRooms');
   const { data: preuploadEnabled } = useSiteSetting('PreuploadPresentation');
 
   return (
@@ -22,12 +20,12 @@ export default function FeatureTabs() {
             <Presentation />
           </Tab>
           )}
-        { shareRoomEnabled
-          && (
-          <Tab eventKey="access" title="Access">
-            <SharedAccess />
-          </Tab>
-          )}
+        {/* { shareRoomEnabled */}
+        {/*  && ( */}
+        {/*  <Tab eventKey="access" title="Access"> */}
+        {/*    <SharedAccess /> */}
+        {/*  </Tab> */}
+        {/*  )} */}
         <Tab eventKey="settings" title="Settings">
           <RoomSettings />
         </Tab>
