ace8f6698dc3e00479f3e1d904785113df08f8d4
farhatahmad <ahmad.af.farhat@gmail.com>
2022-09-15 15:01:38 -0400

    Replaced SiteSettingAsync with SiteSetting

--- app/javascript/hooks/queries/site_settings/useSiteSetting.jsx
+++ app/javascript/hooks/queries/site_settings/useSiteSetting.jsx
@@ -4,6 +4,6 @@ import axios from '../../../helpers/Axios';
 export default function useSiteSetting(name) {
   return useQuery(
     ['getSiteSettings', name],
-    () => axios.get(`/site_settings/${name}.json`).then((resp) => resp.data.data),
+    () => axios.get(`/site_settings/${name}.json`).then((resp) => resp.data.data)
   );
 }
