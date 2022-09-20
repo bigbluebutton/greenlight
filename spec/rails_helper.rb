d91235fd3d63590a2995ee259720d27f8d2edd26
kh-amir-tn <amir.khemissi@insat.ucar.tn>
2022-09-20 18:38:46 +0100

    Mailing: Added testing capabilities. Password reset: Added missing tests.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,4 +67,5 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   #
   config.include ActiveSupport::Testing::TimeHelpers
+  config.include ActiveJob::TestHelper
 end
