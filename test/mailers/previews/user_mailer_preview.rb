8d326e53646559c46db92bacd3ec1110be96e13e
kh-amir-tn <amir.khemissi@insat.ucar.tn>
2022-09-20 15:47:20 +0100

    Mailing: Adding `reset_password_email`. 	TODO: 		+ Add and setup email testing to rspec.

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -4,4 +4,10 @@ class UserMailerPreview < ActionMailer::Preview
   def test_email
     UserMailer.with(to: 'user@users.com', subject: 'Test Subject').test_email
   end
+
+  def reset_password_email
+    fake_user = Struct.new(:name, :email)
+
+    UserMailer.with(user: fake_user.new('user', 'user@users.com'), expires_in: 1.hour.from_now, reset_url: 'https://example.com/').reset_password_email
+  end
 end
