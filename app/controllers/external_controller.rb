d0aec2c13754a02d7070b57271c50860ccf75d9c
farhatahmad <ahmad.af.farhat@gmail.com>
2022-09-21 15:27:05 -0400

    remove unneeded change

--- app/controllers/external_controller.rb
+++ app/controllers/external_controller.rb
@@ -5,14 +5,12 @@ class ExternalController < ApplicationController
   def create_user
     credentials = request.env['omniauth.auth']
     user_info = credentials['info']
-
     user = User.find_or_create_by(email: user_info['email']) do |u|
       u.external_id = credentials['uid']
       u.name = user_info['name']
       u.provider = current_provider
       u.role = Role.find_by(name: 'User') # TODO: - Ahmad: Move to service
       u.language = extract_language_code user_info['locale']
-      u.active = true
     end
 
     session[:user_id] = user.id
