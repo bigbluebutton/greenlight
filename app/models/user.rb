edb5229e71bdbd93e5db42b029186b981baeaf60
Khemissi Amir <amir.khemissi@insat.ucar.tn>
2022-09-26 15:06:55 +0100

    Emails: Added email format validation to `User` model. (#3929)

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,8 +21,11 @@ class User < ApplicationRecord
   has_one_attached :avatar
 
   validates :name, presence: true # TODO: amir - Change into full_name or seperate first and last name.
-  # TODO: amir - Validate email format.
-  validates :email, presence: true, uniqueness: { case_sensitive: false, scope: :provider }
+  validates :email,
+            format: /\A[\w\-.]+@[\w\-.]+\.[a-z]+\z/i,
+            presence: true,
+            uniqueness: { case_sensitive: false, scope: :provider }
+
   validates :provider, presence: true
 
   validates :password,
