6caab833c94a818fcfdca4debd75a879dca65e40
Ahmad Farhat <ahmad.af.farhat@gmail.com>
2022-10-04 10:01:33 -0400

    Reintroduce csrf (#3961)
    * Reintroduce csrf
    
    * eslint

--- app/controllers/external_controller.rb
+++ app/controllers/external_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ExternalController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   # GET 'auth/:provider/callback'
   def create_user
     credentials = request.env['omniauth.auth']
