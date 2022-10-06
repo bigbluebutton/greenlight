d10cc6ce93bff0fe3fbd85398be0d5582fbd9118
scouillard <samuel.couillard@blindsidenetworks.com>
2022-10-06 14:59:59 -0400

    Rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,6 @@ class ApplicationController < ActionController::Base
     @current_user ||= user
   end
 
-
   # Returns whether hcaptcha is enabled by checking if ENV variables are set
   def hcaptcha_enabled?
     (ENV['HCAPTCHA_SITE_KEY'].present? && ENV['HCAPTCHA_SECRET_KEY'].present?)
@@ -39,7 +38,6 @@ class ApplicationController < ActionController::Base
     default_role_setting = SettingGetter.new(setting_name: 'DefaultRole', provider: current_provider).call
     @default_role = Role.find_by(name: default_role_setting)
   end
-
 
   private
 
