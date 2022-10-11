052eb6945bedd6bba2bdc19d80ad8b4792c3e5c9
Khemissi Amir <amir.khemissi@insat.ucar.tn>
2022-10-11 16:28:12 +0100

    V2 Resources Migration: Defined the migrations APIs structure. (#3980)

---
+++ spec/controllers/migrations/external_controller_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Api::V1::Migrations::ExternalController, type: :controller do
+  describe '#create_role' do
+    skip
+  end
+end
