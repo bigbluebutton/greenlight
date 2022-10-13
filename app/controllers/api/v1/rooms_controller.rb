eb296d3fb3284e60741da1bb39a58c9bbb27e64f
Samuel Couillard <43917914+scouillard@users.noreply.github.com>
2022-10-13 17:29:10 -0400

    Improve Rooms query (#3993)
    * Initial commit
    
    * Fix query
    
    * Remove an extra query
    
    * Small refactor

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -21,15 +21,15 @@ module Api
 
       # GET /api/v1/rooms.json
       def index
-        # Return the rooms that belong to current user
-        user_rooms = current_user.rooms.order(online: :desc, last_session: :desc).search(params[:search])
+        shared_rooms = SharedAccess.where(user_id: current_user.id).select(:room_id)
+        rooms = Room.where(user_id: current_user.id)
+                    .or(Room.where(id: shared_rooms))
+                    .order(online: :desc, last_session: :desc)
+                    .search(params[:search])
 
-        shared_rooms = current_user.shared_rooms.order(online: :desc, last_session: :desc).search(params[:search]).map do |room|
-          room.shared = true
-          room
+        rooms.map do |room|
+          room.shared = true if room.user_id != current_user.id
         end
-
-        rooms = user_rooms + shared_rooms
 
         RunningMeetingChecker.new(rooms:).call
 
