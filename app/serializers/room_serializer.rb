08ea96f4114a016f729957201f46f560d6d4a890
scouillard <samuel.couillard@blindsidenetworks.com>
2022-10-14 16:49:45 -0400

    Remove consolelog

--- app/serializers/room_serializer.rb
+++ app/serializers/room_serializer.rb
@@ -4,7 +4,6 @@ class RoomSerializer < ApplicationSerializer
   attributes :id, :name, :friendly_id, :online, :participants, :last_session
 
   attribute :shared_owner, if: -> { object.shared }
-  # attribute :last_session, if: -> { object.last_session }
 
   def shared_owner
     object.user.name
