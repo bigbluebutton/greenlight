f799a18d2191e56aa897225abb62790de8d8e4c0
Khemissi Amir <amir.khemissi@insat.ucar.tn>
2022-10-17 19:30:59 +0100

    V2 Resources Migration: Added `Migrations::create_user` API. (#3990)
    + Added `generate_secure_pwd``to generate secure random complex passwords.

--- config/routes.rb
+++ config/routes.rb
@@ -86,6 +86,7 @@ Rails.application.routes.draw do
 
       namespace :migrations do
         post '/roles', to: 'external#create_role'
+        post '/users', to: 'external#create_user'
       end
     end
   end
