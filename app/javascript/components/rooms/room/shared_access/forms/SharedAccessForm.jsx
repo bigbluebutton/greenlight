f798f75f3338c3934b5e4cf2069ac1b74d427a77
scouillard <samuel.couillard@blindsidenetworks.com>
2022-10-17 16:48:25 -0400

    Fix scrollbar

--- app/javascript/components/rooms/room/shared_access/forms/SharedAccessForm.jsx
+++ app/javascript/components/rooms/room/shared_access/forms/SharedAccessForm.jsx
@@ -28,45 +28,47 @@ export default function SharedAccessForm({ handleClose }) {
     <div id="shared-access-form">
       <SearchBarQuery setInput={setInput} />
       <Form onSubmit={handleSubmit(createSharedUser.mutate)}>
-        <Table hover className="text-secondary my-3">
-          <thead>
-            <tr className="text-muted small">
-              <th className="fw-normal w-50">{ t('user.name') }</th>
-              <th className="fw-normal w-50">{ t('user.email_address') }</th>
-            </tr>
-          </thead>
-          <tbody className="border-top-0">
-            {
-              (() => {
-                if (isThreeCharacters && shareableUsers?.length) {
-                  return (
-                    shareableUsers.map((user) => (
-                      <tr key={user.id} className="align-middle">
-                        <td>
-                          <Stack direction="horizontal" className="py-2">
-                            <Form.Check
-                              type="checkbox"
-                              value={user.id}
-                              className="pe-3"
-                              {...register('shared_users')}
-                            />
-                            <Avatar avatar={user.avatar} radius={40} />
-                            <h6 className="text-brand mb-0 ps-3"> {user.name} </h6>
-                          </Stack>
-                        </td>
-                        <td>
-                          <span className="text-muted"> {user.email} </span>
-                        </td>
-                      </tr>
-                    )));
-                } if (isThreeCharacters && !shareableUsers?.length) {
-                  return (<tr className="fw-bold"><td>{ t('user.no_user_found') }</td><td /></tr>);
-                }
-                return (<tr className="fw-bold"><td colspan="2">{ t('user.type_three_characters') }</td></tr>);
-              })()
-            }
-          </tbody>
-        </Table>
+        <div className="table-scrollbar-wrapper">
+          <Table hover className="text-secondary my-3">
+            <thead>
+              <tr className="text-muted small">
+                <th className="fw-normal w-50">{ t('user.name') }</th>
+                <th className="fw-normal w-50">{ t('user.email_address') }</th>
+              </tr>
+            </thead>
+            <tbody className="border-top-0">
+              {
+                (() => {
+                  if (isThreeCharacters && shareableUsers?.length) {
+                    return (
+                      shareableUsers.map((user) => (
+                        <tr key={user.id} className="align-middle">
+                          <td>
+                            <Stack direction="horizontal" className="py-2">
+                              <Form.Check
+                                type="checkbox"
+                                value={user.id}
+                                className="pe-3"
+                                {...register('shared_users')}
+                              />
+                              <Avatar avatar={user.avatar} radius={40} />
+                              <h6 className="text-brand mb-0 ps-3"> {user.name} </h6>
+                            </Stack>
+                          </td>
+                          <td>
+                            <span className="text-muted"> {user.email} </span>
+                          </td>
+                        </tr>
+                      )));
+                  } if (isThreeCharacters && !shareableUsers?.length) {
+                    return (<tr className="fw-bold"><td>{ t('user.no_user_found') }</td><td /></tr>);
+                  }
+                  return (<tr className="fw-bold"><td colspan="2">{ t('user.type_three_characters') }</td></tr>);
+                })()
+              }
+            </tbody>
+          </Table>
+        </div>
         <Stack className="mt-3" direction="horizontal" gap={1}>
           <Button variant="neutral" className="ms-auto" onClick={handleClose}>
             { t('close') }
