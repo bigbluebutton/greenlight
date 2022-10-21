a5505f85a92c7554bc469a935d21875cecb2281a
scouillard <samuel.couillard@blindsidenetworks.com>
2022-10-21 18:07:52 -0400

    Initial commit

--- app/javascript/helpers/forms/SignupFormHelpers.jsx
+++ app/javascript/helpers/forms/SignupFormHelpers.jsx
@@ -3,8 +3,8 @@ import { yupResolver } from '@hookform/resolvers/yup';
 
 const validationSchema = yup.object({
   // TODO: amir - Revisit validations.
-  name: yup.string().required('Please enter your full name.'),
-  email: yup.string().required('Please enter your email.').email('Entered value does not match email format.'),
+  name: yup.string().required('Please enter a full name.'),
+  email: yup.string().required('Please enter an email.').email('Entered value does not match email format.'),
   password: yup.string()
     .matches(
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[`@%~!#£$\\^&*()\][+={}/|:;"'<>\-,.?_ ]).{8,}$/,
@@ -15,7 +15,7 @@ const validationSchema = yup.object({
     .test('oneUpper', '- One uppercase letter.', (pwd) => pwd.match(/[A-Z]/))
     .test('oneDigit', '- One digit.', (pwd) => pwd.match(/\d/))
     .test('oneSymbol', '- One symbol.', (pwd) => pwd.match(/[`@%~!#£$\\^&*()\][+={}/|:;"'<>\-,.?_ ]/)),
-  password_confirmation: yup.string().required('').oneOf([yup.ref('password')], 'Your passwords do not match.'),
+  password_confirmation: yup.string().required('').oneOf([yup.ref('password')], 'The passwords do not match.'),
 });
 
 export const signupFormConfig = {
