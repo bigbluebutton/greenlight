763a1b15733c698959cd08ead598067b28057630
scouillard <samuel.couillard@blindsidenetworks.com>
2022-10-24 15:54:58 -0400

    Initial commit

--- app/javascript/components/shared_components/search/SearchBarQuery.jsx
+++ app/javascript/components/shared_components/search/SearchBarQuery.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
 import PropTypes from 'prop-types';
+import debounce from 'lodash.debounce';
 
 export default function SearchBarQuery({ setInput }) {
   const onChangeHandler = (event) => {
     setInput(event.target.value);
   };
 
+  const debouncedOnChangeHandler = useMemo(
+    () => debounce(onChangeHandler, 300),
+    [],
+  );
+
+  useEffect(() => () => {
+    debouncedOnChangeHandler.cancel();
+  }, []);
+
   return (
     <input
       className="search-bar rounded border form-control"
       placeholder="Search"
       type="search"
-      onChange={onChangeHandler}
+      onChange={debouncedOnChangeHandler}
     />
   );
 }
