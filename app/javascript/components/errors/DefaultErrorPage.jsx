34a3c51439e852f39b558080fd5113305081b19f
Samuel Couillard <43917914+scouillard@users.noreply.github.com>
2022-10-31 09:50:01 -0400

    Improve error pages layout (#4049)
    Co-authored-by: Ahmad Farhat <ahmad.af.farhat@gmail.com>

--- app/javascript/components/errors/DefaultErrorPage.jsx
+++ app/javascript/components/errors/DefaultErrorPage.jsx
@@ -1,30 +1,28 @@
 import React from 'react';
-import { ExclamationCircleIcon } from '@heroicons/react/24/outline';
 import {
-  Card, Col, Container, Row,
+  Card, Container,
 } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import Logo from '../shared_components/Logo';
+import ButtonLink from '../shared_components/utilities/ButtonLink';
 
 export default function DefaultErrorPage() {
   const { t } = useTranslation();
 
   return (
     <Container className="no-header-height">
-      <Row className="vertical-center text-center">
-        <Row className="mb-2">
-          <h1>{t('global_error_page.title')}</h1>
-        </Row>
-        <Row>
-          <Col md={8} className="mx-auto">
-            <Card className="p-5 border-0 shadow-sm">
-              <ExclamationCircleIcon className="hi-xxl mx-auto" />
-              <pre className="text-muted">
-                {t('global_error_page.message')}
-              </pre>
-            </Card>
-          </Col>
-        </Row>
-      </Row>
+      <div className="vertical-center">
+        <div className="text-center pb-4">
+          <Logo size="medium" />
+        </div>
+        <Card className="col-md-4 mx-auto p-4 border-0 shadow-sm text-center">
+          <Card.Title className="pb-2 fs-1 text-danger">{ t('global_error_page.title') }</Card.Title>
+          <span className="mb-3">{ t('global_error_page.message') }</span>
+          <ButtonLink to="/" variant="brand" className="btn btn-lg m-2">
+            {t('return_home')}
+          </ButtonLink>
+        </Card>
+      </div>
     </Container>
   );
 }
--- app/javascript/components/errors/NotFoundPage.jsx
+++ app/javascript/components/errors/NotFoundPage.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
-import { XCircleIcon } from '@heroicons/react/24/outline';
 import {
-  Card, Col, Row,
+  Card,
 } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import Logo from '../shared_components/Logo';
+import ButtonLink from '../shared_components/utilities/ButtonLink';
 
 export default function NotFoundPage() {
   const { t } = useTranslation();
 
   return (
-    <Row className="vertical-center text-center">
-      <Row className="mb-2">
-        <h1>{t('not_found_error_page.title')}</h1>
-      </Row>
-      <Row>
-        <Col md={8} className="mx-auto">
-          <Card className="p-5 border-0 shadow-sm">
-            <XCircleIcon className="hi-xxl mx-auto" />
-            <pre className="text-muted">
-              {t('not_found_error_page.message')}
-            </pre>
-          </Card>
-        </Col>
-      </Row>
-    </Row>
+    <div className="vertical-center">
+      <div className="text-center pb-4">
+        <Logo size="medium" />
+      </div>
+      <Card className="col-md-4 mx-auto p-4 border-0 shadow-sm text-center">
+        <Card.Title className="pb-2 fs-1 text-danger">{ t('not_found_error_page.title') }</Card.Title>
+        <span className="mb-3">{ t('not_found_error_page.message') }</span>
+        <ButtonLink to="/" variant="brand" className="btn btn-lg m-2">
+          {t('return_home')}
+        </ButtonLink>
+      </Card>
+    </div>
   );
 }
