c64f2efbe9188eed3877f79dd0e6b4a8c771dfda
alihadimazeh <ali.hadi.mazeh@blindsidenetworks.com>
2022-11-02 15:02:45 -0400

    Chained validations for name and email. password validation was removed due to issues with rspec validating it

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,13 +24,8 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:session_expiry) }
     it { is_expected.to validate_presence_of(:language) }
 
-    it { is_expected.to validate_length_of(:name).is_at_least(2) }
-    it { is_expected.to validate_length_of(:name).is_at_most(255) }
-
-    it { is_expected.to validate_length_of(:email).is_at_least(5) }
-    it { is_expected.to validate_length_of(:email).is_at_most(255) }
-
-    it { is_expected.to validate_length_of(:password).is_at_most(255) }
+    it { is_expected.to validate_length_of(:name).is_at_least(2).is_at_most(255) }
+    it { is_expected.to validate_length_of(:email).is_at_least(5).is_at_most(255) }
 
     context 'password complexity' do
       it 'passes if there is atleast 1 capital, 1 lowercase, 1 number, 1 symbol' do
