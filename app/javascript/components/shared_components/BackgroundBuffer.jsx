59d0692d3aff81e5fc5fe9db070b5b49303a17d3
scouillard <samuel.couillard@blindsidenetworks.com>
2022-11-03 16:44:52 -0400

    Improve pathname search

--- app/javascript/components/shared_components/BackgroundBuffer.jsx
+++ app/javascript/components/shared_components/BackgroundBuffer.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 
 export default function BackgroundBuffer() {
   const location = useLocation();
 
-  if (location?.pathname.startsWith('/rooms/')) {
-    return <Container className="background-lg-buffer" fluid />;
-  }
-
   if (location?.pathname.startsWith('/rooms')) {
+    const { friendlyId } = useParams();
+
+    if (location?.pathname === `/rooms/${friendlyId}`) {
+      return <Container className="background-lg-buffer" fluid />;
+    }
     return <Container className="background-sm-buffer" fluid />;
   }
 
