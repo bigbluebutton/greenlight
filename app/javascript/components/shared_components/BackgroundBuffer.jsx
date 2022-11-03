d42041d674fb6a22437b5d13a4f30725633af4eb
scouillard <samuel.couillard@blindsidenetworks.com>
2022-11-03 16:48:49 -0400

    Improve logic a bit

--- app/javascript/components/shared_components/BackgroundBuffer.jsx
+++ app/javascript/components/shared_components/BackgroundBuffer.jsx
@@ -4,15 +4,15 @@ import { useLocation, useParams } from 'react-router-dom';
 
 export default function BackgroundBuffer() {
   const location = useLocation();
+  const { friendlyId } = useParams();
 
-  if (location?.pathname.startsWith('/rooms')) {
-    const { friendlyId } = useParams();
-
-    if (location?.pathname === `/rooms/${friendlyId}`) {
-      return <Container className="background-lg-buffer" fluid />;
-    }
-    return <Container className="background-sm-buffer" fluid />;
+  // The buffer is not needed if pathname doesn't start with /rooms
+  if (!location?.pathname.startsWith('/rooms')) {
+    return null;
   }
 
-  return null;
+  if (location?.pathname === `/rooms/${friendlyId}`) {
+    return <Container className="background-lg-buffer" fluid />;
+  }
+  return <Container className="background-sm-buffer" fluid />;
 }
