52f976300302f7552456e0003d1b3ba2d0581c90
scouillard <samuel.couillard@blindsidenetworks.com>
2022-11-04 12:30:33 -0400

    Fix /rooms/join background buffer

--- app/javascript/components/shared_components/BackgroundBuffer.jsx
+++ app/javascript/components/shared_components/BackgroundBuffer.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import { useLocation, useParams } from 'react-router-dom';
 
+// The background buffer is needed only in /rooms and /rooms/friendly_id
 export default function BackgroundBuffer() {
   const location = useLocation();
   const { friendlyId } = useParams();
 
-  // The background buffer is needed only in /rooms and /rooms/friendly_id
-  if (location?.pathname.startsWith('/rooms')) {
-    if (location?.pathname === `/rooms/${friendlyId}`) {
-      return <Container className="background-lg-buffer" fluid />;
-    }
+  // The background buffer is not needed in /rooms/friendly_id/join
+  if (location?.pathname.includes(`/rooms/${friendlyId}/join`)) {
+    return null;
+  }
+  if (location?.pathname.includes(`/rooms/${friendlyId}`)) {
+    return <Container className="background-lg-buffer" fluid />;
+  }
+  if (location?.pathname.includes('/rooms')) {
     return <Container className="background-sm-buffer" fluid />;
   }
-
   return null;
 }
