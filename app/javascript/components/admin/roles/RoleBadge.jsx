0d3fe9262fdcbfdf6c4e884cf330531549009842
scouillard <samuel.couillard@blindsidenetworks.com>
2022-11-09 12:38:03 -0500

    Rework user role badge

--- app/javascript/components/admin/roles/RoleBadge.jsx
+++ app/javascript/components/admin/roles/RoleBadge.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Badge } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 export default function RoleBadge({ role }) {
   return (
-    <Badge className="rounded-pill role-badge ms-2">
+    <>
       <span className="role-color-dot me-2" ref={(el) => el && el.style.setProperty('background-color', role?.color, 'important')} />
       {role?.name}
-    </Badge>
+    </>
   );
 }
 
