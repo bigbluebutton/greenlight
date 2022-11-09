9797801a5842790019d6decaf9bb6b5ebaa233ff
scouillard <samuel.couillard@blindsidenetworks.com>
2022-11-09 11:47:55 -0500

    Initial commit

--- app/javascript/components/admin/roles/RoleBadge.jsx
+++ app/javascript/components/admin/roles/RoleBadge.jsx
@@ -3,8 +3,6 @@ import { Badge } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 export default function RoleBadge({ role }) {
-  const { name, color } = role;
-
   return (
     <Badge className="rounded-pill role-badge ms-2">
       <span className="role-color-dot me-2" ref={(el) => el && el.style.setProperty('background-color', color, 'important')} />
--- app/javascript/components/admin/roles/RoleRow.jsx
+++ app/javascript/components/admin/roles/RoleRow.jsx
@@ -1,16 +1,25 @@
 import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
-import RoleBadge from './RoleBadge';
+import { ShieldCheckIcon } from '@heroicons/react/20/solid';
+import { Stack } from 'react-bootstrap';
+import { LockClosedIcon } from '@heroicons/react/24/outline';
 
 export default function RoleRow({ role }) {
   const navigate = useNavigate();
   const handleClick = useCallback(() => { navigate(`edit/${role.id}`); }, [role.id]);
 
   return (
-    <tr className="align-middle text-muted border border-2 cursor-pointer" onClick={handleClick}>
+    <tr className="align-middle border border-2 cursor-pointer" onClick={handleClick}>
       <td className="py-4">
-        <RoleBadge role={role} />
+        <Stack direction="horizontal">
+          <ShieldCheckIcon className="hi-s" ref={(el) => el && el.style.setProperty('color', role?.color, 'important')} />
+          {
+            (role?.name === 'Administrator' || role?.name === 'User' || role?.name === 'Guest')
+            && <LockClosedIcon className="hi-xs text-muted" />
+          }
+          <strong className="ms-2"> {role?.name} </strong>
+        </Stack>
       </td>
     </tr>
   );
