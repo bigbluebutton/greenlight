1b09792a24b62c9e19cc569d5e6c6661ac459289
scouillard <samuel.couillard@blindsidenetworks.com>
2022-11-09 15:05:09 -0500

    Add translation

--- app/javascript/components/admin/roles/forms/CreateRoleForm.jsx
+++ app/javascript/components/admin/roles/forms/CreateRoleForm.jsx
@@ -4,6 +4,7 @@ import {
   Button, Stack,
 } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
 import Form from '../../../shared_components/forms/Form';
 import Spinner from '../../../shared_components/utilities/Spinner';
 import FormControl from '../../../shared_components/forms/FormControl';
@@ -11,6 +12,7 @@ import { createRoleFormConfig, createRoleFormFields } from '../../../../helpers/
 import useCreateRole from '../../../../hooks/mutations/admin/roles/useCreateRole';
 
 export default function CreateRoleForm({ handleClose }) {
+  const { t } = useTranslation();
   const createRole = useCreateRole({ onSettled: handleClose });
   const methods = useForm(createRoleFormConfig);
   const fields = createRoleFormFields;
@@ -19,11 +21,11 @@ export default function CreateRoleForm({ handleClose }) {
     <Form methods={methods} onSubmit={createRole.mutate}>
       <FormControl field={fields.name} type="text" />
       <Stack className="mt-1" direction="horizontal" gap={1}>
-        <Button variant="brand-outline" className="ms-auto" onClick={handleClose}>
-          Close
+        <Button variant="neutral" className="ms-auto" onClick={handleClose}>
+          {t('close')}
         </Button>
         <Button variant="brand" type="submit" disabled={createRole.isLoading}>
-          Create Role
+          {t('admin.roles.create_role')}
           {createRole.isLoading && <Spinner />}
         </Button>
       </Stack>
