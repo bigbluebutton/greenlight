e6c956ae3e6b0a05ece0e244dc697185b0d09f52
scouillard <samuel.couillard@blindsidenetworks.com>
2022-11-10 16:54:03 -0500

    Work on Rooms/Room Card responsiveness

--- app/javascript/components/rooms/RoomsList.jsx
+++ app/javascript/components/rooms/RoomsList.jsx
@@ -21,7 +21,7 @@ export default function RoomsList() {
 
   return (
     <>
-      <Stack direction="horizontal" className="w-100 mt-5">
+      <Stack direction="horizontal" className="mt-5" gap={3}>
         <div>
           <SearchBar searchInput={searchInput} id="rooms-search" setSearchInput={setSearchInput} />
         </div>
@@ -36,7 +36,7 @@ export default function RoomsList() {
           // eslint-disable-next-line react/no-array-index-key
           (isLoading && [...Array(8)].map((val, idx) => <Col key={idx} className="col-md-auto mt-0 mb-4"><RoomCardPlaceHolder /></Col>))
           || rooms?.map((room) => (
-            <Col key={room.friendly_id} className="col-md-auto mt-0 mb-4">
+            <Col key={room.friendly_id} className="col-md-auto col-xs-12 mt-0 mb-4">
               {(room.optimistic && <RoomCardPlaceHolder />) || <RoomCard room={room} />}
             </Col>
           ))
