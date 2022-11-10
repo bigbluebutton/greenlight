be1982a3d385cdda771041579078338ba4fc3e99
scouillard <samuel.couillard@blindsidenetworks.com>
2022-11-10 17:57:20 -0500

    Fix RoomCard placeholder

---
+++ app/javascript/components/rooms/RoomCardPlaceHolder.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Card, Placeholder } from 'react-bootstrap';
+
+export default function RoomCardPlaceHolder() {
+  return (
+    <Card id="room-card" border="light">
+      <Card.Body>
+        <Placeholder as={Card.Title} animation="glow" className="mb-3">
+          <Placeholder style={{ height: '65px', width: '65px' }} />
+        </Placeholder>
+        <Placeholder as={Card.Title} animation="glow">
+          <Placeholder xs={5} size="sm" />
+        </Placeholder>
+        <Placeholder as={Card.Text} animation="glow">
+          <Placeholder xs={4} size="xs" /> <Placeholder xs={6} size="xs" />
+          <Placeholder xs={2} size="xs" />
+        </Placeholder>
+        <hr />
+        <Placeholder.Button variant="brand-outline" className="float-end" animation="glow"> Start</Placeholder.Button>
+      </Card.Body>
+    </Card>
+  );
+}
--- app/javascript/components/rooms/RoomsList.jsx
+++ app/javascript/components/rooms/RoomsList.jsx
@@ -6,7 +6,7 @@ import { useTranslation } from 'react-i18next';
 import RoomCard from './RoomCard';
 import useRooms from '../../hooks/queries/rooms/useRooms';
 import useCreateRoom from '../../hooks/mutations/rooms/useCreateRoom';
-import RoomPlaceHolder from './RoomPlaceHolder';
+import RoomCardPlaceHolder from './RoomCardPlaceHolder';
 import Modal from '../shared_components/modals/Modal';
 import CreateRoomForm from './room/forms/CreateRoomForm';
 import { useAuth } from '../../contexts/auth/AuthProvider';
@@ -34,10 +34,10 @@ export default function RoomsList() {
       <Row className="g-4 mt-4">
         {
           // eslint-disable-next-line react/no-array-index-key
-          (isLoading && [...Array(8)].map((val, idx) => <Col key={idx} className="mt-0 mb-4"><RoomPlaceHolder /></Col>))
+          (isLoading && [...Array(8)].map((val, idx) => <Col key={idx} className="col-md-auto mt-0 mb-4"><RoomCardPlaceHolder /></Col>))
           || rooms?.map((room) => (
             <Col key={room.friendly_id} className="col-md-auto mt-0 mb-4">
-              {(room.optimistic && <RoomPlaceHolder />) || <RoomCard room={room} />}
+              {(room.optimistic && <RoomCardPlaceHolder />) || <RoomCard room={room} />}
             </Col>
           ))
         }
