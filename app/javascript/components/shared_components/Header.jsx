18f1a443e745d3ac9d5177e9cffa321ab340dafa
scouillard <samuel.couillard@blindsidenetworks.com>
2022-11-14 18:06:47 -0500

    Improve Navbar dropdown + fix mobile

--- app/javascript/components/shared_components/Header.jsx
+++ app/javascript/components/shared_components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import { Navbar, NavDropdown, Stack } from 'react-bootstrap';
+import { Nav, NavDropdown, Navbar, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { StarIcon, IdentificationIcon, QuestionMarkCircleIcon } from '@heroicons/react/24/outline';
 import { useAuth } from '../../contexts/auth/AuthProvider';
 import useDeleteSession from '../../hooks/mutations/sessions/useDeleteSession';
 import Avatar from '../users/user/Avatar';
 import Logo from './Logo';
+import ButtonLink from './utilities/ButtonLink';
 
 export default function Header() {
   const { t } = useTranslation();
@@ -36,7 +38,7 @@ export default function Header() {
   }
 
   return (
-    <Navbar className="header" expand="sm">
+    <Navbar id="navbar" expand="sm">
       <Container className="ps-0">
         <Navbar.Brand as={Link} to={homePath}>
           <Logo />
@@ -46,18 +48,60 @@ export default function Header() {
           <Avatar avatar={currentUser?.avatar} radius={40} />
         </Navbar.Toggle>
 
-        <Navbar.Collapse className="justify-content-end">
-          <Avatar avatar={currentUser?.avatar} radius={40} />
-          <NavDropdown title={currentUser?.name} id="nav-user-dropdown">
-            <NavDropdown.Item as={Link} to="/profile">{ t('user.profile.profile') }</NavDropdown.Item>
+        {/* /!* Visible only on mobile *!/ */}
+        <Navbar.Collapse id="navbar-menu" className="bg-white">
+          <Nav className="d-block d-sm-none text-black">
+            <Nav.Link as={Link} to="/profile">
+              <IdentificationIcon className="hi-s me-3" />
+              {t('user.profile.profile')}
+            </Nav.Link>
+            <Nav.Link href="https://docs.bigbluebutton.org/greenlight/gl-overview.html">
+              <QuestionMarkCircleIcon className="hi-s me-3" />
+              {t('help_center')}
+            </Nav.Link>
             {
               adminAccess()
-              && <NavDropdown.Item as={Link} to="/admin">{ t('admin.admin_panel') }</NavDropdown.Item>
+              && (
+                <Nav.Link as={Link} to="/admin">
+                  <StarIcon className="hi-s me-3 mb-1" />
+                  { t('admin.admin_panel') }
+                </Nav.Link>
+              )
             }
             <NavDropdown.Divider />
-            <NavDropdown.Item onClick={deleteSession.mutate}>{ t('authentication.sign_out') }</NavDropdown.Item>
-          </NavDropdown>
+            <Button onClick={deleteSession.mutate} variant="brand" className="btn btn-sm mt-2 mb-3 py-2 w-100">{t('authentication.sign_out')}</Button>
+          </Nav>
         </Navbar.Collapse>
+
+        {/* Not visible on mobile */}
+        <div className="justify-content-end d-none d-sm-block">
+          <div className="d-inline-block">
+            <Avatar avatar={currentUser?.avatar} radius={40} />
+          </div>
+          <NavDropdown title={currentUser?.name} id="nav-user-dropdown" className="d-inline-block">
+            <NavDropdown.Item as={Link} to="/profile">
+              <IdentificationIcon className="hi-s me-3" />
+              { t('user.profile.profile') }
+            </NavDropdown.Item>
+            <NavDropdown.Item href="https://docs.bigbluebutton.org/greenlight/gl-overview.html">
+              <QuestionMarkCircleIcon className="hi-s me-3" />
+              {t('help_center')}
+            </NavDropdown.Item>
+            {
+              adminAccess()
+              && (
+                <NavDropdown.Item as={Link} to="/admin">
+                  <StarIcon className="hi-s me-3 mb-1" />
+                  { t('admin.admin_panel') }
+                </NavDropdown.Item>
+              )
+            }
+            <NavDropdown.Divider />
+            <div className="px-2">
+              <Button onClick={deleteSession.mutate} variant="brand" className="btn btn-sm w-100 my-2">{t('authentication.sign_out')}</Button>
+            </div>
+          </NavDropdown>
+        </div>
       </Container>
     </Navbar>
   );
