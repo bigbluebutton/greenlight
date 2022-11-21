6cb4bed221ecfcb7bd69de1a57f821f3b78f24f6
scouillard <samuel.couillard@blindsidenetworks.com>
2022-11-21 12:17:50 -0500

    Add timer to avatar

--- app/javascript/components/users/user/Avatar.jsx
+++ app/javascript/components/users/user/Avatar.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'react-bootstrap/Image';
 import PropTypes from 'prop-types';
+import { Placeholder } from 'react-bootstrap';
 
 export default function Avatar({ avatar, radius, className }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 1000);
+  });
+
+  if (isLoading) {
+    return (
+      <Placeholder animation="glow" className="mb-3">
+        <Placeholder style={{ height: radius, width: radius, 'border-radius': '50%' }} />
+      </Placeholder>
+    );
+  }
+
   return (
     <Image src={avatar} roundedCircle="true" width={radius} height={radius} className={className} />
   );
