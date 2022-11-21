41da5a6fcee3f4ea9d127dc203808ffab68dee57
scouillard <samuel.couillard@blindsidenetworks.com>
2022-11-21 12:22:22 -0500

    Add suspense

--- app/javascript/components/users/user/Avatar.jsx
+++ app/javascript/components/users/user/Avatar.jsx
@@ -1,25 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Image from 'react-bootstrap/Image';
 import PropTypes from 'prop-types';
 import { Placeholder } from 'react-bootstrap';
 
 export default function Avatar({ avatar, radius, className }) {
-  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => setIsLoading(false), 1000);
-  });
-
-  if (isLoading) {
-    return (
+  return (
+    <React.Suspense fallback={(
       <Placeholder animation="glow" className="mb-3">
         <Placeholder style={{ height: radius, width: radius, 'border-radius': '50%' }} />
       </Placeholder>
-    );
-  }
-
-  return (
-    <Image src={avatar} roundedCircle="true" width={radius} height={radius} className={className} />
+    )}
+    >
+      <Image src={avatar} roundedCircle="true" width={radius} height={radius} className={className} />
+    </React.Suspense>
   );
 }
 
