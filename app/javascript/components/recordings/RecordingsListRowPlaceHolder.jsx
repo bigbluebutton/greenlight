6905d4d1255e887df26c8f705d57380283cbaf57
alihadimazeh <ali.hadi.mazeh@blindsidenetworks.com>
2022-11-22 10:40:20 -0500

    Server Rooms PlaceHolders
    Added placeholders to server rooms

---
+++ app/javascript/components/recordings/RecordingsListRowPlaceHolder.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Placeholder } from 'react-bootstrap';
+
+export default function RecordingsListRowPlaceHolder() {
+  return (
+    <tr>
+      <td>
+        <Placeholder className="ps-0" animation="glow">
+          <Placeholder xs={8} size="md" className="me-2" bg="secondary" />
+        </Placeholder>
+        <Placeholder className="ps-0" animation="glow">
+          <Placeholder xs={10} size="md" className="me-2" bg="secondary" />
+        </Placeholder>
+      </td>
+      <td>
+        <Placeholder className="ps-0" animation="glow">
+          <Placeholder xs={6} size="md" className="me-2" bg="secondary" />
+        </Placeholder>
+      </td>
+      <td>
+        <Placeholder className="ps-0" animation="glow">
+          <Placeholder xs={6} size="md" className="me-2" bg="secondary" />
+        </Placeholder>
+      </td>
+      <td>
+        <Placeholder className="ps-0" animation="glow">
+          <Placeholder xs={6} size="md" className="me-2" bg="secondary" />
+        </Placeholder>
+      </td>
+      <td>
+        <Placeholder className="ps-0" animation="glow">
+          <Placeholder xs={6} size="md" className="me-2" bg="secondary" />
+        </Placeholder>
+      </td>
+    </tr>
+  );
+}
