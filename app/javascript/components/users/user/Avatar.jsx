59763216a0fc19d17cabaa3135ee2ac82f65aee0
Samuel Couillard <43917914+scouillard@users.noreply.github.com>
2022-11-23 10:20:59 -0500

    Add Placeholder to Avatar (#4166)
    * Add timer to avatar
    
    * Add suspense
    
    * Add loading state with onLoad

--- app/javascript/components/users/user/Avatar.jsx
+++ app/javascript/components/users/user/Avatar.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+/* eslint-disable react/jsx-no-bind */
+import React, { useState } from 'react';
 import Image from 'react-bootstrap/Image';
 import PropTypes from 'prop-types';
+import { Placeholder } from 'react-bootstrap';
 
 export default function Avatar({ avatar, radius, className }) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  function onLoad() {
+    setIsLoading(true);
+  }
+
   return (
-    <Image src={avatar} roundedCircle="true" width={radius} height={radius} className={className} />
+    <>
+      <Image
+        src={avatar}
+        roundedCircle="true"
+        width={radius}
+        height={radius}
+        className={className}
+        onLoad={onLoad}
+      />
+      {!isLoading && (
+      <Placeholder animation="glow" className="mb-3">
+        <Placeholder style={{ height: radius, width: radius, 'border-radius': '50%' }} />
+      </Placeholder>
+      )}
+    </>
   );
 }
 
