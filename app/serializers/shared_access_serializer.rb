1975d71f2fe6f3a3bb52ddd8a73fc97a5720dbbf
alihadimazeh <ali.hadi.mazeh@blindsidenetworks.com>
2022-12-09 15:30:44 -0500

    Created Shared Access Serializer
    Created new Shared Access Serializer that only returns name, id, email and avatar

---
+++ app/serializers/shared_access_serializer.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class SharedAccessSerializer < ApplicationSerializer
+  include Avatarable
+    attributes :name, :email, :id, :avatar
+
+  def avatar
+    user_avatar(object)
+  end
+end
+  
