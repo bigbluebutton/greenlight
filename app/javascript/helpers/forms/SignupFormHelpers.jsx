708bdc08a21f9b14f0cb137f46fc72ae7eece7e5
Ahmad Farhat <ahmad.af.farhat@gmail.com>
2022-12-19 14:17:36 -0500

    Added localization to validations (#4281)

--- app/javascript/helpers/forms/SignupFormHelpers.jsx
+++ app/javascript/helpers/forms/SignupFormHelpers.jsx
@@ -3,25 +3,26 @@ import { yupResolver } from '@hookform/resolvers/yup';
 
 const validationSchema = yup.object({
   // TODO: amir - Revisit validations.
-  name: yup.string().required('Please enter a full name.')
-    .min(2, 'Name must be at least 2 characters long')
-    .max(255, 'Name must be at most 255 characters long'),
+  name: yup.string().required('forms.user.signup.full_name.required')
+    .min(2, 'forms.user.signup.full_name.min')
+    .max(255, 'forms.user.signup.full_name.max'),
 
-  email: yup.string().required('Please enter an email.').email('Entered value does not match email format.')
-    .min(6, 'Email must be at least 6 characters long')
-    .max(255, 'Email must be at most 255 characters long'),
+  email: yup.string().required('forms.user.signup.email.required').email('forms.user.signup.email.email')
+    .min(6, 'forms.user.signup.email.min')
+    .max(255, 'forms.user.signup.email.max'),
 
-  password: yup.string().max(255, 'Password must be at most 255 characters long')
+  password: yup.string().max(255, 'forms.user.signup.password.max')
     .matches(
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[`@%~!#£$\\^&*()\][+={}/|:;"'<>\-,.?_ ]).{8,}$/,
-      'Password must have at least:',
+      'forms.user.signup.password.match',
     )
-    .min(8, '- Eight characters.')
-    .test('oneLower', '- One lowercase letter.', (pwd) => pwd.match(/[a-z]/))
-    .test('oneUpper', '- One uppercase letter.', (pwd) => pwd.match(/[A-Z]/))
-    .test('oneDigit', '- One digit.', (pwd) => pwd.match(/\d/))
-    .test('oneSymbol', '- One symbol.', (pwd) => pwd.match(/[`@%~!#£$\\^&*()\][+={}/|:;"'<>\-,.?_ ]/)),
-  password_confirmation: yup.string().required('').oneOf([yup.ref('password')], 'The passwords do not match.'),
+    .min(8, 'forms.user.signup.password.min')
+    .test('oneLower', 'forms.user.signup.password.lower', (pwd) => pwd.match(/[a-z]/))
+    .test('oneUpper', 'forms.user.signup.password.upper', (pwd) => pwd.match(/[A-Z]/))
+    .test('oneDigit', 'forms.user.signup.password.digit', (pwd) => pwd.match(/\d/))
+    .test('oneSymbol', 'forms.user.signup.password.symbol', (pwd) => pwd.match(/[`@%~!#£$\\^&*()\][+={}/|:;"'<>\-,.?_ ]/)),
+  password_confirmation: yup.string().required('forms.user.signup.password_confirmation.required')
+    .oneOf([yup.ref('password')], 'forms.user.signup.password_confirmation.match'),
 });
 
 export const signupFormConfig = {
