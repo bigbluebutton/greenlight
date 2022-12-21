10c2414d7dc8ed9fd7b3b13f07cd2559886b8a59
Khemissi Amir <amir.khemissi@insat.ucar.tn>
2022-12-21 17:58:01 +0100

    Forms: Refactored `UserSignupForm`. (#4300)

--- app/javascript/components/admin/manage_users/forms/UserSignupForm.jsx
+++ app/javascript/components/admin/manage_users/forms/UserSignupForm.jsx
@@ -6,20 +6,16 @@ import FormControl from '../../../shared_components/forms/FormControl';
 import Form from '../../../shared_components/forms/Form';
 import Spinner from '../../../shared_components/utilities/Spinner';
 import useAdminCreateUser from '../../../../hooks/mutations/admin/manage_users/useAdminCreateUser';
-import useSignUpForm from '../../../../hooks/forms/authentication/useSignUpForm';
+import useCreateUserForm from '../../../../hooks/forms/admin/create_user/useCreateUserForm';
 
 export default function UserSignupForm({ handleClose }) {
   const { t } = useTranslation();
-  const { fields, methods } = useSignUpForm();
-  const createUser = useAdminCreateUser({ onSettled: handleClose });
-  const { isSubmitting } = methods.formState;
-
-  fields.name.placeHolder = t('admin.manage_users.enter_user_name');
-  fields.email.placeHolder = t('admin.manage_users.enter_user_email');
+  const { fields, methods } = useCreateUserForm();
+  const createUserAPI = useAdminCreateUser({ onSettled: handleClose });
 
   return (
-    <Form methods={methods} onSubmit={createUser.mutate}>
-      <FormControl field={fields.name} type="text" />
+    <Form methods={methods} onSubmit={createUserAPI.mutate}>
+      <FormControl field={fields.name} type="text" autoFocus />
       <FormControl field={fields.email} type="email" />
       <FormControl field={fields.password} type="password" />
       <FormControl field={fields.password_confirmation} type="password" />
@@ -28,8 +24,8 @@ export default function UserSignupForm({ handleClose }) {
         <Button variant="neutral" className="ms-auto" onClick={handleClose}>
           {t('close')}
         </Button>
-        <Button variant="brand" type="submit" disabled={isSubmitting}>
-          { isSubmitting && <Spinner className="me-2" /> }
+        <Button variant="brand" type="submit" disabled={createUserAPI.isLoading}>
+          { createUserAPI.isLoading && <Spinner className="me-2" /> }
           { t('admin.manage_users.create_account') }
         </Button>
       </Stack>
