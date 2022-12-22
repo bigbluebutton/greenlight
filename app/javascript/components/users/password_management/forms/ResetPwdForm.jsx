6e6f773978a31a4153b728aabc9476e1070b1ad9
kh-amir-tn <amir.khemissi@insat.ucar.tn>
2022-12-22 18:41:17 +0100

    Password Mgmt Forms: Refactored ResetPwdForm.

--- app/javascript/components/users/password_management/forms/ResetPwdForm.jsx
+++ app/javascript/components/users/password_management/forms/ResetPwdForm.jsx
@@ -1,29 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Stack } from 'react-bootstrap';
-import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import FormControl from '../../../shared_components/forms/FormControl';
 import Form from '../../../shared_components/forms/Form';
-import { resetPwdFormConfig, resetPwdFormFields } from '../../../../helpers/forms/ResetPwdFormHelpers';
 import Spinner from '../../../shared_components/utilities/Spinner';
 import useResetPwd from '../../../../hooks/mutations/users/useResetPwd';
+import useResetPwdForm from '../../../../hooks/forms/users/password_management/useResetPwdForm';
 
 export default function ResetPwdForm({ token }) {
   const { t } = useTranslation();
-  const resetPwd = useResetPwd();
-  resetPwdFormConfig.defaultValues.token = token;
-  const methods = useForm(resetPwdFormConfig);
-  const fields = resetPwdFormFields;
+  const resetPwdAPI = useResetPwd();
+  const { methods, fields } = useResetPwdForm({ defaultValues: { token } });
 
   return (
-    <Form methods={methods} onSubmit={resetPwd.mutate}>
-      <FormControl field={fields.new_password} type="password" />
+    <Form methods={methods} onSubmit={resetPwdAPI.mutate}>
+      <FormControl field={fields.new_password} type="password" autoFocus />
       <FormControl field={fields.password_confirmation} type="password" />
-
       <Stack className="mt-1" gap={1}>
-        <Button variant="brand" className="w-100 mb- mt-1" type="submit" disabled={resetPwd.isLoading}>
-          {resetPwd.isLoading && <Spinner className="me-2" />}
+        <Button variant="brand" className="w-100 mb- mt-1" type="submit" disabled={resetPwdAPI.isLoading}>
+          {resetPwdAPI.isLoading && <Spinner className="me-2" />}
           { t('user.account.change_password') }
         </Button>
       </Stack>
