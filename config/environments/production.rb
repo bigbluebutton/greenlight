d6568c844590c77f7fc2da63ba8bc848f485bb6d
farhatahmad <ahmad.af.farhat@gmail.com>
2023-01-03 14:51:15 -0500

    Added Redis connection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_cache_store, { url: ENV.fetch('REDIS_URL', nil) }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter = :async # TODO: Configure :resque
