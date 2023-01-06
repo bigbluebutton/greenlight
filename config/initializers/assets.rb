b54c842e2c3228821d92934ca169e256d7048c4f
farhatahmad <ahmad.af.farhat@gmail.com>
2023-01-06 15:00:48 -0500

    Solved issue with locales'

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,7 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap-icons/font')
+Rails.application.config.assets.paths << Rails.root.join('app/assets/locales')
 
 # Precompile additional assets.
 # application.jsx, application.css, and all non-JS/CSS in the app/assets
