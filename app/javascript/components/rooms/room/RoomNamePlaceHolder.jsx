6b8d91d0aa4bbc7e98a30f9d0e3b13a95afd4d89
alihadimazeh <ali.hadi.mazeh@blindsidenetworks.com>
2023-01-11 11:14:51 -0500

    Added padding to align the nav tabs properly with the layout of the page

--- app/javascript/components/rooms/room/RoomNamePlaceHolder.jsx
+++ app/javascript/components/rooms/room/RoomNamePlaceHolder.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Stack from 'react-bootstrap';
+import { Stack } from 'react-bootstrap';
 import Placeholder from '../../shared_components/utilities/Placeholder';
 
 export default function RoomNamePlaceHolder() {
   return (
     <Stack className="room-header-wrapper">
-      <Placeholder width={8} size="lg" className="me-2" />
-      <Placeholder width={6} size="lg" className="me-2" />
+      <Placeholder width={8} size="lg" className="placeholder-xlg mb-3" />
+      <Placeholder width={6} size="lg" className="placeholder mb-3" />
     </Stack>
   );
 }
