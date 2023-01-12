350c6a2c7eb7cc7f7d097e5994cdfe04cc90ad21
scouillard <samuel.couillard@blindsidenetworks.com>
2023-01-12 16:57:48 -0500

    Add HomepageIcon component

--- app/javascript/components/home/HomePage.jsx
+++ app/javascript/components/home/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import {
-  Col, Row, Form, Stack, Container
+  Col, Row, Form, Stack,
 } from 'react-bootstrap';
 import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
@@ -11,7 +11,11 @@ import Logo from '../shared_components/Logo';
 import useSiteSetting from '../../hooks/queries/site_settings/useSiteSetting';
 import { useAuth } from '../../contexts/auth/AuthProvider';
 
-import Image from '../../../assets/images/setting.png';
+import MeetingIcon from '../../../assets/images/desktop-computer.png';
+import RecordingIcon from '../../../assets/images/webcam.png';
+import SettingsIcon from '../../../assets/images/setting.png';
+import LearningToolsIcon from '../../../assets/images/paint-brush.png';
+import HomepageIcon from './HomepageIcon';
 
 export default function HomePage() {
   const { data: env } = useEnv();
@@ -46,14 +50,14 @@ export default function HomePage() {
   // TODO - samuel: OPENID signup and signin are both pointing at the same endpoint
   return (
     <>
-      <Row className="bg-white">
-        <Col className="col-lg-6 mx-auto">
+      <Row className="wide-white">
+        <Col className="mx-auto">
           <div className="text-center pt-xl-5 my-3">
             <Logo size="medium" />
           </div>
           <div className="text-center">
             <h1 className="my-4"> {t('homepage.welcome_bbb')} </h1>
-            <p className="text-muted fs-5">
+            <p className="text-muted fs-5 px-xxl-5">
               {t('homepage.greenlight_description')}
             </p>
             <a href="https://bigbluebutton.org/" className="pt-5 fs-5 text-link"> Learn more about BigBlueButton. </a>
@@ -81,48 +85,20 @@ export default function HomePage() {
           </div>
         </Col>
       </Row>
-
-      <Container className="pt-xl-3">
-        <Row>
-          <Col>
-            <Stack className="text-center my-5 homepage-icon d-block mx-auto">
-              <div className="mb-3">
-                <img src={Image} />
-              </div>
-              <h4> Launch a meeting </h4>
-              <span> Launch a virtual class that maximizes applied learning. </span>
-            </Stack>
-          </Col>
-          <Col>
-            <Stack className="text-center my-5 homepage-icon d-block mx-auto">
-              <div className="mb-3">
-                <img src="https://i.ibb.co/Gk1mPKv/webcam.png" />
-              </div>
-              <h4> Record your meetings </h4>
-              <span> BigBlueButton meetings can be recorded and shared to the students. </span>
-            </Stack>
-          </Col>
-          <Col>
-            <Stack className="text-center my-5 homepage-icon d-block mx-auto">
-              <div className="mb-3">
-                <img src="https://i.ibb.co/T82M2jY/setting.png" />
-              </div>
-              <h4> Manage your rooms </h4>
-              <span> Configure a room to help you run effective virtual classes. </span>
-            </Stack>
-          </Col>
-          <Col>
-            <Stack className="text-center my-5 homepage-icon d-block mx-auto">
-              <div className="mb-3">
-                <img src="https://i.ibb.co/3p7shD1/paint-brush.png" />
-              </div>
-              <h4> And more! </h4>
-              <span> BigBlueButton gives you built-in tools to improve the online learning & teaching experience. </span>
-              <br />
-            </Stack>
-          </Col>
-        </Row>
-      </Container>
+      <Row>
+        <Col>
+          <HomepageIcon title={t('homepage.meeting_title')} description={t('homepage.meeting_description')} icon={MeetingIcon} />
+        </Col>
+        <Col>
+          <HomepageIcon title={t('homepage.recording_title')} description={t('homepage.recording_description')} icon={RecordingIcon} />
+        </Col>
+        <Col>
+          <HomepageIcon title={t('homepage.settings_title')} description={t('homepage.settings_description')} icon={SettingsIcon} />
+        </Col>
+        <Col>
+          <HomepageIcon title={t('homepage.learning_tools_title')} description={t('homepage.learning_tools_description')} icon={LearningToolsIcon} />
+        </Col>
+      </Row>
     </>
   );
 }
