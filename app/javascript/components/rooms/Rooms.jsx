2dea72cba387bbb16ce9917c6d42a5589fe8c861
Ahmad Farhat <ahmad.af.farhat@gmail.com>
2023-01-16 13:19:20 -0500

    Visual tweak to fix bug when switching tabs (#4515)

--- app/javascript/components/rooms/Rooms.jsx
+++ app/javascript/components/rooms/Rooms.jsx
@@ -12,16 +12,14 @@ export default function Rooms() {
   const { data: recordingsCount } = useRecordingsCount();
   const { t } = useTranslation();
   return (
-    <div className="pt-5 wide-white">
-      <Tabs defaultActiveKey="rooms" unmountOnExit>
-        <Tab className="background-whitesmoke" eventKey="rooms" title={t('room.rooms')}>
-          <RoomsList />
-        </Tab>
-        {/* TODO: May need to change this to it's own component depending on how RecordingsTable will work */}
-        <Tab className="background-whitesmoke" eventKey="recordings" title={<RecordingsCountTab count={recordingsCount} />}>
-          <Recordings />
-        </Tab>
-      </Tabs>
-    </div>
+    <Tabs className="wide-white pt-5" defaultActiveKey="rooms" unmountOnExit>
+      <Tab className="background-whitesmoke" eventKey="rooms" title={t('room.rooms')}>
+        <RoomsList />
+      </Tab>
+      {/* TODO: May need to change this to it's own component depending on how RecordingsTable will work */}
+      <Tab className="background-whitesmoke" eventKey="recordings" title={<RecordingsCountTab count={recordingsCount} />}>
+        <Recordings />
+      </Tab>
+    </Tabs>
   );
 }
