11e02c2c4b662737f839c551560793d2c775e9b6
Samuel Couillard <43917914+scouillard@users.noreply.github.com>
2023-01-19 10:43:23 -0500

    Lighten Placeholder color (#4538)

--- app/javascript/components/rooms/RoomCardPlaceHolder.jsx
+++ app/javascript/components/rooms/RoomCardPlaceHolder.jsx
@@ -8,18 +8,18 @@ export default function RoomCardPlaceHolder() {
   return (
     <Card id="room-card" border="light">
       <Card.Body>
-        <Placeholder as={Card.Title} animation="glow" className="mb-3">
+        <Placeholder as={Card.Title} animation="glow" className="mb-3" bg="placeholder">
           <Placeholder style={{ height: '65px', width: '65px', borderRadius: '10%' }} />
         </Placeholder>
-        <Placeholder as={Card.Title} animation="glow">
+        <Placeholder as={Card.Title} animation="glow" bg="placeholder">
           <Placeholder xs={5} size="sm" />
         </Placeholder>
-        <Placeholder as={Card.Text} animation="glow">
+        <Placeholder as={Card.Text} animation="glow" bg="placeholder">
           <Placeholder xs={4} size="xs" /> <Placeholder xs={6} size="xs" />
           <Placeholder xs={2} size="xs" />
         </Placeholder>
         <hr />
-        <Placeholder.Button variant="brand-outline" className="disabled float-end" animation="glow">{t('start')}</Placeholder.Button>
+        <Placeholder.Button variant="brand-outline" className="disabled float-end" animation="glow" bg="placeholder">{t('start')}</Placeholder.Button>
       </Card.Body>
     </Card>
   );
