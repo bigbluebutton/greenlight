26cf9ed85466bd4aaea8e188ef21a69dd2ab7a7a
Ahmad Farhat <ahmad.af.farhat@gmail.com>
2023-01-19 13:03:22 -0500

    Homepage and footer cleanup (#4544)

--- app/javascript/components/home/HomepageFeatureCard.jsx
+++ app/javascript/components/home/HomepageFeatureCard.jsx
@@ -6,10 +6,10 @@ export default function HomepageFeatureCard({ title, description, icon }) {
   return (
     <Card className="homepage-card h-100 shadow-sm border-0">
       <Card.Body className="p-4">
-        <div className="homepage-card-icon-circle rounded-circle mb-5 d-flex align-items-center justify-content-center">
+        <div className="homepage-card-icon-circle rounded-circle mb-4 d-flex align-items-center justify-content-center">
           { icon }
         </div>
-        <Card.Title> { title } </Card.Title>
+        <Card.Title className="pt-2"> { title } </Card.Title>
         <Card.Text className="text-muted"> { description } </Card.Text>
       </Card.Body>
     </Card>
