75feaa83b541b987efb109955a36527430369a2a
Ahmad Farhat <ahmad.af.farhat@gmail.com>
2023-01-25 17:39:34 -0500

    Order recording formats (#4650)

--- app/serializers/recording_serializer.rb
+++ app/serializers/recording_serializer.rb
@@ -4,4 +4,8 @@ class RecordingSerializer < ApplicationSerializer
   attributes :id, :record_id, :name, :length, :participants, :visibility, :protectable, :created_at
 
   has_many :formats
+
+  def formats
+    object.formats.order(:recording_type)
+  end
 end
