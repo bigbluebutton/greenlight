41269739f08d3d7f8bb8935721ea8fa7551cae6b
farhatahmad <ahmad.af.farhat@gmail.com>
2023-01-26 11:26:17 -0500

    Small fix for invite

--- app/javascript/components/users/authentication/SignIn.jsx
+++ app/javascript/components/users/authentication/SignIn.jsx
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import SigninForm from './forms/SigninForm';
 import Logo from '../../shared_components/Logo';
+import useSiteSetting from "../../../hooks/queries/site_settings/useSiteSetting";
 
 export default function SignIn() {
   const { t } = useTranslation();
+  const { data: registrationMethod } = useSiteSetting('RegistrationMethod');
 
   return (
     <div className="vertical-center">
@@ -16,9 +18,12 @@ export default function SignIn() {
       <Card className="col-xl-3 col-lg-4 col-md-6 col-8 mx-auto p-4 border-0 shadow-sm">
         <Card.Title className="text-center pb-2"> { t('authentication.sign_in') } </Card.Title>
         <SigninForm />
-        <span className="text-center text-muted small"> { t('authentication.dont_have_account') }
-          <Link to="/signup" className="text-link"> { t('authentication.sign_up') } </Link>
-        </span>
+        { registrationMethod !== 'invite' && (
+            <span className="text-center text-muted small"> { t('authentication.dont_have_account') }
+              <Link to="/signup" className="text-link"> { t('authentication.sign_up') } </Link>
+            </span>
+          )
+        }
       </Card>
     </div>
   );
