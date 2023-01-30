bde26132cd5998cc6af15daaff8486b242cdc1e0
farhatahmad <ahmad.af.farhat@gmail.com>
2023-01-30 15:48:36 -0500

    Refactor more forms

--- app/javascript/components/rooms/room/forms/CreateRoomForm.jsx
+++ app/javascript/components/rooms/room/forms/CreateRoomForm.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
 import {
   Button, Stack,
 } from 'react-bootstrap';
@@ -8,20 +7,18 @@ import { useTranslation } from 'react-i18next';
 import Form from '../../../shared_components/forms/Form';
 import Spinner from '../../../shared_components/utilities/Spinner';
 import FormControl from '../../../shared_components/forms/FormControl';
-import { createRoomFormConfig, createRoomFormFields } from '../../../../helpers/forms/CreateRoomFormHelpers';
 import { useAuth } from '../../../../contexts/auth/AuthProvider';
+import useRoomForm from '../../../../hooks/forms/rooms/useRoomForm';
 
 export default function CreateRoomForm({ mutation: useCreateRoomAPI, userId, handleClose }) {
   const { t } = useTranslation();
   const currentUser = useAuth();
   const createRoomAPI = useCreateRoomAPI({ onSettled: handleClose, user_id: currentUser.id });
-  createRoomFormConfig.defaultValues.user_id = userId;
-  const methods = useForm(createRoomFormConfig);
-  const { name } = createRoomFormFields;
+  const { methods, fields } = useRoomForm({ defaultValues: { user_id: userId } });
 
   return (
     <Form methods={methods} onSubmit={createRoomAPI.mutate}>
-      <FormControl field={name} type="text" />
+      <FormControl field={fields.name} type="text" />
       <Stack className="mt-1" direction="horizontal" gap={1}>
         <Button variant="neutral" className="ms-auto" onClick={handleClose}>
           { t('close') }
