94e5099e7bb8f1b577d3f2f1a58c342acc7013b5
scouillard <samuel.couillard@blindsidenetworks.com>
2023-01-31 14:53:54 -0500

    Reduce width of RecordingsRowPlaceholder tds

--- app/javascript/components/admin/manage_users/ManageUsersRowPlaceHolder.jsx
+++ app/javascript/components/admin/manage_users/ManageUsersRowPlaceHolder.jsx
@@ -3,10 +3,10 @@ import { Stack } from 'react-bootstrap';
 import Placeholder from '../../shared_components/utilities/Placeholder';
 import RoundPlaceholder from '../../shared_components/utilities/RoundPlaceholder';
 
-export default function RecordingsListRowPlaceHolder() {
+export default function ManageUsersRowPlaceHolder() {
   return (
     <tr>
-      <td className="border-0 py-2 xl-td-placeholder">
+      <td className="border-0 py-2 lg-td-placeholder">
         <Stack direction="horizontal">
           <RoundPlaceholder size="small" className="ms-1 me-3 mt-1" />
           <Stack>
@@ -15,7 +15,7 @@ export default function RecordingsListRowPlaceHolder() {
           </Stack>
         </Stack>
       </td>
-      <td className="border-0 py-3 xl-td-placeholder">
+      <td className="border-0 py-3 lg-td-placeholder">
         <Placeholder width={10} size="md" />
       </td>
       <td className="border-0 py-3 md-td-placeholder">
