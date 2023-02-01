5600808846d9c656778ff6a02aa022e6f2bb3ac4
farhatahmad <ahmad.af.farhat@gmail.com>
2023-02-01 14:34:58 -0500

    Revert guest=true

--- app/controllers/concerns/joiner.rb
+++ app/controllers/concerns/joiner.rb
@@ -68,7 +68,6 @@ module Joiner
       opts[:record] = record_meeting
       opts[:require_moderator_approval] = room_setting_with_config("requireModeratorApproval")
       opts[:mute_on_start] = room_setting_with_config("muteOnStart")
-      opts[:guest] = current_user.nil?
 
       if current_user
         redirect_to join_path(@room, current_user.name, opts, current_user.uid)
