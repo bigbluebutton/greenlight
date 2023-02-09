dd8e587b37e89360e70f3d4c5e86f31286ecff36
Ahmad Farhat <ahmad.af.farhat@gmail.com>
2023-02-09 15:22:41 -0500

    Solve recordings n+1 (#4783)

--- app/serializers/recording_serializer.rb
+++ app/serializers/recording_serializer.rb
@@ -4,8 +4,4 @@ class RecordingSerializer < ApplicationSerializer
   attributes :id, :record_id, :name, :length, :participants, :visibility, :protectable, :created_at
 
   has_many :formats
-
-  def formats
-    object.formats.order(:recording_type)
-  end
 end
