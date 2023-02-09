785886c2ec713f36a20fcf9ef64af42aefc6a93f
scouillard <samuel.couillard@blindsidenetworks.com>
2023-02-08 23:01:52 -0500

    Fix SharedAccess query

--- app/services/permissions_checker.rb
+++ app/services/permissions_checker.rb
@@ -62,10 +62,8 @@ class PermissionsChecker
   def authorize_shared_room
     return false if @friendly_id.blank? && @record_id.blank?
 
-    # SharedAccess.joins(:shared_room).find_by(user_id: @current_user.id, 'shared_room.friendly_id': @friendly_id).present?
-      # double check the other method with Recording.. not sure why this exists
-
-    @current_user.shared_rooms.exists?(friendly_id: @friendly_id) ||
+    SharedAccess.joins(:shared_room).find_by(user_id: @current_user.id, 'shared_room.friendly_id': @friendly_id).present? ||
+      # allow shared user to access shared rooms
       @current_user.shared_rooms.exists?(id: Recording.find_by(record_id: @record_id)&.room_id)
   end
 
