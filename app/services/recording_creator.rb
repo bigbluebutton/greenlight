49b128ed0bd09bdacdf33bd9b60de779fada4eab
scouillard <samuel.couillard@blindsidenetworks.com>
2023-02-27 14:44:02 -0500

    Improve comments

--- app/services/recording_creator.rb
+++ app/services/recording_creator.rb
@@ -30,7 +30,7 @@ class RecordingCreator
 
     new_name = @recording[:metadata][:name] || @recording[:name]
 
-    # Disable timestamps so the created_at and updated_at are fetched from the BBB response
+    # Disable timestamps so the created_at is hardcoded from the BBB response
     Recording.record_timestamps = false
     new_recording = Recording.find_or_create_by(room_id:,
                                                 name: new_name,
@@ -41,7 +41,7 @@ class RecordingCreator
                                                 protectable: @recording[:protected].present?,
                                                 # updated_at is set to the date of the re-sync if the recordings are re-synced
                                                 updated_at: Time.zone.now,
-                                                # created_at is hardcoded with the response from BBB
+                                                # created_at is hardcoded from the BBB response
                                                 created_at: @recording[:startTime])
 
     # Create format(s)
