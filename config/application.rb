02d63e6d776c5989f02603a05644b8ed9fa2d50f
kh-amir-tn <amir.khemissi@insat.ucar.tn>
2023-03-15 20:53:19 +0100

    Backend: Added support for relative URL root.

--- config/application.rb
+++ config/application.rb
@@ -62,5 +62,7 @@ module Greenlight
     config.bigbluebutton_endpoint = File.join(config.bigbluebutton_endpoint, '/api/') unless config.bigbluebutton_endpoint.end_with?('api', 'api/')
 
     config.bigbluebutton_secret = ENV.fetch('BIGBLUEBUTTON_SECRET', '8cd8ef52e8e101574e400365b55e11a6')
+
+    config.relative_url_root = "/#{ENV.fetch('RELATIVE_URL_ROOT', '')}"
   end
 end
