d03b7d53a95d9686ae8c08f0f0db35d980fd5f1a
scouillard <samuel.couillard@blindsidenetworks.com>
2023-03-20 16:38:06 -0400

    Fix logic error - set smtp_ssl_verify default to true

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
       enable_starttls_auto: ActiveModel::Type::Boolean.new.cast(ENV.fetch('SMTP_STARTTLS_AUTO', nil)),
       enable_starttls: ActiveModel::Type::Boolean.new.cast(ENV.fetch('SMTP_STARTTLS', nil)),
       tls: ActiveModel::Type::Boolean.new.cast(ENV.fetch('SMTP_TLS', nil)),
-      openssl_verify_mode: ENV.fetch('SMTP_SSL_VERIFY', nil) == 'false' ? OpenSSL::SSL::VERIFY_NONE : OpenSSL::SSL::VERIFY_PEER
+      openssl_verify_mode: ENV.fetch('SMTP_SSL_VERIFY', 'true') == 'false' ? OpenSSL::SSL::VERIFY_NONE : OpenSSL::SSL::VERIFY_PEER
     }.compact
 
     config.action_mailer.smtp_settings = smtp_settings
