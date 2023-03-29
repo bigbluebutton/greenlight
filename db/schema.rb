359640573edd3f35f68cac1563a35ff614ede0f4
Your Name <you@example.com>
2023-03-29 09:50:13 +0000

    improved database voice_brige population

--- db/schema.rb
+++ db/schema.rb
@@ -175,6 +175,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_125010) do
     t.index ["setting_id"], name: "index_site_settings_on_setting_id"
   end
 
+  create_table "tenants", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "client_secret", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tenants_on_name", unique: true
+  end
+
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
