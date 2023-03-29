96df717c3bc6b20531ae3dbaf016bde1fe538dac
Your Name <you@example.com>
2023-03-29 06:20:13 +0000

    Voice Brige database migration for existing rooms

---
+++ db/data/20230328124724_populate_voice_brige_for_existing_rooms.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class PopulateVoiceBrigeForExistingRooms < ActiveRecord::Migration[7.0]
+  def up
+    i = 0
+    Room.where(voice_bridge: nil).each do |room|
+      while Room.where(voice_bridge: 10000+i).length > 0
+        i = i + 1
+        if i >= 99999
+          raise "The db contains to many rooms to assign each one a unique voice_brige"
+        end
+      end
+      room.update(voice_bridge: 10000 + i)
+      i = i + 1
+    end
+  end
+
+  def down
+    Room.update_all(voice_bridge: nil)
+  end
+end
