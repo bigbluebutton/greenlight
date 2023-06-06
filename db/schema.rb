c6202c71eeab2d851e9e09f351195c6c1b6b059e
kh-amir-tn <amir.khemissi@insat.ucar.tn>
2023-06-06 15:07:27 +0100

    Added Listed attribute to Recordings.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_184209) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_133204) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_184209) do
     t.integer "participants", null: false
     t.boolean "protectable"
     t.datetime "recorded_at"
+    t.boolean "listed", default: false, null: false
     t.index ["room_id"], name: "index_recordings_on_room_id"
   end
 
