73112a258fbfb17a94b19cb97ceeda9b4d592c93
Ahmad Farhat <ahmad.af.farhat@gmail.com>
2023-11-20 11:26:58 -0500

    Add a new role permission for receiving an email on user signup (#5529)
    * Add a new role permission for receiving an email on user signup
    
    * Fix text email
    
    * rubo

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -76,6 +76,9 @@ module Api
             UserMailer.with(user:,
                             activation_url: activate_account_url(token), base_url: request.base_url,
                             provider: current_provider).activate_account_email.deliver_later
+
+            UserMailer.with(user:, admin_panel_url:, provider: current_provider)
+                      .new_user_signup_email.deliver_later
           end
 
           create_default_room(user)
