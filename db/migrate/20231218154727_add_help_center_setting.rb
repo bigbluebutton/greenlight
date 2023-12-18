25e5f6871bb6bfc71d8906f95afb8a2695985824
SilentFlameCR <rodrigues.rahul77@gmail.com>
2023-12-18 13:32:11 -0500

    combined HelpCenter migrations for simplicity

--- db/migrate/20231218154727_add_help_center_setting.rb
+++ db/migrate/20231218154727_add_help_center_setting.rb
@@ -3,9 +3,18 @@
 class AddHelpCenterSetting < ActiveRecord::Migration[7.1]
   def up
     Setting.create!(name: 'HelpCenter') unless Setting.exists?(name: 'HelpCenter')
+
+    return if SiteSetting.exists?(setting: Setting.find_by(name: 'HelpCenter'))
+
+    SiteSetting.create!(
+      setting: Setting.find_by(name: 'HelpCenter'),
+      value: '',
+      provider: 'greenlight'
+    )
   end
 
   def down
     Setting.find_by(name: 'HelpCenter')&.destroy
+    SiteSetting.find_by(setting: Setting.find_by(name: 'HelpCenter')).destroy
   end
 end
