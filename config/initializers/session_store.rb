a9a8181aaec530ed3538db0765af492638858587
wolbernd <wolbernd@users.noreply.github.com>
2024-01-08 17:36:19 +0100

    Set path attribute in session cookie when relative url is set (#5651)
    * Set path attribute in session cookie when relative url is set
    
    * Update session_store.rb
    
    ---------
    
    Co-authored-by: Ahmad Farhat <ahmad.farhat@blindsidenetworks.com>

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -17,7 +17,8 @@
 # frozen_string_literal: true
 
 if ENV['LOADBALANCER_ENDPOINT'].present?
-  Rails.application.config.session_store :cookie_store, key: '_greenlight-3_0_session', domain: ENV.fetch('SESSION_DOMAIN_NAME', nil)
+  Rails.application.config.session_store :cookie_store, key: '_greenlight-3_0_session', domain: ENV.fetch('SESSION_DOMAIN_NAME', nil),
+                                                        path: ENV.fetch('RELATIVE_URL_ROOT', '/')
 else
-  Rails.application.config.session_store :cookie_store, key: '_greenlight-3_0_session'
+  Rails.application.config.session_store :cookie_store, key: '_greenlight-3_0_session', path: ENV.fetch('RELATIVE_URL_ROOT', '/')
 end
