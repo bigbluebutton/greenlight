7eb120f5c5d03fbe23b3e7008582c37a15b36819
farhatahmad <ahmad.af.farhat@gmail.com>
2024-01-23 13:47:33 -0500

    rubo

--- db/data/20240123184213_fix_access_to_visibilities_value.rb
+++ db/data/20240123184213_fix_access_to_visibilities_value.rb
@@ -2,7 +2,9 @@
 
 class FixAccessToVisibilitiesValue < ActiveRecord::Migration[7.1]
   def up
-    Permission.find_by(name: "AccessToVisibilities").role_permissions.where(value: "false").update_all(value: Recording::VISIBILITIES.values)
+    # rubocop:disable Rails/SkipsModelValidations
+    Permission.find_by(name: 'AccessToVisibilities').role_permissions.where(value: 'false').update_all(value: Recording::VISIBILITIES.values)
+    # rubocop:enable Rails/SkipsModelValidations
   end
 
   def down
