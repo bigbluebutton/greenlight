411782cf5fb8c38f4987f6eeac740228ffa195b9
farhatahmad <ahmad.af.farhat@gmail.com>
2024-01-31 16:54:36 -0500

    Fixed small typo

--- db/data/20231213203353_add_default_recording_visibility_to_settings.rb
+++ db/data/20231213203353_add_default_recording_visibility_to_settings.rb
@@ -4,7 +4,7 @@ class AddDefaultRecordingVisibilityToSettings < ActiveRecord::Migration[7.1]
   def up
     setting = Setting.create!(name: 'DefaultRecordingVisibility')
     SiteSetting.create!(setting:, value: 'Published', provider: 'greenlight')
-    Tenant.each do |tenant|
+    Tenant.all.each do |tenant|
       SiteSetting.create!(setting:, value: 'Published', provider: tenant.name)
     end
   end
