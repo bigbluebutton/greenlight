07181298cb2eaf1c6e9d1fdb9cda5c30fce4a8d9
Jan Kessler <Ithanil@users.noreply.github.com>
2024-02-12 16:48:23 +0100

    actually fix Help Center migration, including tenants and down-migration (#5711)

--- db/data/20240125154727_add_help_center_setting.rb
+++ db/data/20240125154727_add_help_center_setting.rb
@@ -2,17 +2,22 @@
 
 class AddHelpCenterSetting < ActiveRecord::Migration[7.1]
   def up
-    setting = Setting.create(name: 'HelpCenter') unless Setting.exists?(name: 'HelpCenter')
+    setting = Setting.find_or_create_by(name: 'HelpCenter')
 
-    SiteSetting.create(setting:, value: '', provider: 'greenlight') unless SiteSetting.exists?(setting:, value: '', provider: 'greenlight')
+    SiteSetting.create(setting:, value: '', provider: 'greenlight') unless SiteSetting.exists?(setting:, provider: 'greenlight')
 
     Tenant.all.each do |tenant|
-      SiteSetting.find_or_create_by(setting:, value: '', provider: tenant.name)
+      SiteSetting.create(setting:, value: '', provider: tenant.name) unless SiteSetting.exists?(setting:, provider: tenant.name)
     end
   end
 
   def down
+    Tenant.all.each do |tenant|
+      SiteSetting.find_by(setting: Setting.find_by(name: 'HelpCenter'), provider: tenant.name).destroy
+    end
+
+    SiteSetting.find_by(setting: Setting.find_by(name: 'HelpCenter'), provider: 'greenlight').destroy
+
     Setting.find_by(name: 'HelpCenter')&.destroy
-    SiteSetting.find_by(setting: Setting.find_by(name: 'HelpCenter')).destroy
   end
 end
