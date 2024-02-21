fefa6c8d292c738f24210fdd0a75f63570c8c847
Rahul Rodrigues <rodrigues.rahul77@gmail.com>
2024-02-21 15:47:55 -0500

    fixed data migrations maintenance and session timeout (#5721)

--- db/data/20240205160221_add_maintenance_setting.rb
+++ db/data/20240205160221_add_maintenance_setting.rb
@@ -2,18 +2,22 @@
 
 class AddMaintenanceSetting < ActiveRecord::Migration[7.1]
   def up
-    Setting.create!(name: 'Maintenance') unless Setting.exists?(name: 'Maintenance')
+    setting = Setting.find_or_create_by(name: 'Maintenance')
 
-    return if SiteSetting.exists?(setting: Setting.find_by(name: 'Maintenance'))
+    SiteSetting.create!(setting:, value: '', provider: 'greenlight') unless SiteSetting.exists?(setting:, provider: 'greenlight')
 
-    SiteSetting.create!(
-      setting: Setting.find_by(name: 'Maintenance'),
-      value: '',
-      provider: 'greenlight'
-    )
+    Tenant.all.each do |tenant|
+      SiteSetting.create!(setting:, value: '', provider: tenant.name) unless SiteSetting.exists?(setting:, provider: tenant.name)
+    end
   end
 
   def down
-    raise ActiveRecord::IrreversibleMigration
+    Tenant.all.each do |tenant|
+      SiteSetting.find_by(setting: Setting.find_by(name: 'Maintenance'), provider: tenant.name)&.destroy
+    end
+
+    SiteSetting.find_by(setting: Setting.find_by(name: 'Maintenance'), provider: 'greenlight')&.destroy
+
+    Setting.find_by(name: 'Maintenance')&.destroy
   end
 end
