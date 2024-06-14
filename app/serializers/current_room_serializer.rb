6fed1f143dc413143c74101f093862344788603c
Nadler, Sebastian <sebastian.nadler@secunet.com>
2024-06-14 10:24:54 +0000

    Clean seperation of pr #5068 (in backend)

--- app/serializers/current_room_serializer.rb
+++ app/serializers/current_room_serializer.rb
@@ -23,9 +23,6 @@ class CurrentRoomSerializer < ApplicationSerializer
 
   attribute :last_session, if: -> { object.last_session }
 
-  attribute :voice_bridge, if: -> { Rails.application.config.voice_bridge_phone_number }
-  attribute :voice_bridge_phone_number, if: -> { Rails.application.config.voice_bridge_phone_number }
-
   def presentation_name
     presentation_file_name(object)
   end
@@ -36,9 +33,5 @@ class CurrentRoomSerializer < ApplicationSerializer
 
   def owner_name
     object.user.name
-  end
-
-  def voice_bridge_phone_number
-    Rails.application.config.voice_bridge_phone_number
   end
 end
