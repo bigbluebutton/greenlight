53c4a2cf4892a4f1e60c57d5987279ca221fe408
tamar-5698 <tmrgwln31@gmail.com>
2025-06-24 14:52:14 +0300

    Clean PR – remove invalid migration only

--- db/migrate/20250605094420_add_fields_to_your_table_name.rb
+++
@@ -1,8 +0,0 @@
-class AddFieldsToRecordings < ActiveRecord::Migration[7.2]
-  def change
-    add_column :recordings, :recycle_bin_at, :datetime
-    add_column :recordings, :display_name, :string
-    add_column :recordings, :folder_path, :string
-    add_column :recordings, :downloadable, :boolean
-  end
-end
