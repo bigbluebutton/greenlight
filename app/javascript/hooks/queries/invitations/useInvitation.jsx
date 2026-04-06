85eea5cad96dd1d8cdf866b4d4046ed9296fa977
Ahmad Farhat <ahmad.af.farhat@gmail.com>
2026-04-06 10:38:37 -0400

    Improve invited registration flow (#6246)
    * Improve invited registration flow
    
    * Remove stale schema changes

--- app/javascript/hooks/queries/invitations/useInvitation.jsx
+++ app/javascript/hooks/queries/invitations/useInvitation.jsx
@@ -14,24 +14,16 @@
 // You should have received a copy of the GNU Lesser General Public License along
 // with Greenlight; if not, see <http://www.gnu.org/licenses/>.
 
-import React from 'react';
-import Placeholder from '../../shared_components/utilities/Placeholder';
+import { useQuery } from 'react-query';
+import axios from '../../../helpers/Axios';
 
-export default function ManageUsersInvitedRowPlaceHolder() {
-  return (
-    <tr>
-      <td className="border-0 py-2 lg-td-placeholder">
-        <Placeholder width={10} size="md" />
-      </td>
-      <td className="border-0 py-3 lg-td-placeholder">
-        <Placeholder width={10} size="md" />
-      </td>
-      <td className="border-0 py-3 lg-td-placeholder">
-        <Placeholder width={10} size="md" />
-      </td>
-      <td className="border-0 py-3 sm-td-placeholder">
-        <Placeholder width={5} size="md" />
-      </td>
-    </tr>
+export default function useInvitation(token) {
+  return useQuery(
+    ['getInvitation', token],
+    () => axios.get(`/invitations/${token}.json`).then((resp) => resp.data.data),
+    {
+      enabled: !!token,
+      retry: false,
+    },
   );
 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_09_153256) do
+ActiveRecord::Schema[7.2].define(version: 2026_04_02_000000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_09_153256) do
     t.string "token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email", "provider"], name: "index_invitations_on_email_and_provider", unique: true
     t.index ["token"], name: "index_invitations_on_token", unique: true
   end
